86bfeebc06b66eba0bb5c09cc58f9de991d8a7d2
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2020-11-09 15:11:04 -0600

    reset index file

--- index.php
+++ index.php
@@ -2,10 +2,4 @@
 require "vendor/autoload.php";
 use helpers\View;
 
-$subpath = $_SERVER['REQUEST_URI'];
-
-if($subpath == '/styleguide'){
-    View::render('pages/styleguide');
-}else{
-    View::render('pages/home');
-}
+View::render('pages/home');
---
+++ views/pages/styleguide.php
@@ -0,0 +1,19 @@
+<?php
+use helpers\View;
+?>
+<!-- Page Headers -->
+<?php View::render('header') ?>
+
+<body>
+<!-- Navigation -->
+<?php View::render('navigation') ?>
+
+<div class="styleguide">
+    <span>1690312</span>
+</div>
+
+<!-- Footer -->
+<?php View::render('footer'); ?>
+<script type="text/javascript" src="/dist/app.js"></script>
+</body>
+</html>
