ff5b2049203f9381618ea35681c7adb0998e3797
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2020-11-09 17:31:47 -0600

    updated classes names

--- views/pages/styleguide.php
+++ views/pages/styleguide.php
@@ -12,21 +12,21 @@ use helpers\View;
     <div class="grid-x">
         <div class="cell">
             <!--  headings  -->
-            <p class="number">1690312</p>
+            <p class="numerals">1690312</p>
             <h1 class="heading h1">HEADLINE</h1>
             <h2 class="heading h2">Headline 2</h2>
             <h3 class="heading h3">Headline 3</h3>
             <h4 class="heading h4">Headline 4 - Callout and Big Headline for Content Titles</h4>
             <h5 class="heading h5">Headline 5 - Callout and Small Headline for Content Titles</h5>
-            <p class="paragraph">Big Body. Lorem ipsum dolor sit amet, consectetur domus adipiscing elit, sed do eiusmod
+            <p class="big-copy">Big Body. Lorem ipsum dolor sit amet, consectetur domus adipiscing elit, sed do eiusmod
                 tempor incididunt</p>
-            <p class="paragraph small">Small Body. Lorem ipsum dolor sit amet, consectetur domus adipiscing elit, sed do
+            <p class="small-copy">Small Body. Lorem ipsum dolor sit amet, consectetur domus adipiscing elit, sed do
                 eiusmod tempor incididunt</p>
             <div>
-                <a class="link" href="#">READ MORE</a>
+                <a class="cta-link" href="#">READ MORE</a>
             </div>
             <div>
-                <a class="menu-item" href="#">READ MORE</a>
+                <a class="nav-item" href="#">READ MORE</a>
             </div>
             <p class="tag">Content Tag</p>
         </div>
