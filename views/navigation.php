41797bdbfc5093371513a14a87a97aae31f321a1
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2020-11-11 10:08:18 -0600

    WIP navigation

--- views/navigation.php
+++ views/navigation.php
@@ -1,2 +1,28 @@
 <header class="header">
+    <div class="grid-container">
+        <div class="grid-x">
+            <div class="cell flex-container align-justify align-middle custom-cell">
+                <div class="logo">
+                    <a class="nav-item dark" href="#">English</a>
+                </div>
+                <div class="flex-container align-middle middle-menu">
+                    <a class="nav-item dark active" href="#">Who we are</a>
+                    <a class="nav-item dark" href="#">What we do</a>
+                    <div class="middle-logo">
+                        <img src="../assets/images/logo.svg" alt="alt description">
+                    </div>
+                    <a class="nav-item dark" href="#">Our impact</a>
+                    <a class="nav-item dark" href="#">Get Involved</a>
+                </div>
+                <div class="logo right-menu">
+                    <a class="nav-item dark" href="#">
+                        <img src="../assets/images/site.svg" alt="alt description">
+                    </a>
+                    <a class="nav-item dark" href="#">
+                        <img src="../assets/images/search.svg" alt="alt description">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
