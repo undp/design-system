1e078359870d018e80407eec1bc8f8fb52fb30e6
Kevin Chavez <kevin@maniak.com>
2020-11-11 11:34:25 -0700

    added style for buttons

--- views/pages/styleguide.php
+++ views/pages/styleguide.php
@@ -31,8 +31,230 @@ use helpers\View;
             <p class="tag">Content Tag</p>
         </div>
     </div>
-</div>
 
+    <grid class="container">
+        <div class="grid-x">
+            <div class="cell medium-4">
+                <button class="btn">
+                    Submit
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11.643" height="17.341" viewBox="0 0 11.643 17.341">
+                        <path id="Path_637" data-name="Path 637" d="M-19204.023,14199.331l9.441,7.9-9.441,7.905" transform="translate(19204.666 -14198.564)" fill="none" stroke="#232e3e" stroke-width="2"/>
+                    </svg>
+                </button>
+
+                <button class="btn dark">
+                    Submit
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11.643" height="17.341" viewBox="0 0 11.643 17.341">
+                        <path id="Path_637" data-name="Path 637" d="M-19204.023,14199.331l9.441,7.9-9.441,7.905" transform="translate(19204.666 -14198.564)" fill="none" stroke="#232e3e" stroke-width="2"/>
+                    </svg>
+                </button>
+
+                <button class="btn light">
+                    Submit
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11.643" height="17.341" viewBox="0 0 11.643 17.341">
+                        <path id="Path_637" data-name="Path 637" d="M-19204.023,14199.331l9.441,7.9-9.441,7.905" transform="translate(19204.666 -14198.564)" fill="none" stroke="#D12800" stroke-width="2"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="cell medium-4">
+                <div>
+                    <button class="btn-round rotate">
+                        <span class="arrow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11.752" height="17.525" viewBox="0 0 11.752 17.525">
+                                <path id="Path_470" data-name="Path 470" d="M-19204.021,14199.33l9.551,7.994-9.551,8" transform="translate(19204.664 -14198.563)" fill="none" stroke="#fff" stroke-width="2"/>
+                            </svg>
+                        </span>
+                    </button>
+                    <button class="btn-round">
+                        <span class="arrow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11.752" height="17.525" viewBox="0 0 11.752 17.525">
+                                <path id="Path_470" data-name="Path 470" d="M-19204.021,14199.33l9.551,7.994-9.551,8" transform="translate(19204.664 -14198.563)" fill="none" stroke="#fff" stroke-width="2"/>
+                            </svg>
+                        </span>
+                    </button>
+                </div>
+                <div>
+                    <button class="btn-round">
+                        <span class="arrow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="9.332" height="15.628" viewBox="0 0 9.332 15.628">
+                              <path id="Path_605" data-name="Path 605" d="M-19204.02,14214.959l9.332-7.816-9.332-7.812Z" transform="translate(19204.02 -14199.331)" fill="#fff"/>
+                            </svg>
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div class="cell medium-4" style="background-color: #232E3E">
+                <div>
+                    <button class="btn-round light rotate">
+                        <span class="arrow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11.752" height="17.525" viewBox="0 0 11.752 17.525">
+                                <path id="Path_470" data-name="Path 470" d="M-19204.021,14199.33l9.551,7.994-9.551,8" transform="translate(19204.664 -14198.563)" fill="none" stroke="#232E3E" stroke-width="2"/>
+                            </svg>
+                        </span>
+                    </button>
+                    <button class="btn-round light">
+                        <span class="arrow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11.752" height="17.525" viewBox="0 0 11.752 17.525">
+                                <path id="Path_470" data-name="Path 470" d="M-19204.021,14199.33l9.551,7.994-9.551,8" transform="translate(19204.664 -14198.563)" fill="none" stroke="#232E3E" stroke-width="2"/>
+                            </svg>
+                        </span>
+                    </button>
+                </div>
+                <div>
+                    <button class="btn-round light">
+                        <span class="arrow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="9.332" height="15.628" viewBox="0 0 9.332 15.628">
+                              <path id="Path_605" data-name="Path 605" d="M-19204.02,14214.959l9.332-7.816-9.332-7.812Z" transform="translate(19204.02 -14199.331)" fill="#232E3E"/>
+                            </svg>
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="grid-x">
+            <div class="cell medium-4">
+                <div style="margin:20px 0">
+                    <a class="text-link" href="#">
+                        Read More
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11.633" height="17.59" viewBox="0 0 11.633 17.59">
+                            <g id="Group_282" data-name="Group 282" transform="translate(0.648 0.762)">
+                                <path id="Path_637" data-name="Path 637" d="M-19204.023,14199.331l9.441,8.031-9.441,8.035" transform="translate(19204.023 -14199.331)" fill="none" stroke="#d12800" stroke-width="2"/>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+                <div style="margin:20px 0">
+                    <a class="text-link" href="#">
+                        Explore
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11.633" height="17.59" viewBox="0 0 11.633 17.59">
+                            <g id="Group_282" data-name="Group 282" transform="translate(0.648 0.762)">
+                                <path id="Path_637" data-name="Path 637" d="M-19204.023,14199.331l9.441,8.031-9.441,8.035" transform="translate(19204.023 -14199.331)" fill="none" stroke="#d12800" stroke-width="2"/>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+                <div style="margin:20px 0">
+                    <a class="text-link arrow-3" href="#">
+                        Read More
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18.084" height="17.222" viewBox="0 0 18.084 17.222">
+                            <g id="Group_422" data-name="Group 422" transform="translate(6.844 4.729) rotate(-45)">
+                                <path id="Path_637" data-name="Path 637" d="M0,0,4.573,4.6,0,9.208" fill="none" stroke="#d12800" stroke-width="2"/>
+                            </g>
+                            <path id="Path_737" data-name="Path 737" d="M-9104.7-11526.148v9.855h9.72" transform="translate(9105.698 11532.515)" fill="none" stroke="#d12800" stroke-width="2"/>
+                            <path id="Path_738" data-name="Path 738" d="M-9099.69-11520.746l7.751-6.75" transform="translate(9104.953 11532.716)" fill="none" stroke="#d12800" stroke-width="2"/>
+                        </svg>
+                    </a>
+                </div>
+                <div style="margin:20px 0">
+                    <a class="text-link underline" href="#">
+                        www.helloworld.com
+                    </a>
+                </div>
+            </div>
+            <div class="cell medium-4" style="background-color: #232E3E">
+                <div style="margin:20px 0">
+                    <a class="text-link light-red" href="#">
+                        Read More
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11.633" height="17.59" viewBox="0 0 11.633 17.59">
+                            <g id="Group_282" data-name="Group 282" transform="translate(0.648 0.762)">
+                                <path id="Path_637" data-name="Path 637" d="M-19204.023,14199.331l9.441,8.031-9.441,8.035" transform="translate(19204.023 -14199.331)" fill="none" stroke="#d12800" stroke-width="2"/>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+                <div style="margin:20px 0">
+                    <a class="text-link light-red" href="#">
+                        Explore
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11.633" height="17.59" viewBox="0 0 11.633 17.59">
+                            <g id="Group_282" data-name="Group 282" transform="translate(0.648 0.762)">
+                                <path id="Path_637" data-name="Path 637" d="M-19204.023,14199.331l9.441,8.031-9.441,8.035" transform="translate(19204.023 -14199.331)" fill="none" stroke="#d12800" stroke-width="2"/>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+                <div style="margin:20px 0">
+                    <a class="text-link light-red arrow-3" href="#">
+                        Read More
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18.084" height="17.222" viewBox="0 0 18.084 17.222">
+                            <g id="Group_422" data-name="Group 422" transform="translate(6.844 4.729) rotate(-45)">
+                                <path id="Path_637" data-name="Path 637" d="M0,0,4.573,4.6,0,9.208" fill="none" stroke="#d12800" stroke-width="2"/>
+                            </g>
+                            <path id="Path_737" data-name="Path 737" d="M-9104.7-11526.148v9.855h9.72" transform="translate(9105.698 11532.515)" fill="none" stroke="#d12800" stroke-width="2"/>
+                            <path id="Path_738" data-name="Path 738" d="M-9099.69-11520.746l7.751-6.75" transform="translate(9104.953 11532.716)" fill="none" stroke="#d12800" stroke-width="2"/>
+                        </svg>
+                    </a>
+                </div>
+                <div style="margin:20px 0">
+                    <a class="text-link underline light-red" href="#">
+                        www.helloworld.com
+                    </a>
+                </div>
+            </div>
+            <div class="cell medium-4" style="background-color: #D12800">
+                <div style="margin:20px 0">
+                    <a class="text-link arrow-1 light-blue" href="#">
+                        Read More
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11.633" height="17.59" viewBox="0 0 11.633 17.59">
+                            <g id="Group_282" data-name="Group 282" transform="translate(0.648 0.762)">
+                                <path id="Path_637" data-name="Path 637" d="M-19204.023,14199.331l9.441,8.031-9.441,8.035" transform="translate(19204.023 -14199.331)" fill="none" stroke="#232E3E" stroke-width="2"/>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+                <div style="margin:20px 0">
+                    <a class="text-link arrow-2 light-blue" href="#">
+                        Explore
+                        <svg xmlns="http://www.w3.org/2000/svg" width="11.633" height="17.59" viewBox="0 0 11.633 17.59">
+                            <g id="Group_282" data-name="Group 282" transform="translate(0.648 0.762)">
+                                <path id="Path_637" data-name="Path 637" d="M-19204.023,14199.331l9.441,8.031-9.441,8.035" transform="translate(19204.023 -14199.331)" fill="none" stroke="#232E3E" stroke-width="2"/>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+                <div style="margin:20px 0">
+                    <a class="text-link arrow-3 light-blue" href="#">
+                        Read More
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18.084" height="17.222" viewBox="0 0 18.084 17.222">
+                            <g id="Group_422" data-name="Group 422" transform="translate(6.844 4.729) rotate(-45)">
+                                <path id="Path_637" data-name="Path 637" d="M0,0,4.573,4.6,0,9.208" fill="none" stroke="#232E3E" stroke-width="2"/>
+                            </g>
+                            <path id="Path_737" data-name="Path 737" d="M-9104.7-11526.148v9.855h9.72" transform="translate(9105.698 11532.515)" fill="none" stroke="#232E3E" stroke-width="2"/>
+                            <path id="Path_738" data-name="Path 738" d="M-9099.69-11520.746l7.751-6.75" transform="translate(9104.953 11532.716)" fill="none" stroke="#232E3E" stroke-width="2"/>
+                        </svg>
+                    </a>
+                </div>
+                <div style="margin:20px 0">
+                    <a class="text-link underline light-blue" href="#">
+                        www.helloworld.com
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="grid-x">
+            <div class="cell medium-4">
+                <div class="filter-tag">
+                    <input type="checkbox" id="check-1" class="check-button"/>
+                    <label for="check-1">Filter tag</label>
+                </div>
+            </div>
+            <div class="cell medium-4" style="background-color: #232E3E">
+                <div class="filter-tag">
+                    <input type="checkbox" id="check-2" class="check-button light-blue"/>
+                    <label class="light-blue" for="check-2">Filter tag</label>
+                </div>
+            </div>
+            <div class="cell medium-4" style="background-color: #D12800">
+                <div class="filter-tag">
+                    <input type="checkbox" id="check-3" class="check-button light-red"/>
+                    <label class="light-red" for="check-3">Filter tag</label>
+                </div>
+            </div>
+        </div>
+    </grid>
+</div>
+<div style="height: 500px"></div>
 <!-- Footer -->
 <?php View::render('footer'); ?>
 <script type="text/javascript" src="/dist/app.js"></script>
