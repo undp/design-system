4fb3f7619bd68eaf897fd76e484cb88ac279510c
Kevin Chavez <kevin@maniak.com>
2020-11-14 16:15:59 -0700

    update breakpoints, update hero markup styles for all of the breakpoints

--- views/pages/components/hero.php
+++ views/pages/components/hero.php
@@ -1,25 +1,26 @@
-
 <div class="hero" style="background: url('assets/images/hero.png') center 0 / cover no-repeat;">
-    <div class="text-box">
-        <div class="title">
-            <h1 class="heading h1 uppercase">
-                What’s next?
-            </h1>
+    <div class="hero-content">
+        <div class="text-box">
+            <div class="title">
+                <h1 class="heading h1">
+                    What’s next?
+                </h1>
+            </div>
+            <div class="separator"></div>
+            <div class="subtitle">
+                <h3 class="heading h3">
+                    Re-imagining Life in a Post-pandemic World
+                </h3>
+            </div>
         </div>
-        <div class="separator"></div>
-        <div class="subtitle">
-            <h3 class="heading h3">
-                Re-imagining Life in a Post-pandemic World
-            </h3>
+        <div class="cta-button">
+            <button class="btn">
+                Explore
+                <?=  file_get_contents("assets/images/arrows/btn-arrow-blue.svg");  ?>
+            </button>
         </div>
-    </div>
-    <div class="cta-button">
-        <button class="btn">
-            Explore
-            <?=  file_get_contents("assets/images/arrows/btn-arrow-blue.svg");  ?>
-        </button>
-    </div>
-    <div class="cta-arrow show-for-large">
-        <?=  file_get_contents("assets/images/arrows/hero-arrow-down.svg");  ?>
+        <div class="cta-arrow show-for-large">
+            <?=  file_get_contents("assets/images/arrows/hero-arrow-down.svg");  ?>
+        </div>
     </div>
 </div>
