185d063106d5997a012048378c3b2de528701e69
Kevin Chavez <kevin@maniak.com>
2020-11-16 13:04:34 -0700

    markup and style

--- views/pages/components/featured-stories.php
+++ views/pages/components/featured-stories.php
@@ -1,31 +1,78 @@
-<div class="grid-container">
-    <div class="grid-x grid-margin-x">
-        <div class="cell small-12 medium-4">
-            <h2 class="heading h2">
-                Featured
-                <br>
-                Stories
-            </h2>
-        </div>
-        <div class="cell small-12 medium-4">
-            <p class="tag">
+<section class="featured-stories">
+    <div class="grid-container">
+        <div class="grid-x grid-margin-x">
+            <div class="cell small-12 medium-4">
+                <div class="section-title">
+                    <h2 class="heading h2">
+                        Featured
+                    </h2>
+                    <h2 class="heading h2">
+                        Stories
+                    </h2>
+                </div>
+            </div>
+            <div class="cell small-12 medium-4">
+                <div class="story-card light">
+                    <p class="tag">
+                        Content Tag
+                    </p>
+                    <h5 class="heading h5">
+                        Lorem ipsum dolor sit amet, consectetur domus adipiscing elit, sed do eiusmod tempor incididunt
+                    </h5>
+                    <p class="small-copy">
+                        This year the world is commemorating the 25th anniversary of the Beijing Declaration and Platform for Action This year the world is commemorating the 25th anniversary of the Beijing Declaration and Platform for Action
+                    </p>
+                    <div class="cta">
+                        <a class="text-link arrow-1" href="#">
+                            Read More
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11.633" height="17.59"><g data-name="Group 282"><path data-name="Path 637" d="M.648.762l9.441 8.031-9.441 8.035" fill="none" stroke="#d12800" stroke-width="2"></path></g></svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="cell small-12 medium-4">
                 Content Tag
-            </p>
-
+            </div>
         </div>
-        <div class="cell small-12 medium-4">
-            Content Tag
+        <div class="grid-x grid-margin-x">
+            <div class="cell small-12 medium-8">
+                <div class="story-card large dark">
+                    <p class="tag">
+                        Content Tag
+                    </p>
+                    <h4 class="heading h4">
+                        Lorem ipsum dolor sit amet, consectetur domus adipiscing elit, sed do eiusmod tempor incididunt
+                    </h4>
+                    <p class="small-copy">
+                        This year the world is commemorating the 25th anniversary of the Beijing Declaration and Platform for Action This year the world is commemorating the 25th anniversary of the Beijing Declaration and Platform for Action
+                    </p>
+                    <div class="cta">
+                        <a class="text-link arrow-1" href="#">
+                            Read More
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11.633" height="17.59"><g data-name="Group 282"><path data-name="Path 637" d="M.648.762l9.441 8.031-9.441 8.035" fill="none" stroke="#d12800" stroke-width="2"></path></g></svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="cell small-12 medium-4">
+                <div class="story-card light">
+                    <p class="tag">
+                        Content Tag
+                    </p>
+                    <h5 class="heading h5">
+                        Lorem ipsum dolor sit amet, consectetur domus adipiscing elit, sed do eiusmod tempor incididunt
+                    </h5>
+                    <p class="small-copy">
+                        This year the world is commemorating the 25th anniversary of the Beijing Declaration and Platform for Action This year the world is commemorating the 25th anniversary of the Beijing Declaration and Platform for Action
+                    </p>
+                    <div class="cta">
+                        <a class="text-link arrow-1" href="#">
+                            Read More
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11.633" height="17.59"><g data-name="Group 282"><path data-name="Path 637" d="M.648.762l9.441 8.031-9.441 8.035" fill="none" stroke="#d12800" stroke-width="2"></path></g></svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="grid-x">
-        <div class="cell small-12 medium-4">
-            Image
-        </div>
-        <div class="cell small-12 medium-4">
-            Content Tag
-        </div>
-        <div class="cell small-12 medium-4">
-            Content Tag
-        </div>
-    </div>
-</div>
+</section>
