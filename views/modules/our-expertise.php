03b9484cfa980784d9b69685efaa06392bb8115d
Kevin Chavez <kevin@maniak.com>
2020-11-17 18:22:07 -0700

    update markup and styles

--- views/modules/our-expertise.php
+++ views/modules/our-expertise.php
@@ -1,103 +1,61 @@
+<?php
+use helpers\View;
+?>
 <div class="our-expertise">
     <div class="grid-container">
-        <div class="grid-x show-for-small-only">
-            <div class="cell small-offset-1 small-10">
-                <div class="section-title ">
+        <div class="grid-x our-expertise-row">
+            <div class="medium-offset-1 medium-5">
+                <div class="section-title hide-for-small-only">
                     <h2 class="heading h2">Our</h2>
                     <h2 class="heading h2">
                         Expertise
                     </h2>
                 </div>
             </div>
+            <div class="cell medium-3">
+                <?php
+                    View::render('components/expertise-card')
+                ?>
+            </div>
+            <div class="cell medium-3">
+                <div class="expertise-image">
+                    <?php
+                        View::render('components/image-card', [
+                            'route' => '/assets/images/placeholder/our-expertise.jpg'
+                        ])
+                    ?>
+                </div>
+            </div>
         </div>
-        <div class="our-expertise-container">
-            <div class="our-expertise-cards-container">
-                <div class="grid-x">
-                    <div class="medium-offset-1 medium-5">
-                        <div class="section-title hide-for-small-only">
-                            <h2 class="heading h2">Our</h2>
-                            <h2 class="heading h2">
-                                Expertise
-                            </h2>
-                        </div>
-                    </div>
-                    <div class="cell medium-3">
-                        <div class="expertise-image">
-                            <img src="/assets/images/placeholder/our-expertise.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="cell medium-3">
-                        <div class="expertise-card">
-                            <div class="text">
-                                <h3 class="heading h3">
-                                    People
-                                </h3>
-                                <p class="heading big-copy">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                </p>
-                            </div>
-                            <div class="cta-container">
-                                <a class="text-link arrow-1" href="#">
-                                    Read More
-                                    <?=  file_get_contents("assets/images/arrows/btn-arrow-red.svg");  ?>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+        <div class="grid-x our-expertise-row">
+            <div class="cell medium-3">
+                <div class="expertise-image">
+                    <?php
+                        View::render('components/image-card', [
+                            'route' => '/assets/images/placeholder/our-expertise.jpg'
+                        ])
+                    ?>
                 </div>
             </div>
-            <div class="our-expertise-cards-container">
-                <div class="grid-x">
-                    <div class="cell medium-3">
-                        <div class="expertise-image">
-                            <img src="/assets/images/placeholder/our-expertise.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="cell medium-3">
-                        <div class="expertise-card">
-                            <div class="text">
-                                <h3 class="heading h3">
-                                    People
-                                </h3>
-                                <p class="heading big-copy">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                </p>
-                            </div>
-                            <div class="cta-container">
-                                <a class="text-link arrow-1" href="#">
-                                    Read More
-                                    <?=  file_get_contents("assets/images/arrows/btn-arrow-red.svg");  ?>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="cell medium-3">
+                <?php
+                    View::render('components/expertise-card')
+                ?>
             </div>
-            <div class="our-expertise-cards-container">
-                <div class="grid-x">
-                    <div class="cell medium-offset-6 medium-3">
-                        <div class="expertise-image">
-                            <img src="/assets/images/placeholder/our-expertise.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="cell medium-3">
-                        <div class="expertise-card">
-                            <div class="text">
-                                <h3 class="heading h3">
-                                    People
-                                </h3>
-                                <p class="heading big-copy">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                </p>
-                            </div>
-                            <div class="cta-container">
-                                <a class="text-link arrow-1" href="#">
-                                    Read More
-                                    <?=  file_get_contents("assets/images/arrows/btn-arrow-red.svg");  ?>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+        </div>
+        <div class="grid-x our-expertise-row">
+            <div class="cell medium-3 medium-offset-6">
+                <?php
+                    View::render('components/expertise-card')
+                ?>
+            </div>
+            <div class="cell medium-3">
+                <div class="expertise-image">
+                    <?php
+                        View::render('components/image-card', [
+                                'route' => '/assets/images/placeholder/our-expertise.jpg'
+                        ])
+                    ?>
                 </div>
             </div>
         </div>
