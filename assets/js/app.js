448dfd3f0b7ff507ff285469c6f310d5004c770b
Kevin Chavez <kevin@maniak.com>
2020-11-19 12:08:20 -0700

    update footer markup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
 import modal from './components/modal.js';
 import select from './partials/select.js';
+import footerMenu from './partials/footer-menu-mobile.js';
 
 function main() {
     modal()
     select()
+    footerMenu()
 }
 
 document.addEventListener("DOMContentLoaded", function () {
---
+++ assets/js/partials/footer-menu-mobile.js
@@ -0,0 +1,24 @@
+import Foundation from 'foundation-sites'
+
+const init = function () {
+    $('.footer-subtitle').click((e)=>{
+        let $target = $(e.currentTarget);
+        let $list = $target.parent().children('.list');
+        let $subtitle = $('.footer-subtitle');
+
+        $subtitle.parent().children('.list').hide();
+        $subtitle.children('.footer-subtitle-arrow').removeClass('rotate');
+
+        $list.show();
+        $target.children('.footer-subtitle-arrow').addClass('rotate');
+
+    });
+
+    $(window).resize(()=>{
+        if(Foundation.MediaQuery.is('medium up')){
+            $('.footer-body').find('.list').show();
+        }
+    })
+}
+
+export default init
