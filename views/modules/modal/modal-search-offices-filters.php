21f1ba6ecf23faec4830a2278ec51986cf688961
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2020-11-19 12:41:17 -0600

    added custom checkbox

--- views/modules/modal/modal-search-offices-filters.php
+++ views/modules/modal/modal-search-offices-filters.php
@@ -29,63 +29,81 @@ use helpers\Svg;
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Arab States
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     East Asia and the Pacific
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Europe and Central Asia
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Latin America and the Caribbean
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Latin America and the Caribbean
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     South Asia
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Sub-Saharan Africa
+                                    <span class="checkmark"></span>
                                 </label>
                             </div>
                         </div>
                         <div class="filter-office-type">
                             <p class="text-link">Office Type</p>
                             <div class="checkbox-group">
+                                <label class="checkbox-item">One
+                                    <input type="checkbox" checked="checked">
+                                    <span class="checkmark"></span>
+                                </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Arab States
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     East Asia and the Pacific
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Europe and Central Asia
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Latin America and the Caribbean
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Latin America and the Caribbean
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     South Asia
+                                    <span class="checkmark"></span>
                                 </label>
                                 <label class="checkbox-item">
                                     <input type="checkbox">
                                     Sub-Saharan Africa
+                                    <span class="checkmark"></span>
                                 </label>
                             </div>
                         </div>
