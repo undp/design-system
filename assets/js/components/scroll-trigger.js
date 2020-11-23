f6f3e5cc91cfda98c3c9a0debf112ae1038b960b
Kevin Chavez <kevin@maniak.com>
2020-11-23 14:30:05 -0700

    removed duplicate function

--- assets/js/components/scroll-trigger.js
+++ assets/js/components/scroll-trigger.js
@@ -12,40 +12,39 @@ const init = function () {
     });
 
     let rightSections = document.querySelectorAll(".expertise-section-right");
-
-    rightSections.forEach((target, key) => {
-        let sectionImage = target.querySelector('.image-card');
-        gsap.timeline({
-            defaults: {duration: 1},
-            scrollTrigger: {
-                trigger: target,
-                scrub: true,
-                start: "bottom bottom-=20px",
-                end: "center 10%"
-            }
-        }).fromTo(target, {y: 0}, {y: -20})
-            .fromTo(sectionImage,{y: 200}, {y: -180})
-            .from(target, {opacity: 0, duration: 0.2}, 0)
-            .to(target, {opacity: 0, duration: 1}, '-=0.5')
-    });
-
     let leftSections = document.querySelectorAll(".expertise-section-left");
 
-    leftSections.forEach(target => {
-        let sectionImage = target.querySelector('.image-card');
-        gsap.timeline({
-            defaults: {duration: 1},
-            scrollTrigger: {
-                trigger: target,
-                scrub: true,
-                start: "top bottom-=180px",
-                end: "center 35%"
-            }
-        }).fromTo(target, {y: 0}, {y: -20})
-            .fromTo(sectionImage,{y: 100}, {y: -180})
-            .from(target, {opacity: 0, duration: 0.2}, 0)
-            .to(target, {opacity: 0, duration: 1}, '-=0.5')
-    });
+    animation(rightSections, 'right');
+    animation(leftSections, 'left');
+
+    function animation(sections, side) {
+
+        let start = "";
+        let end = "";
+
+        if(side == 'right'){
+            start = "bottom bottom-=20px",
+            end = "center 10%"
+        }else{
+            start = "top bottom-=180px";
+            end = "center 35%";
+        }
+        sections.forEach(target => {
+            let sectionImage = target.querySelector('.image-card');
+            gsap.timeline({
+                defaults: {duration: 0.5},
+                scrollTrigger: {
+                    trigger: target,
+                    scrub: true,
+                    start: start,
+                    end: end
+                }
+            }).fromTo(target, {y: 0}, {y: -20})
+                .fromTo(sectionImage,{y: 100}, {y: -180})
+                .from(target, {opacity: 0, duration: 0.2}, 0)
+                .to(target, {opacity: 0, duration: 1}, '-=0.5')
+        });
+    }
 }
 
 export default init
