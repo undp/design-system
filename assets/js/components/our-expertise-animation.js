84842d52933d28505e9fd18f5f7830a89d63c136
Kevin Chavez <kevin@maniak.com>
2020-11-27 15:13:10 -0700

    removed unncesary code

--- assets/js/components/our-expertise-animation.js
+++ assets/js/components/our-expertise-animation.js
@@ -5,52 +5,43 @@ gsap.registerPlugin(ScrollTrigger)
 
 const init = function () {
 
-    let rightSections = document.querySelectorAll(".expertise-section-right");
-    let leftSections = document.querySelectorAll(".expertise-section-left");
+    let sections = document.querySelectorAll(".expertise-section");
 
-    animateExpertiseSection(rightSections, 'right');
-    animateExpertiseSection(leftSections, 'left');
-
-    function animateExpertiseSection(sections, side) {
-
-        sections.forEach(target => {
-            let sectionImage = target.querySelector('.image-card');
-             let image = $(target).find('.image-card');
-            gsap.timeline({
-                defaults: {
-                    duration: 2,
-                    ease: "slow"
+    sections.forEach(target => {
+        let image = $(target).find('.image-card');
+        gsap.timeline({
+            defaults: {
+                duration: 2,
+                ease: "slow"
+            },
+            scrollTrigger: {
+                trigger: target,
+                scrub: true,
+                start: "center bottom",
+                end: "bottom top+=300px",
+                onEnter: ()=>{
+                    if(image.length){
+                        image.addClass('visible')
+                    }
                 },
-                scrollTrigger: {
-                    trigger: target,
-                    scrub: true,
-                    start: "center bottom",
-                    end: "bottom top+=300px",
-                    markers: side == "right",
-                    onEnter: ()=>{
-                        if(image.length){
-                            image.addClass('visible')
-                        }
-                    },
-                    onLeave: ()=>{
-                        if(image.length){
-                            image.addClass('in-visible')
-                        }
-                    },
-                    onEnterBack: ()=>{
-                        if(image.length){
-                            image.removeClass('in-visible').addClass('visible')
-                        }
-                    },
-                    onLeaveBack: ()=>{
-                        if(image.length){
-                            image.removeClass('visible').removeClass('in-visible')
-                        }
+                onLeave: ()=>{
+                    if(image.length){
+                        image.addClass('in-visible')
+                    }
+                },
+                onEnterBack: ()=>{
+                    if(image.length){
+                        image.removeClass('in-visible').addClass('visible')
+                    }
+                },
+                onLeaveBack: ()=>{
+                    if(image.length){
+                        image.removeClass('visible').removeClass('in-visible')
                     }
                 }
-            })
-        });
-    }
+            }
+        })
+    });
 }
 
 export default init
