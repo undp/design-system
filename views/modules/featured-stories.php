35c244437b308b5ac25f4531e3b4799eca783250
Diego Rodríguez <diego@maniak.com.mx>
2020-12-01 23:05:43 -0600

    Hotfix: slider padding, story parallax hover

--- views/modules/featured-stories.php
+++ views/modules/featured-stories.php
@@ -37,29 +37,27 @@
                     ])
                 ?>
             </div>
-        </div>
-        <div class="grid-x grid-padding-x">
             <div class="cell medium-8 large-8">
                 <?php
-                    View::render('components/story-card', [
-                        'size' => 'large',
-                        'image' => '/assets/images/placeholder/featured-story-3.jpeg',
-                        'tag' => 'UNDP Response',
-                        'title' => 'In Costa Rica, rural women grow their own businesses',
-                        'description' => 'Working with organic agriculture and native stingless honey bee production',
-                        'cta' => 'Discover more'
-                    ])
+                View::render('components/story-card', [
+                    'size' => 'large',
+                    'image' => '/assets/images/placeholder/featured-story-3.jpeg',
+                    'tag' => 'UNDP Response',
+                    'title' => 'In Costa Rica, rural women grow their own businesses',
+                    'description' => 'Working with organic agriculture and native stingless honey bee production',
+                    'cta' => 'Discover more'
+                ])
                 ?>
             </div>
             <div class="cell small-12 medium-4 overflow-hidden">
                 <?php
-                    View::render('components/story-card', [
-                        'image' => '/assets/images/placeholder/featured-story-4.jpeg',
-                        'tag' => 'UNDP Response',
-                        'title' => '“I cry for no apparent reason.”',
-                        'description' => "UNDP launches Syria's first online mental health service",
-                        'cta' => 'Discover more'
-                    ])
+                View::render('components/story-card', [
+                    'image' => '/assets/images/placeholder/featured-story-4.jpeg',
+                    'tag' => 'UNDP Response',
+                    'title' => '“I cry for no apparent reason.”',
+                    'description' => "UNDP launches Syria's first online mental health service",
+                    'cta' => 'Discover more'
+                ])
                 ?>
             </div>
         </div>
--- views/modules/publications.php
+++ views/modules/publications.php
@@ -13,63 +13,61 @@ use helpers\View;
                 </div>
             </div>
         </div>
-        <div class="grid-x scroll-track right-left delay-1">
-            <div class="cell medium-12">
-                <div class="publications-slider">
-                    <div class="grid-x">
-                        <div class="cell large-10 large-offset-1">
-                            <div class="glide__bullets" data-glide-el="controls[nav]">
-                                <div class="control-slider"></div>
-                                <button class="glide__bullet" data-glide-dir="=0" aria-label="Slide 1">
-                                    <span class="show-for-sr">Slide 1</span>
-                                </button>
-                                <button class="glide__bullet" data-glide-dir="=1" aria-label="Slide 2">
-                                    <span class="show-for-sr">Slide 2</span>
-                                </button>
-                                <button class="glide__bullet" data-glide-dir="=2" aria-label="Slide 3">
-                                    <span class="show-for-sr">Slide 3</span>
-                                </button>
-                            </div>
+        <div class="publications-slider scroll-track right-left delay-1">
+            <div class="grid-x">
+                <div class="cell large-10 large-offset-1">
+                    <div class="bullets-container">
+                        <div class="glide__bullets" data-glide-el="controls[nav]">
+                            <div class="control-slider"></div>
+                            <button class="glide__bullet" data-glide-dir="=0" aria-label="Slide 1">
+                                <span class="show-for-sr">Slide 1</span>
+                            </button>
+                            <button class="glide__bullet" data-glide-dir="=1" aria-label="Slide 2">
+                                <span class="show-for-sr">Slide 2</span>
+                            </button>
+                            <button class="glide__bullet" data-glide-dir="=2" aria-label="Slide 3">
+                                <span class="show-for-sr">Slide 3</span>
+                            </button>
                         </div>
                     </div>
-                    <div class="glide__track" data-glide-el="track">
-                        <ul class="glide__slides">
-                            <li class="glide__slide">
-                                <?php
-                                View::render('components/publication',[
-                                    'image' => '/assets/images/placeholder/publication-1.jpg',
-                                    'tag' => 'Report',
-                                    'title' => 'Human mobility, shared opportunities',
-                                    'description' => 'This UNDP publication recommends actions for policymakers to enhance the benefits and reduce the costs of human mobility to help achieve the Sustainable Development Goals.',
-                                    'cta' => 'Read more'
-                                ])
-                                ?>
-                            </li>
-                            <li class="glide__slide">
-                                <?php
-                                View::render('components/publication',[
-                                    'image' => '/assets/images/placeholder/publication-2.jpg',
-                                    'tag' => 'Report',
-                                    'title' => 'UNDP Annual Report 2019',
-                                    'description' => 'As one of the world’s largest multilateral development agencies, present in over 170 countries and territories, UNDP is on the frontlines of anticipating, understanding and acting on today’s opportunities and risks.',
-                                    'cta' => 'Read more'
-                                ])
-                                ?>
-                            </li>
-                            <li class="glide__slide">
-                                <?php
-                                View::render('components/publication',[
-                                    'image' => '/assets/images/placeholder/publication-3.jpg',
-                                    'tag' => 'Report',
-                                    'title' => 'UNDP Issues Brief on Resilient Livelihoods Value Chains',
-                                    'description' => 'Through this thematic area, UNDP supports the advancement of climate resilient livelihoods for vulnerable communities, including resilient agricultural value chain development.',
-                                    'cta' => 'Read more'
-                                ])
-                                ?>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
+            </div>
+            <div class="glide__track" data-glide-el="track">
+                <ul class="glide__slides">
+                    <li class="glide__slide">
+                        <?php
+                        View::render('components/publication',[
+                            'image' => '/assets/images/placeholder/publication-1.jpg',
+                            'tag' => 'Report',
+                            'title' => 'Human mobility, shared opportunities',
+                            'description' => 'This UNDP publication recommends actions for policymakers to enhance the benefits and reduce the costs of human mobility to help achieve the Sustainable Development Goals.',
+                            'cta' => 'Read more'
+                        ])
+                        ?>
+                    </li>
+                    <li class="glide__slide">
+                        <?php
+                        View::render('components/publication',[
+                            'image' => '/assets/images/placeholder/publication-2.jpg',
+                            'tag' => 'Report',
+                            'title' => 'UNDP Annual Report 2019',
+                            'description' => 'As one of the world’s largest multilateral development agencies, present in over 170 countries and territories, UNDP is on the frontlines of anticipating, understanding and acting on today’s opportunities and risks.',
+                            'cta' => 'Read more'
+                        ])
+                        ?>
+                    </li>
+                    <li class="glide__slide">
+                        <?php
+                        View::render('components/publication',[
+                            'image' => '/assets/images/placeholder/publication-3.jpg',
+                            'tag' => 'Report',
+                            'title' => 'UNDP Issues Brief on Resilient Livelihoods Value Chains',
+                            'description' => 'Through this thematic area, UNDP supports the advancement of climate resilient livelihoods for vulnerable communities, including resilient agricultural value chain development.',
+                            'cta' => 'Read more'
+                        ])
+                        ?>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
