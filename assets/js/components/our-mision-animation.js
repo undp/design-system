c92e1cb5868df2bcd9d9f9a435568d692985fc5b
Kevin Chavez <kevin@maniak.com>
2020-12-04 09:10:49 -0700

    adjusted resize function on missions section, adjust css values for mobile and tablet

--- assets/js/components/our-mision-animation.js
+++ assets/js/components/our-mision-animation.js
@@ -1,29 +1,36 @@
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
+let _ = require('lodash')
 
 gsap.registerPlugin(ScrollTrigger)
 
 const init = function () {
-    const $section = $(".our-mission")
-    const $startTrigger = $section.find('.title-container');
-
-    //white to blue
-    const whiteToBlue = gsap.fromTo($section,
-        { backgroundColor: "#fff" },
-        { backgroundColor: "#232E3E" })
+    let $section = $(".our-mission")
+    let $startTrigger = $section.find('.title-container');
 
     let startWhiteToBlue = "top-=700px top+=105px"
     let endWhiteToBlue = "+=400px"
     let startBlueToWhite = "bottom top+=200px"
+    let whiteToBlueScroll = null,
+        blueToWhiteScroll = null,
+        whiteToBlue = null,
+        blueToWhite = null
 
 
-    function calculateSizes(){
-        if($(window).width() < 1194){
+    function calculateSizes() {
+
+        if ($(window).width() > 1194) {
+            startWhiteToBlue = "top-=700px top+=105px"
+            endWhiteToBlue = "+=400px"
+            startBlueToWhite = "bottom top+=200px"
+        }
+
+        if ($(window).width() < 1194) {
             startWhiteToBlue = "top-=600px top+=70px"
             endWhiteToBlue = "+=400px"
         }
 
-        if($(window).width() < 834){
+        if ($(window).width() < 834) {
             startWhiteToBlue = "top-=500px top+=70px"
             endWhiteToBlue = "+=400px"
 
@@ -33,32 +40,41 @@ const init = function () {
 
     calculateSizes()
 
-    let whiteToBlueScroll = ScrollTrigger.create({
-        trigger: $startTrigger,
-        start: startWhiteToBlue,
-        end: endWhiteToBlue,
-        scrub: true,
-        animation: whiteToBlue,
-        invalidateOnRefresh: false
-    });
+    function createScrollsWhiteToBlue(){
+        whiteToBlue = gsap.fromTo($section,
+            { backgroundColor: "#fff" },
+            { backgroundColor: "#232E3E" })
 
+        whiteToBlueScroll = ScrollTrigger.create({
+            trigger: $startTrigger,
+            start: startWhiteToBlue,
+            end: endWhiteToBlue,
+            scrub: true,
+            animation: whiteToBlue,
+            invalidateOnRefresh: false,
+            markers: true,
+        });
+    }
 
-    //blue to white
-    const blueToWhite= gsap.fromTo($section,
-        { backgroundColor: "#232E3E" },
-        { backgroundColor: "#fff",
-            immediateRender: false })
+    function createScrollsBlueToWhite(){
+        //blue to white
+        blueToWhite = gsap.fromTo($section,
+            { backgroundColor: "#232E3E" },
+            { backgroundColor: "#fff",
+                immediateRender: false })
 
+        blueToWhiteScroll = ScrollTrigger.create({
+            trigger: $startTrigger,
+            start: startBlueToWhite,
+            end: "+=200px",
+            scrub: true,
+            animation: blueToWhite,
+            invalidateOnRefresh: false
+        });
+    }
 
-    let blueToWhiteScroll = ScrollTrigger.create({
-        trigger: $startTrigger,
-        start: startBlueToWhite,
-        end: "+=200px",
-        scrub: true,
-        animation: blueToWhite,
-        invalidateOnRefresh: false
-    });
-
+    createScrollsWhiteToBlue()
+    createScrollsBlueToWhite()
 
     //appear expertise section
     let appearScroll = ScrollTrigger.create({
@@ -74,12 +90,16 @@ const init = function () {
         },
     });
 
-    $(window).resize(()=>{
+    $(window).resize('resize',_.debounce(() => {
+        whiteToBlue.kill();
+        blueToWhite.kill();
+        whiteToBlueScroll.kill()
+        blueToWhiteScroll.kill()
+
         calculateSizes()
-        whiteToBlueScroll.update()
-        blueToWhiteScroll.update()
-        appearScroll.update()
-    })
+        createScrollsWhiteToBlue()
+        createScrollsBlueToWhite()
+    }, 200))
 }
 
 export default init
