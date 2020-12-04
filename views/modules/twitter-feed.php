5d5f5bcf08252a8f731067d0abc82157fa2496e4
Diego Rodríguez <diego@maniak.com.mx>
2020-12-04 08:38:42 -0600

    Remove twitter title

--- views/modules/twitter-feed.php
+++ views/modules/twitter-feed.php
@@ -4,16 +4,6 @@ use helpers\View;
 <div class="twitter-feed">
     <div class="grid-container">
         <div class="grid-container">
-            <div class="grid-x">
-                <div class="cell small-offset-1 small-11 medium-offset-1 medium-11">
-                    <div class="section-title scroll-track left-right delay-1 regular">
-                        <h2 class="heading h2">
-                            @UNDP
-                        </h2>
-                        <img src="/assets/images/icon-twitter-white.png" alt="">
-                    </div>
-                </div>
-            </div>
             <div class="grid-x grid-padding-x tweets">
                 <div class="cell small-12 medium-3 scroll-track left-right delay-2 regular">
                     <?php View::render('components/tweet', [
