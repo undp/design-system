f77266d47e5e1f7114c3b00c0fcb3b2de1d7c15c
Kevin Chavez <kevin@maniak.com>
2020-12-07 11:29:49 -0700

    removed console log

--- assets/js/components/publications-slider.js
+++ assets/js/components/publications-slider.js
@@ -38,7 +38,6 @@ const init = function () {
         });
 
         $glideTrack.click(function(e) {
-            console.log(!$(e.target).is(':button'));
             if(!$(e.target).is(':button')){
                 if ((e.pageX - this.offsetLeft) < $(this).width() / 2) {
                     glide.go('<')
