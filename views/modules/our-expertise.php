b1c79c66661c6aafe2f04ccff557ac99082895c8
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2020-12-10 17:12:14 -0600

    removed unnecessary class

--- views/modules/our-expertise.php
+++ views/modules/our-expertise.php
@@ -21,7 +21,7 @@ use helpers\View;
                 ?>
             </div>
             <div class="cell small-10 medium-3 expertise-section-right">
-                <div class="expertise-image lazy">
+                <div class="expertise-image">
                     <div class="background-video hide-for-small-only">
                         <video class="lazy" data-src="https://player.vimeo.com/external/477328671.sd.mp4?s=802e70ad5a37b78d49301875e78f4c36c15418a0&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                     </div>
@@ -35,7 +35,7 @@ use helpers\View;
         </div>
         <div class="grid-x our-expertise-row">
             <div class="cell small-10 medium-3 expertise-section-left">
-                <div class="expertise-image lazy">
+                <div class="expertise-image">
                     <div class="background-video hide-for-small-only">
                         <video class="lazy" data-src="https://player.vimeo.com/external/478247383.sd.mp4?s=94744ea0dcaed3ec3f7a51dd1fc9ba4705ccc9df&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                     </div>
@@ -67,7 +67,7 @@ use helpers\View;
                 ?>
             </div>
             <div class="cell small-10 medium-3 expertise-section-right">
-                <div class="expertise-image lazy">
+                <div class="expertise-image">
                     <div class="background-video hide-for-small-only">
                         <video class="lazy" data-src="https://player.vimeo.com/external/478247389.sd.mp4?s=b3b51d67a8018dae0e0cacc8bb0b18fa2c7e9b4a&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                     </div>
@@ -81,7 +81,7 @@ use helpers\View;
         </div>
         <div class="grid-x our-expertise-row">
             <div class="cell small-10 medium-3 expertise-section-left">
-                <div class="expertise-image lazy">
+                <div class="expertise-image">
                     <div class="background-video hide-for-small-only">
                         <video class="lazy" data-src="https://player.vimeo.com/external/478247434.sd.mp4?s=1e617d39a7b53c5d3a7b47d8a7516f534effdb5b&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                     </div>
@@ -113,7 +113,7 @@ use helpers\View;
                 ?>
             </div>
             <div class="cell small-10 medium-3 expertise-section-right">
-                <div class="expertise-image lazy">
+                <div class="expertise-image">
                     <div class="background-video hide-for-small-only">
                         <video class="lazy" data-src="https://player.vimeo.com/external/478247411.sd.mp4?s=908b276285cd8792b5d7614afcac51f040daac07&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                     </div>
@@ -186,7 +186,7 @@ use helpers\View;
                         ?>
                     </div>
                     <div class="cell small-11 medium-3 expertise-section-image">
-                        <div class="expertise-image lazy">
+                        <div class="expertise-image">
                             <div class="background-video hide-for-small-only">
                                 <video class="lazy" data-src="https://player.vimeo.com/external/477328671.sd.mp4?s=802e70ad5a37b78d49301875e78f4c36c15418a0&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                             </div>
@@ -200,7 +200,7 @@ use helpers\View;
                 </div>
                 <div class="grid-x our-expertise-row glide__slide">
                     <div class="cell small-11 medium-3 expertise-section-image">
-                        <div class="expertise-image lazy">
+                        <div class="expertise-image">
                             <div class="background-video hide-for-small-only">
                                 <video class="lazy" data-src="https://player.vimeo.com/external/478247383.sd.mp4?s=94744ea0dcaed3ec3f7a51dd1fc9ba4705ccc9df&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                             </div>
@@ -232,7 +232,7 @@ use helpers\View;
                         ?>
                     </div>
                     <div class="cell small-11 medium-3 expertise-section-image">
-                        <div class="expertise-image lazy">
+                        <div class="expertise-image">
                             <div class="background-video hide-for-small-only">
                                 <video class="lazy" data-src="https://player.vimeo.com/external/478247389.sd.mp4?s=b3b51d67a8018dae0e0cacc8bb0b18fa2c7e9b4a&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                             </div>
@@ -246,7 +246,7 @@ use helpers\View;
                 </div>
                 <div class="grid-x our-expertise-row glide__slide">
                     <div class="cell small-11 medium-3 expertise-section-image">
-                        <div class="expertise-image lazy">
+                        <div class="expertise-image">
                             <div class="background-video hide-for-small-only">
                                 <video class="lazy" data-src="https://player.vimeo.com/external/478247434.sd.mp4?s=1e617d39a7b53c5d3a7b47d8a7516f534effdb5b&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                             </div>
@@ -278,7 +278,7 @@ use helpers\View;
                         ?>
                     </div>
                     <div class="cell small-11 medium-3 expertise-section-image">
-                        <div class="expertise-image lazy">
+                        <div class="expertise-image">
                             <div class="background-video hide-for-small-only">
                                 <video class="lazy" data-src="https://player.vimeo.com/external/478247411.sd.mp4?s=908b276285cd8792b5d7614afcac51f040daac07&profile_id=164" autoplay muted loop playsinline aria-hidden="true"></video>
                             </div>
