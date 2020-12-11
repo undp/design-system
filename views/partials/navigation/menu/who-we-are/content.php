f3edae8a4703b3d869a1dc1824f9876a43e79816
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2020-12-11 12:56:42 -0600

    included submenus on section "who we are"

--- views/partials/navigation/menu/who-we-are/content.php
+++ views/partials/navigation/menu/who-we-are/content.php
@@ -1,3 +1,7 @@
+<?php
+use helpers\Svg;
+?>
+
 <div id="content-about-us" class="grid-x modal-nav-content">
     <div class="cell large-auto content-text">
         <h2 class="title">About us</h2>
@@ -6,6 +10,20 @@
             exclusion, and build resilience so countries can sustain progress. As the UN’s development agency, UNDP
             plays a critical role in helping countries achieve the Sustainable Development Goals.
         </p>
+        <div class="links">
+            <a class="text-link arrow-1" href="#">
+                Results at a glance
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Organizational chart
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                FAQs
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+        </div>
     </div>
     <div class="cell large-auto flex-container align-right content-image">
         <img class="image" src="../../assets/images/placeholder/nav-hover/undp-africa-field-staff.jpg"
@@ -21,6 +39,12 @@
             General Assembly confirmed his nomination on 19 April 2017, following his nomination by Secretary-General
             António Guterres.
         </p>
+        <div class="links">
+            <a class="text-link arrow-1" href="#">
+                Past administrations
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+        </div>
     </div>
     <div class="cell large-auto flex-container align-right content-image">
         <img class="image" src="../../assets/images/placeholder/nav-hover/undp-nyc-2019-achim-steiner.jpg"
@@ -35,6 +59,44 @@
             The Executive Board provides inter-governmental support to and supervision of UNDP activities, ensuring that
             the organization remains responsive to the evolving needs of programme countries.
         </p>
+        <div class="links">
+            <a class="text-link arrow-1" href="#">
+                Information note on the Executive Board
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                The Bureau
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Membership
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Session dates
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Secretariat of the Board
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Decisions of the Board
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Reports on sessions
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Documents for sessions
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Additional resources
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+        </div>
     </div>
     <div class="cell large-auto flex-container align-right content-image">
         <img class="image" src="../../assets/images/placeholder/nav-hover/undp-nyc-2020-executive-board.jpg"
@@ -50,6 +112,44 @@
             substantial expertise—both intellectual acumen and practical experience—cuts across diverse
             development issues and settings.
         </p>
+        <div class="links">
+            <a class="text-link arrow-1" href="#">
+                Governments
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                United Nations System
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                International Financial Institutions
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Islamic Finance
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Private Sector
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Foundations
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Civil Society Organizations
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Goodwill Ambassadors and Advocates
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Open Dialogue Series
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+        </div>
     </div>
     <div class="cell large-auto flex-container align-right content-image">
         <img class="image" src="../../assets/images/placeholder/nav-hover/undp-papp-2019-partnership-japan-football.jpg"
@@ -65,6 +165,21 @@
             multilateral organizations, private sector and other sources, in the form of unrestricted regular resources
             (core), and contributions earmarked for a specific theme, programme or project.
         </p>
+        <div class="links">
+            <a class="text-link arrow-1" href="#">
+                Funding windows
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Regular resources contributors
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Structured funding dialogues
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+        </div>
+
     </div>
     <div class="cell large-auto flex-container align-right content-image">
         <img class="image" src="../../assets/images/placeholder/nav-hover/annual-report-result-page-vertica.jpg"
@@ -79,6 +194,36 @@
             Accountability is the obligation to demonstrate that work has been conducted in accordance with agreed rules
             and standards and report fairly and accurately on performance results vis-à-vis mandated roles and/or plans.
         </p>
+        <div class="links">
+            <a class="text-link arrow-1" href="#">
+                Audit and Investigations
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Ethics
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Evaluation
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Transparency
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Policies and procedures
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Prevention and Response to Sexual Misconduct
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+            <a class="text-link arrow-1" href="#">
+                Social and Environmental Responsibility
+                <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
+            </a>
+        </div>
     </div>
     <div class="cell large-auto flex-container align-right content-image">
         <img class="image" src="../../assets/images/placeholder/nav-hover/transparency-portal-screen-shot-2020.jpg" alt="undp transparency">
