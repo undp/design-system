49b523956c3cb1543f3d98e612d7fcab449dc154
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2020-12-14 12:22:52 -0600

    improve submenus

--- views/partials/navigation/menu/who-we-are/content.php
+++ views/partials/navigation/menu/who-we-are/content.php
@@ -202,36 +202,7 @@ use helpers\Svg;
             <a class="text-link arrow-1" href="#">
                 Ethics
                 <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
-            </a><div class="links">
-                <a class="text-link arrow-1" href="#">
-                    Audit and Investigations
-                    <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
-                </a>
-                <a class="text-link arrow-1" href="#">
-                    Ethics
-                    <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
-                </a>
-                <a class="text-link arrow-1" href="#">
-                    Evaluation
-                    <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
-                </a>
-                <a class="text-link arrow-1" href="#">
-                    Transparency
-                    <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
-                </a>
-                <a class="text-link arrow-1" href="#">
-                    Policies and procedures
-                    <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
-                </a>
-                <a class="text-link arrow-1" href="#">
-                    Prevention and Response to Sexual Misconduct
-                    <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
-                </a>
-                <a class="text-link arrow-1" href="#">
-                    Social and Environmental Responsibility
-                    <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
-                </a>
-            </div>
+            </a>
             <a class="text-link arrow-1" href="#">
                 Evaluation
                 <?php Svg::render('btn-arrow-red', false, '', 'assets/images/arrows/')?>
