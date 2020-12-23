b3d455aff1eb670f8f6d0c7dd341435fd5f59935
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2020-12-22 20:04:37 -0600

    include data from quieres to our expertise on mobile

--- views/modules/our-expertise.php
+++ views/modules/our-expertise.php
@@ -1,6 +1,8 @@
 <?php
+
 use helpers\View;
 use core\Queries;
+
 $queries = new Queries();
 $dataOurExpertise = $queries->getOurExpertise();
 ?>
@@ -40,74 +42,25 @@ $dataOurExpertise = $queries->getOurExpertise();
                 </div>
             </div>
         </div>
-<!--        <div class="glide__bullets" data-glide-el="controls[nav]">-->
-<!--            <div class="control-slider"></div>-->
-<!--            --><?php //foreach ($data as $index => $item) { ?>
-<!--                <button class="glide__bullet" data-glide-dir="=--><?//= $index + 1 ?><!--">-->
-<!--                    <span class="show-for-sr">Slide --><?//= $index + 1 ?><!--</span>-->
-<!--                </button>-->
-<!--            --><?php //} ?>
-<!--        </div>-->
+        <div class="glide__bullets" data-glide-el="controls[nav]">
+            <div class="control-slider"></div>
+            <?php foreach ($dataOurExpertise as $index => $item) { ?>
+                <button class="glide__bullet" data-glide-dir="=<?= $index + 1 ?>">
+                    <span class="show-for-sr">Slide <?= $index + 1 ?></span>
+                </button>
+            <?php } ?>
+        </div>
         <div class="glide__track" data-glide-el="track">
             <div class="glide__slides">
                 <?php
-                foreach ($data as $index => $item) {
-                    if ($index % 2 == 0) {
-                        ?>
-                        <div class="grid-x our-expertise-row glide__slide">
-                            <div class="cell small-11 medium-3 expertise-section-text">
-                                <?php
-                                View::render('components/expertise-card', [
-                                    'title' => $item['title'],
-                                    'description' => $item['description'],
-                                    'cta' => 'Read more'
-                                ])
-                                ?>
-                            </div>
-                            <div class="cell small-11 medium-3 expertise-section-image">
-                                <div class="expertise-image">
-                                    <div class="background-video hide-for-small-only">
-                                        <video class="lazy"
-                                               data-src="<?= $item['videoUrl'] ?? '' ?>"
-                                               autoplay muted loop playsinline aria-hidden="true"></video>
-                                    </div>
-                                    <?php
-                                    View::render('components/image-card', [
-                                        'route' => $item['imageUrl'] ?? ''
-                                    ])
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                        <?php
-                    } else { ?>
-                        <div class="grid-x our-expertise-row glide__slide">
-                            <div class="cell small-11 medium-3 expertise-section-image">
-                                <div class="expertise-image">
-                                    <div class="background-video hide-for-small-only">
-                                        <video class="lazy"
-                                               data-src="<?= $item['videoUrl'] ?? '' ?>"
-                                               autoplay muted loop playsinline aria-hidden="true"></video>
-                                    </div>
-                                    <?php
-                                    View::render('components/image-card', [
-                                        'route' => $item['imageUrl'] ?? ''
-                                    ])
-                                    ?>
-                                </div>
-                            </div>
-                            <div class="cell small-11 medium-3 expertise-section-text">
-                                <?php
-                                View::render('components/expertise-card', [
-                                    'title' => $item['title'],
-                                    'description' => $item['description'],
-                                    'cta' => 'Read more'
-                                ])
-                                ?>
-                            </div>
-                        </div>
-                        <?php
-                    }
+                foreach ($dataOurExpertise as $index => $item) {
+                    echo View::render('modules/our-expertise-mobile-slider-items', [
+                        'title' => $item->title,
+                        'description' => $item->description,
+                        'videoUrl' => $item->videoUrl,
+                        'imageUrl' => $item->imageUrl,
+                        'index' => $index,
+                    ]);
                 }
                 ?>
             </div>
