1288d8d262f631ad546554735137e154e55b7f94
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2021-01-04 17:25:14 -0600

    updated routes

--- views/partials/navigation/menu/main.php
+++ views/partials/navigation/menu/main.php
@@ -1,7 +1,7 @@
 <?php
     use helpers\View;
 ?>
-<?php View::render('partials/navigation/menu/who-we-are/main') ?>
-<?php View::render('partials/navigation/menu/what-we-do/main') ?>
-<?php View::render('partials/navigation/menu/our-impact/main') ?>
-<?php View::render('partials/navigation/menu/get-involved/main') ?>
+<?php View::render('partials/navigation/menu/who-we-are') ?>
+<?php View::render('partials/navigation/menu/what-we-do') ?>
+<?php View::render('partials/navigation/menu/our-impact') ?>
+<?php View::render('partials/navigation/menu/get-involved') ?>
