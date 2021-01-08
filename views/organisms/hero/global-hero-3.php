a211e650be98e01f6437ebe2c04f6c966bcac85c
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2021-01-08 01:24:10 -0600

    included breadcrumb as partials

--- views/organisms/hero/global-hero-3.php
+++ views/organisms/hero/global-hero-3.php
@@ -1,11 +1,21 @@
 <?php
 use helpers\Svg;
+use helpers\View;
 ?>
 
 <section class="global-hero hero-3"
          style="background-image: url('../../../assets/images/placeholder/hero/global-hero-3.png')">
-    <div class="hero-3-title">The SDGs In ACTION.</div>
-    <div class="cta-arrow show-for-medium">
-        <?php Svg::render('hero-arrow-down', true, 'Scroll Down', 'assets/images/arrows/')?>
+    <div class="grid-container">
+        <div class="grid-x">
+            <div class="cell">
+                <?php View::render('partials/breadcrumb', [
+                    'links' => [(object)['name' => 'About', 'link' => '#']]
+                ]) ?>
+                <div class="hero-3-title">The SDGs In ACTION.</div>
+                <div class="cta-arrow show-for-large">
+                    <?php Svg::render('hero-arrow-down', true, 'Scroll Down', 'assets/images/arrows/') ?>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
