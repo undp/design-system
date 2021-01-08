ae390db812d2df34813b031c1905917354889d55
Kevin Chavez <kevin@maniak.com>
2021-01-07 17:41:06 -0700

    markup and style for sdg cards

---
+++ views/molecules/statistics-figures/sdg-card.php
@@ -0,0 +1,10 @@
+<!--<div class="sdg-card-container">-->
+    <a href="#" class="sdg-card">
+        <div class="number">1</div>
+        <div class="title">Responsible Consumption and Production</div>
+        <div class="hover-image">
+            <img src="/assets/images/placeholder/sdg/sdg-background.png" alt="SDG background">
+            <div class="gradient"></div>
+        </div>
+    </a>
+<!--</div>-->
--- views/pages/the-sdgs.php
+++ views/pages/the-sdgs.php
@@ -7,10 +7,10 @@ use helpers\View;
 <?php View::render('layout/header', ['pageTitle' => 'UNDP - The SDGS']) ?>
 
 <!-- Navigation -->
-<?php View::render('layout/navigation/main') ?>
-
+<?php //View::render('layout/navigation/main') ?>
+<?php View::render('molecules/statistics-figures/sdg-card') ?>
 <!-- Footer -->
-<?php View::render('layout/footer'); ?>
+<?php //View::render('layout/footer'); ?>
 <script type="text/javascript" src="/dist/app.js"></script>
 </body>
 </html>
