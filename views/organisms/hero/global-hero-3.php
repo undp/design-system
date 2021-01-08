f13a7c99dcf79285f49d5beebb9eb1e939067037
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2021-01-07 23:58:42 -0600

    markup and stules for global hero 3

---
+++ views/organisms/hero/global-hero-3.php
@@ -0,0 +1,11 @@
+<?php
+use helpers\Svg;
+?>
+
+<section class="global-hero hero-3"
+         style="background-image: url('../../../assets/images/placeholder/hero/global-hero-3.png')">
+    <div class="hero-3-title">The SDGs In ACTION.</div>
+    <div class="cta-arrow show-for-medium">
+        <?php Svg::render('hero-arrow-down', true, 'Scroll Down', 'assets/images/arrows/')?>
+    </div>
+</section>
--- views/pages/sdgs.php
+++ views/pages/sdgs.php
@@ -10,10 +10,14 @@ use helpers\View;
 <!-- Navigation -->
 <?php View::render('layout/navigation/main') ?>
 
+<!--Global hero 3-->
+<?php View::render('organisms/hero/global-hero-3') ?>
+
 <!-- SDG Cards list -->
 <?php
     View::render('molecules/statistics-figures/sdg-list');
 ?>
+
 <!-- Footer -->
 <?php View::render('layout/footer'); ?>
 <script type="text/javascript" src="/dist/app.js"></script>
