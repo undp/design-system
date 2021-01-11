6ae3ff3fdd0a792cd53e49e4aa20b18a5454916e
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2021-01-11 16:35:12 -0600

    improve code

---
+++ assets/js/classes/ModalSdgs.js
@@ -0,0 +1,59 @@
+class ModalSdgs {
+    constructor() {
+        this.$body = $('body')
+        this.$modal = $('#modal-sdgs')
+        this.$openModal = $('.sdg-card')
+        this.$modalBtnClose = this.$modal.find('.btn-close')
+        this.$modalContent = this.$modal.find('.modal-content')
+
+        this.url = null;
+        this.classBodyModalOpen = 'modal-open'
+    }
+
+    init() {
+        this.listenerLoadModalData();
+        this.listenerCloseModal();
+    }
+
+    listenerLoadModalData() {
+        this.$openModal.click((current) => {
+            this.url = $(current.currentTarget).data('url');
+            this.ajax();
+        })
+    }
+
+    listenerCloseModal() {
+        this.$modalBtnClose.click(() => {
+            this.close();
+        })
+    }
+
+    open() {
+        this.$modal.removeClass('hide');
+        this.$body.addClass(this.classBodyModalOpen)
+    }
+
+    close() {
+        this.$modal.addClass('hide');
+        this.$modalContent.html('');
+        this.$body.removeClass(this.classBodyModalOpen)
+    }
+
+    ajax() {
+        $.ajax({
+            type: 'GET',
+            url: this.url,
+            dataType: 'json',
+            success: ((response) => {
+                this.$modalContent.append(`
+                    <h2 class="heading h2">${ response.title }</h2>
+                    <div class="${ response.color }"></div>
+                `)
+
+                //open modal after load data
+                this.open();
+            })
+        })
+    }
+}
+export default ModalSdgs;
