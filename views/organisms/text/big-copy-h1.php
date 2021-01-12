acf4985577f81a42f5efa85689a0e43341760b4c
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2021-01-12 13:02:20 -0600

    included class for animations on big copy

--- views/organisms/text/big-copy-h1.php
+++ views/organisms/text/big-copy-h1.php
@@ -1,4 +1,4 @@
-<div class="grid-x sustainable-developments">
+<div class="grid-x sustainable-developments scroll-track left-right delay-2">
     <div class="cell small-9 medium-3 small-offset-1 heading h2"><?= $title ?? '' ?></div>
     <div class="cell small-10 medium-6 small-offset-1 large-copy"><?= $description ?? '' ?></div>
 </div>
