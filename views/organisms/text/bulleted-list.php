06c6b8ff11f047d83f73141ed558e55065fbf882
Kevin Chavez <kevin@maniak.com>
2021-01-11 18:18:21 -0700

    markup and style for bulleted lst

---
+++ views/organisms/text/bulleted-list.php
@@ -0,0 +1,37 @@
+<div class="grid-container">
+    <div class="grid-x bulleted-list">
+        <div class="cell medium-offset-1 medium-3">
+            <div class="title">
+                <h2 class="heading h2">Goal Targets</h2>
+            </div>
+        </div>
+        <div class="cell medium-offset-1 medium-5">
+            <ul class="list">
+                <li>
+                    By 2030, end hunger and ensure access by all people, in particular the poor and people in vulnerable situations, including infants, to safe, nutritious and sufficient food all year round
+                </li>
+                <li>
+                    By 2030, end all forms of malnutrition, including achieving, by 2025, the internationally agreed targets on stunting and wasting in children under 5 years of age, and address the nutritional needs of adolescent girls, pregnant and lactating women and older persons
+                </li>
+                <li>
+                    By 2030, double the agricultural productivity and incomes of small-scale food producers, in particular women, indigenous peoples, family farmers, pastoralists and fishers, including through secure and equal access to land, other productive resources and inputs, knowledge, financial services, markets and opportunities for value addition and non-farm employment
+                </li>
+                <li>
+                    By 2030, ensure sustainable food production systems and implement resilient agricultural practices that increase productivity and production, that help maintain ecosystems, that strengthen capacity for adaptation to climate change, extreme weather, drought, flooding and other disasters and that progressively improve land and soil quality
+                </li>
+                <li>
+                    By 2020, maintain the genetic diversity of seeds, cultivated plants and farmed and domesticated animals and their related wild species, including through soundly managed and diversified seed and plant banks at the national, regional and international levels, and promote access to and fair and equitable sharing of benefits arising from the utilization of genetic resources and associated traditional knowledge, as internationally agreed
+                </li>
+                <li>
+                    Increase investment, including through enhanced international cooperation, in rural infrastructure, agricultural research and extension services, technology development and plant and livestock gene banks in order to enhance agricultural productive capacity in developing countries, in particular least developed countries
+                </li>
+                <li>
+                    Correct and prevent trade restrictions and distortions in world agricultural markets, including through the parallel elimination of all forms of agricultural export subsidies and all export measures with equivalent effect, in accordance with the mandate of the Doha Development Round
+                </li>
+                <li>
+                    Adopt measures to ensure the proper functioning of food commodity markets and their derivatives and facilitate timely access to market information, including on food reserves, in order to help limit extreme food price volatility.
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
