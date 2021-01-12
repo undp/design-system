a920be450134ced873b1ec1c153bf947da93e288
Kevin Chavez <kevin@maniak.com>
2021-01-12 09:35:45 -0700

    markup and style for single content card with accent color

---
+++ views/molecules/cards/single-content-card-accent-color.php
@@ -0,0 +1,18 @@
+<div class="grid-x">
+    <div class="cell medium-3">
+        <div class="single-content-card-accent-color accent-1">
+            <div class="card-content">
+                <div class="tag">Content tag</div>
+                <div class="title">
+                    EIF and UNDP to strengthen cooperation for sustainable finance.
+                </div>
+                <div class="cta">
+                    <div class="text-link arrow-2">
+                        <span>Read more</span>
+                        <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
