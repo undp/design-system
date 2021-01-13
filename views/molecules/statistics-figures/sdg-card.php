f5503311353e44ace18582148a73615aadce4a35
Kevin Chavez <kevin@maniak.com>
2021-01-13 16:10:53 -0700

    added trigger for sdg cards

--- views/molecules/statistics-figures/sdg-card.php
+++ views/molecules/statistics-figures/sdg-card.php
@@ -1,6 +1,6 @@
-<button class="sdg-card  <?= $class ?>" data-color="<?= $class ?? '' ?>" data-url="<?= $urlData ?? ''?>">
-    <span class="number scroll-track left-right delay-2"><?= $number ?></span>
-    <span class="title scroll-track left-right delay-2"><?= $title ?></span>
+<button class="sdg-card <?= $class ?>" data-color="<?= $class ?? '' ?>" data-url="<?= $urlData ?? ''?>">
+    <span class="number track left-right delay-1"><?= $number ?></span>
+    <span class="title track left-right delay-2"><?= $title ?></span>
     <span class="hover-image">
         <img src="<?= $image ?>" alt="<?= $imageAlt ?>">
         <span class="gradient"></span>
