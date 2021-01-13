29b910dbc0c04718335e30b06d6a547bb4a24d93
Kevin Chavez <kevin@maniak.com>
2021-01-12 18:20:56 -0700

    update image to background image

--- views/molecules/cards/single-content-card-image.php
+++ views/molecules/cards/single-content-card-image.php
@@ -4,74 +4,7 @@
             <div class="single-content-card-image">
                 <div class="card-content">
                     <div class="tag">Content tag</div>
-                    <div class="image">
-                        <img src="/assets/images/placeholder/cards/single-content-card-image.png" alt="Single content card">
-                        <div class="gradient"></div>
-                    </div>
-                    <div class="title">
-                        <h4 class="heading h5">
-                            A vineyard with a storied past receives an injection of new life.
-                        </h4>
-                    </div>
-                    <div class="cta">
-                        <div class="text-link arrow-2">
-                            <span>Read more</span>
-                            <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="cell small-12 medium-6 large-3 flex-container align-center">
-            <div class="single-content-card-image">
-                <div class="card-content">
-                    <div class="tag">Content tag</div>
-                    <div class="image">
-                        <img src="/assets/images/placeholder/cards/single-content-card-image.png" alt="Single content card">
-                        <div class="gradient"></div>
-                    </div>
-                    <div class="title">
-                        <h4 class="heading h5">
-                            A vineyard with a storied past receives an injection of new life. A vineyard with a storied past receives an injection of new life.
-                        </h4>
-                    </div>
-                    <div class="cta">
-                        <div class="text-link arrow-2">
-                            <span>Read more</span>
-                            <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="cell small-12 medium-6 large-3 flex-container align-center">
-            <div class="single-content-card-image">
-                <div class="card-content">
-                    <div class="tag">Content tag</div>
-                    <div class="image">
-                        <img src="/assets/images/placeholder/cards/single-content-card-image.png" alt="Single content card">
-                        <div class="gradient"></div>
-                    </div>
-                    <div class="title">
-                        <h4 class="heading h5">
-                            A vineyard with a storied past receives an injection of new life.
-                        </h4>
-                    </div>
-                    <div class="cta">
-                        <div class="text-link arrow-2">
-                            <span>Read more</span>
-                            <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="cell small-12 medium-6 large-3 flex-container align-center">
-            <div class="single-content-card-image">
-                <div class="card-content">
-                    <div class="tag">Content tag</div>
-                    <div class="image">
-                        <img src="/assets/images/placeholder/cards/single-content-card-image.png" alt="Single content card">
+                    <div class="image" style="background-image: url('/assets/images/placeholder/cards/single-content-card-image.png')">
                         <div class="gradient"></div>
                     </div>
                     <div class="title">
