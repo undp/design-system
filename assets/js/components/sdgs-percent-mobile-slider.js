80e6498321f526685365a9ec86ae78eca522537e
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2021-01-13 09:52:03 -0600

    implemented slider on sdgs percent cards

---
+++ assets/js/components/sdgs-percent-mobile-slider.js
@@ -0,0 +1,25 @@
+import Glide from '@glidejs/glide/';
+
+const init = function () {
+    const $glideExpertiseSection = $('.glide-sdgs-percent')
+
+    if($glideExpertiseSection.length){
+        const $controlSlider = $glideExpertiseSection.find('.control-slider')
+        const numberOfSlides = $glideExpertiseSection.find('.glide__slide').length
+        const slideWidth = 100/numberOfSlides;
+
+        $controlSlider.css('width', slideWidth + "%")
+
+        const glideExpertise = new Glide('.glide-sdgs-percent', {
+            peek: { before: 0, after: 40 }
+        });
+
+        glideExpertise.on(['mount.after', 'run'], () => {
+            $controlSlider.css('left', (glideExpertise.index*slideWidth) + "%")
+        })
+
+        glideExpertise.mount();
+    }
+}
+
+export default init
