ff76d5153f9070df08a52597fd1a8d6dfb54338f
Kevin Chavez <kevin@maniak.com>
2021-01-12 18:21:27 -0700

    remove grid html

--- views/molecules/cards/single-content-card-image.php
+++ views/molecules/cards/single-content-card-image.php
@@ -1,24 +1,18 @@
-<div class="grid-container" style="max-width: 1440px">
-    <div class="grid-x grid-padding-x">
-        <div class="cell small-12 medium-6 large-3 flex-container align-center">
-            <div class="single-content-card-image">
-                <div class="card-content">
-                    <div class="tag">Content tag</div>
-                    <div class="image" style="background-image: url('/assets/images/placeholder/cards/single-content-card-image.png')">
-                        <div class="gradient"></div>
-                    </div>
-                    <div class="title">
-                        <h4 class="heading h5">
-                            A vineyard with a storied past receives an injection of new life.
-                        </h4>
-                    </div>
-                    <div class="cta">
-                        <div class="text-link arrow-2">
-                            <span>Read more</span>
-                            <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
-                        </div>
-                    </div>
-                </div>
+<div class="single-content-card-image">
+    <div class="card-content">
+        <div class="tag">Content tag</div>
+        <div class="image" style="background-image: url('/assets/images/placeholder/cards/single-content-card-image.png')">
+            <div class="gradient"></div>
+        </div>
+        <div class="title">
+            <h4 class="heading h5">
+                A vineyard with a storied past receives an injection of new life.
+            </h4>
+        </div>
+        <div class="cta">
+            <div class="text-link arrow-2">
+                <span>Read more</span>
+                <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
             </div>
         </div>
     </div>
