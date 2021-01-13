6101f5117baef42384c283afcdbc9f3baeda9596
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2021-01-13 08:06:31 -0600

    included percent cards on modal sdgs

--- views/partials/modal-sdgs.php
+++ views/partials/modal-sdgs.php
@@ -15,36 +15,36 @@ use helpers\Svg;
             </div>
             <div class="cell medium-10  height-full modal-content">
                 <?php View::render('organisms/hero/sdg-deep-dive-hero') ?>
-                <div class="grid-x grid-padding-x cards-slider">
-                    <div class="cell medium-2 medium-offset-1">
+                <div class="grid-x grid-padding-x cards-slider hide-for-small-only scroll-track right-left delay-2">
+                    <div class="cell auto">
                         <?php View::render('molecules/statistics-figures/stat-card',[
                                 'number' => 821,
                                 'title' => 'Percent',
                                 'description' => 'The number of undernourished people reached 821 million in 2017.'
                         ]) ?>
                     </div>
-                    <div class="cell medium-2">
+                    <div class="cell auto">
                         <?php View::render('molecules/statistics-figures/stat-card',[
                             'number' => 63,
                             'title' => 'Percent',
                             'description' => 'In 2017 Asia accounted for nearly two thirds, 63 percent, of the world’s hungry.'
                         ]) ?>
                     </div>
-                    <div class="cell medium-2">
+                    <div class="cell auto">
                         <?php View::render('molecules/statistics-figures/stat-card',[
                             'number' => 22,
                             'title' => 'Percent',
                             'description' => 'Nearly 151 million children under five, 22 percent, were still stunted in 2017.'
                         ]) ?>
                     </div>
-                    <div class="cell medium-2">
+                    <div class="cell auto">
                         <?php View::render('molecules/statistics-figures/stat-card',[
                             'number' => 13,
                             'title' => 'Percent',
                             'description' => 'More than 1 in 8 adults is obese.'
                         ]) ?>
                     </div>
-                    <div class="cell medium-2">
+                    <div class="cell auto">
                         <?php View::render('molecules/statistics-figures/stat-card',[
                             'number' => 'One',
                             'title' => 'Percent',
