8afbd5545def4e739842f076d1dbd80d3dd05190
Kevin Chavez <kevin@maniak.com>
2021-01-13 17:26:53 -0700

    update markup and style for sdg cards

--- views/molecules/statistics-figures/sdg-card-link.php
+++ views/molecules/statistics-figures/sdg-card-link.php
@@ -1,7 +1,6 @@
 <a href="<?= $link ?? '#'?>" target="_blank" class="sdg-card <?= $class ?>" data-color="<?= $class ?? '' ?>" data-url="<?= $urlData ?? ''?>">
     <span class="title scroll-track left-right delay-2"><?= $title ?></span>
-    <span class="flex-container align-center hover-image">
-        <img src="<?= $image ?>" alt="<?= $imageAlt ?>">
+    <span class="hover-image" style="background-image: url('<?= $image ?> ')">
         <span class="gradient"></span>
     </span>
 </a>
