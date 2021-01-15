df554fb175de92ec6e1916f96080bc2eb1eb67a4
Diego Rodríguez <diego@maniak.com.mx>
2021-01-15 12:04:38 -0600

    Expertise fixes

--- views/components/expertise-card.php
+++ views/components/expertise-card.php
@@ -4,18 +4,20 @@ use helpers\Svg;
 
 ?>
 <article class="expertise-card">
-    <div class="text">
-        <h3 class="heading h3">
-            <?= $title ?? '' ?>
-        </h3>
-        <p class="heading medium-copy">
-            <?= $description ?? '' ?>
-        </p>
-    </div>
-    <div class="cta-container">
-        <a class="text-link arrow-1" href="#">
-            <?= $cta ?? 'Read more' ?>
-            <?php Svg::render('btn-arrow-red', true, $cta ?? 'Read more', 'assets/images/arrows/')?>
-        </a>
-    </div>
+    <a href="#">
+        <div class="text">
+            <h3 class="heading h3">
+                <?= $title ?? '' ?>
+            </h3>
+            <p class="heading medium-copy">
+                <?= $description ?? '' ?>
+            </p>
+        </div>
+        <div class="cta-container">
+            <span class="text-link arrow-1">
+                <?= $cta ?? 'Read more' ?>
+                <?php Svg::render('btn-arrow-red', true, $cta ?? 'Read more', 'assets/images/arrows/')?>
+            </span>
+        </div>
+    </a>
 </article>
