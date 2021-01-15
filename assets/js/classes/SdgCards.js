97dddd3f69bf6d8bb31764043ba0d7cb3ed84085
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2021-01-15 12:25:56 -0600

    udpated cards functionality

--- assets/js/classes/SdgCards.js
+++ assets/js/classes/SdgCards.js
@@ -22,29 +22,78 @@ class SdgCards {
             dataType: 'json',
             success: ((response) => {
                 response.forEach(item => {
-                    this.$container.append(`
-                        <div class="cell medium-6 large-4">
-                            <a href="#">
-                            <div class="single-content-card-body">
-                                 <div class="card-content">
-                                     <div class="tag">${ item.tag }</div>
-                                     <div class="title">
-                                         <h4 class="heading h5">${ item.title }</h4>
-                                     </div>
-                                     <div class="description">
-                                         <p class="medium-copy">${ item.description }</p>
-                                     </div>
-                                     <div class="cta">
-                                         <div class="text-link arrow-2">
-                                             <span>${ item.cta }</span>
-                                             ${ this.$arrowRed.html() }
+                    switch (item.style) {
+                        case 'style-1':
+                            this.$container.append(`
+                            <div class="cell medium-6 large-4">
+                                <a href="${ item.link ? item.link : '' }">
+                                <div class="single-content-card-body">
+                                     <div class="card-content">
+                                         <div class="tag">${ item.tag }</div>
+                                         <div class="title">
+                                             <h4 class="heading h5">${ item.title }</h4>
                                          </div>
-                                     </div>
+                                         <div class="description">
+                                             <p class="medium-copy">${ item.description }</p>
+                                         </div>
+                                         <div class="cta">
+                                             <div class="text-link arrow-2">
+                                                 <span>${ item.cta }</span>
+                                                 ${ this.$arrowRed.html() }
+                                             </div>
+                                         </div>
+                                    </div>
                                 </div>
+                            </a>
                             </div>
-                        </a>
-                        </div>
-                    `);
+                            `);
+                            break;
+                        case 'style-2':
+                            this.$container.append(`
+                            <div class="cell medium-6 large-4">
+                                <a href="${ item.link ? item.link : '' }">
+                                    <div class="single-content-card-accent-color">
+                                        <div class="card-content">
+                                            <div class="tag">${ item.tag }</div>
+                                            <div class="title">
+                                                <h4 class="heading h4">${ item.title }</h4>
+                                            </div>
+                                            <div class="cta">
+                                                <div class="text-link arrow-2">
+                                                      <span>${ item.cta }</span>
+                                                      ${ this.$arrowRed.html() }
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                             </div>`)
+                            break;
+                        case 'style-3':
+                            this.$container.append(`
+                            <div class="cell medium-6 large-4">
+                                <a href="${ item.link ? item.link : '' }">
+                                    <div class="single-content-card-image">
+                                        <div class="card-content">
+                                            <div class="tag">${ item.tag }</div>
+                                            <div class="image" style="background-image: url( ${ item.image })">
+                                                <div class="gradient"></div>
+                                            </div>
+                                            <div class="title">
+                                                <h4 class="heading h5">${ item.title }</h4>
+                                            </div>
+                                            <div class="cta">
+                                                <div class="text-link arrow-2">
+                                                       <span>${ item.cta }</span>
+                                                        ${ this.$arrowRed.html() }
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>`)
+                            break;
+                    }
                 })
             })
         })
