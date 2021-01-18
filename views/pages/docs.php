2ecc76d11f4e3773c275b341bebd8ed15d9fdf50
Huzzidiel Harrison Jaimes <harrison@maniak.com.mx>
2021-01-17 18:09:44 -0600

    removed duplicate button doc

--- views/pages/docs.php
+++ views/pages/docs.php
@@ -84,29 +84,7 @@ use helpers\Svg;
             </pre>
                 </div>
                 <div id="doc-button-cta-mobile" class="hide">
-                    <div class="panel callout radius">
-                        this one
-                        <div>
-                            <button class="btn dark">
-                                Submit
-                                <svg xmlns="http://www.w3.org/2000/svg" width="11.643" height="17.341">
-                                    <path data-name="Path 637" d="M.643.767l9.441 7.9-9.441 7.905" fill="none"
-                                          stroke="#232e3e"
-                                          stroke-width="2"/>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                    <pre data-src="prism.js" class="language-html line-numbers" data-src-status="loaded">
-                <code class="language-html">
-                    &lt;button class=&quot;btn dark&quot;&gt;
-                        Submit
-                        &lt;svg xmlns=&quot;http://www.w3.org/2000/svg&quot; width=&quot;11.643&quot; height=&quot;17.341&quot;&gt;
-                            &lt;path data-name=&quot;Path 637&quot; d=&quot;M.643.767l9.441 7.9-9.441 7.905&quot; fill=&quot;none&quot; stroke=&quot;#232e3e&quot; stroke-width=&quot;2&quot;/&gt;
-                        &lt;/svg&gt;
-                    &lt;/button&gt;
-                </code>
-            </pre>
+                    <h2>working on it</h2>
                 </div>
             </div>
         </div>
