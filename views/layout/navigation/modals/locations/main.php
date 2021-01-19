1a8e856a3cd012163cfeb5a98d99b05936d67451
Diego Rodríguez <diego@maniak.com.mx>
2021-01-19 15:55:13 -0600

    Change lorem text

--- views/layout/navigation/modals/locations/main.php
+++ views/layout/navigation/modals/locations/main.php
@@ -18,7 +18,7 @@ use helpers\View;
                     <h3 class="heading h3 location-undp show-for-large"><span class="uppercase">undp</span> global</h3>
 
                     <h2 class="heading h2 title">Find Our Offices Around the World.</h2>
-                    <p class="big-copy description">The UNDP works in 170 countries around the world. Lorem ipsum dolor sit amet,  consectetur adipiscing elit. Duis auctor, ante sed auctor faucibus, diam ante pellentesque turpis, sit amet dictum felis felis bibendum nisl. </p>
+                    <p class="big-copy description">Get in touch, share your ideas, and discover how we can work together for a sustainable, just, and equitable future.</p>
                     <p class="tag uppercase hide-for-large tag-current-section">current selection</p>
                     <h3 class="heading h3 title hide-for-large title-undp"><span class="uppercase">undp</span> Global</h3>
                     <a class="text-link arrow-3 show-for-large" href="#">
