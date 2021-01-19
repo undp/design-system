6d1d6b1b9a5ca1eca192dcd5b649621cffc105e3
Carlos Bazana <bazana@maniak.com.mx>
2021-01-19 15:51:28 -0600

    Remove sdg stat cards slider and add horizontal scroll instead

--- views/partials/modal-sdgs.php
+++ views/partials/modal-sdgs.php
@@ -1,6 +1,8 @@
 <?php
+
 use helpers\View;
 use helpers\Svg;
+
 ?>
 
 <div id="modal-sdgs" class="modal-sdgs hide">
@@ -14,74 +16,44 @@ use helpers\Svg;
             <div class="cell medium-10 height-full modal-content">
                 <div class="side-spacing">
                     <?php View::render('organisms/hero/sdg-deep-dive-hero') ?>
-                    <div class="grid-x grid-padding-x cards-slider show-for-large">
-                        <div class="cell auto">
-                            <?php View::render('molecules/statistics-figures/stat-card',[
-                                    'number' => 821,
-                                    'title' => 'Percent',
-                                    'description' => 'The number of undernourished people reached 821 million in 2017.'
-                            ]) ?>
-                        </div>
-                        <div class="cell auto">
-                            <?php View::render('molecules/statistics-figures/stat-card',[
-                                'number' => 63,
-                                'title' => 'Percent',
-                                'description' => 'In 2017 Asia accounted for nearly two thirds, 63 percent, of the world’s hungry.'
-                            ]) ?>
-                        </div>
-                        <div class="cell auto">
-                            <?php View::render('molecules/statistics-figures/stat-card',[
-                                'number' => 22,
-                                'title' => 'Percent',
-                                'description' => 'Nearly 151 million children under five, 22 percent, were still stunted in 2017.'
-                            ]) ?>
-                        </div>
-                        <div class="cell auto">
-                            <?php View::render('molecules/statistics-figures/stat-card',[
-                                'number' => 13,
-                                'title' => 'Percent',
-                                'description' => 'More than 1 in 8 adults is obese.'
-                            ]) ?>
-                        </div>
-                        <div class="cell auto">
-                            <?php View::render('molecules/statistics-figures/stat-card',[
-                                'number' => 'One',
-                                'title' => 'Percent',
-                                'description' => '1 in 3 women of reproductive age is anemic.'
-                            ]) ?>
-                        </div>
-                    </div>
-                    <div class="sdg-stat-cards-slider hide-for-large">
-                        <div>
-                            <?php View::render('molecules/statistics-figures/stat-card',[
+                    <div class="grid-x grid-padding-x cards-slider">
+                        <div class="sdg-card-container">
+                            <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 821,
                                 'title' => 'Percent',
                                 'description' => 'The number of undernourished people reached 821 million in 2017.'
                             ]) ?>
                         </div>
-                        <div>
-                            <?php View::render('molecules/statistics-figures/stat-card',[
+                        <div class="sdg-card-container">
+                            <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 63,
                                 'title' => 'Percent',
                                 'description' => 'In 2017 Asia accounted for nearly two thirds, 63 percent, of the world’s hungry.'
                             ]) ?>
                         </div>
-                        <div>
-                            <?php View::render('molecules/statistics-figures/stat-card',[
+                        <div class="sdg-card-container">
+                            <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 22,
                                 'title' => 'Percent',
                                 'description' => 'Nearly 151 million children under five, 22 percent, were still stunted in 2017.'
                             ]) ?>
                         </div>
-                        <div>
-                            <?php View::render('molecules/statistics-figures/stat-card',[
+                        <div class="sdg-card-container">
+                            <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 13,
                                 'title' => 'Percent',
                                 'description' => 'More than 1 in 8 adults is obese.'
                             ]) ?>
                         </div>
-                        <div>
-                            <?php View::render('molecules/statistics-figures/stat-card',[
+                        <div class="sdg-card-container">
+                            <?php View::render('molecules/statistics-figures/stat-card', [
+                                'number' => 'One',
+                                'title' => 'Percent',
+                                'description' => '1 in 3 women of reproductive age is anemic.'
+                            ]) ?>
+                        </div>
+                        <div class="sdg-card-container">
+                            <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 'One',
                                 'title' => 'Percent',
                                 'description' => '1 in 3 women of reproductive age is anemic.'
