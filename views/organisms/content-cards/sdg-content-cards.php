ee881f5f0da6d569697339946e3688426e7eb0b8
mariotorres <mario@maniak.com.mx>
2021-01-20 13:52:12 -0600

    Discard changes on content card file

--- views/organisms/content-cards/sdg-content-cards.php
+++ views/organisms/content-cards/sdg-content-cards.php
@@ -67,8 +67,7 @@
                         'tag' => 'Content tag',
                         'link' => $_SERVER['REQUEST_URI'],
                         'title' => 'UN calls for comprehensive debt standstill in all developing countries.',
-                        'cta' => 'Read More',
-                        'color' => 'accent-green'
+                        'cta' => 'Read More'
                     ])
                     ?>
                 </div>
