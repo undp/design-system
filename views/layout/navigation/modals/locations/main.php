1e09b52066d978f89c5c0e4287acdec181cab210
Diego Rodríguez <diego@maniak.com.mx>
2021-01-19 18:59:33 -0600

    Location modal fixes

--- views/layout/navigation/modals/locations/main.php
+++ views/layout/navigation/modals/locations/main.php
@@ -6,7 +6,7 @@ use helpers\View;
 <section id="modal-search-offices" class="menu-modal modal-search-offices hide">
     <div class="grid-container fluid modal-content height-full">
         <div class="grid-x modal-body">
-            <div class="cell large-auto  cell-search">
+            <div class="cell large-auto cell-search">
                 <a class="hide-for-large btn-back" href="#" data-action-back data-btn-close>
                     <div class="arrow-back">
                         <?php Svg::render('arrow-back-bigger', true, 'back', 'assets/images/arrows/')?>
@@ -17,8 +17,8 @@ use helpers\View;
                     <p class="tag uppercase show-for-large">current selection</p>
                     <h3 class="heading h3 location-undp show-for-large"><span class="uppercase">undp</span> global</h3>
 
-                    <h2 class="heading h2 title">Find Our Offices Around the World.</h2>
-                    <p class="big-copy description">Get in touch, share your ideas, and discover how we can work together for a sustainable, just, and equitable future.</p>
+                    <h2 class="heading h2 title show-for-large">Find Our Offices Around the World.</h2>
+                    <p class="big-copy description show-for-large">Get in touch, share your ideas, and discover how we can work together for a sustainable, just, and equitable future.</p>
                     <p class="tag uppercase hide-for-large tag-current-section">current selection</p>
                     <h3 class="heading h3 title hide-for-large title-undp"><span class="uppercase">undp</span> Global</h3>
                     <a class="text-link arrow-3 show-for-large" href="#">
@@ -42,10 +42,31 @@ use helpers\View;
                             <input class="big-copy input-search" type="text" name="search-offices" id="search-offices"
                                    placeholder="Search offices" data-input-search>
                         </div>
-                        <?php View::render('layout/navigation/modals/locations/multi-selects') ?>
+                        <div class="search-select-group show-for-large">
+                            <?php
+                            View::render('partials/multi-select', [
+                                'id' => 'regions-select',
+                                'class' => 'regions-select',
+                                'ariaLabel' => 'Regions',
+                                'title' => 'Region',
+                                'dataType' => 'region'
+                            ])
+                            ?>
+                        </div>
+                        <div class="search-select-group show-for-large">
+                            <?php
+                            View::render('partials/multi-select', [
+                                'id' => 'office-type-select',
+                                'class' => 'office-type-select',
+                                'ariaLabel' => 'Offices',
+                                'title' => 'Office type',
+                                'dataType' => 'office'
+                            ])
+                            ?>
+                        </div>
                     </div>
                 </div>
-                <div class="flex-container align-middle active-filters" data-container-filters></div>
+                <div class="flex-container align-middle active-filters show-for-large" data-container-filters></div>
 
                 <div class="countries" tabindex="-1"></div>
                 <div class="flex-container align-center hide-for-large filter-options uppercase">
