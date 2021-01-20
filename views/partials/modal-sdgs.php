efa28c804c45eefce7bd51cc9ee3605e595c09cd
mariotorres <mario@maniak.com.mx>
2021-01-19 21:42:45 -0600

    Fix 456 - DD - Stat Cards Animation

--- views/partials/modal-sdgs.php
+++ views/partials/modal-sdgs.php
@@ -23,42 +23,42 @@ use helpers\Svg;
                 <div class="side-spacing">
                     <?php View::render('organisms/hero/sdg-deep-dive-hero') ?>
                     <div class="grid-x grid-padding-x cards-slider">
-                        <div class="sdg-card-container right-left delay-2">
+                        <div class="sdg-card-container right-left delay-1">
                             <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 821,
                                 'title' => 'Percent',
                                 'description' => 'The number of undernourished people reached 821 million in 2017.'
                             ]) ?>
                         </div>
-                        <div class="sdg-card-container right-left delay-3">
+                        <div class="sdg-card-container right-left small-delay-3">
                             <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 63,
                                 'title' => 'Percent',
                                 'description' => 'In 2017 Asia accounted for nearly two thirds, 63 percent, of the world’s hungry.'
                             ]) ?>
                         </div>
-                        <div class="sdg-card-container right-left delay-4">
+                        <div class="sdg-card-container right-left small-delay-4">
                             <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 22,
                                 'title' => 'Percent',
                                 'description' => 'Nearly 151 million children under five, 22 percent, were still stunted in 2017.'
                             ]) ?>
                         </div>
-                        <div class="sdg-card-container right-left delay-5">
+                        <div class="sdg-card-container right-left small-delay-5">
                             <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 13,
                                 'title' => 'Percent',
                                 'description' => 'More than 1 in 8 adults is obese.'
                             ]) ?>
                         </div>
-                        <div class="sdg-card-container right-left delay-6">
+                        <div class="sdg-card-container right-left small-delay-6">
                             <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 'One',
                                 'title' => 'Percent',
                                 'description' => '1 in 3 women of reproductive age is anemic.'
                             ]) ?>
                         </div>
-                        <div class="sdg-card-container right-left delay-7">
+                        <div class="sdg-card-container right-left small-delay-7">
                             <?php View::render('molecules/statistics-figures/stat-card', [
                                 'number' => 'One',
                                 'title' => 'Percent',
