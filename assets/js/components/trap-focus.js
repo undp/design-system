e4cbf92e481ee56a78047e7ad4f884dfcf6a4ef0
Diego Rodríguez <diego@maniak.com.mx>
2021-01-25 13:49:50 -0600

    Add trap focus and fix outline for sdgs

--- assets/js/components/trap-focus.js
+++ assets/js/components/trap-focus.js
@@ -1,7 +1,7 @@
 import ModalTrapFocus from "../classes/ModalTrapFocus";
 
 const init = function () {
-    const modals = $('.modal-nav-hover, .menu-modal');
+    const modals = $('.modal-nav-hover, .menu-modal, .modal-sdgs');
 
     modals.each((i, modal) => {
         let modalTrapFocus = new ModalTrapFocus(modal)
