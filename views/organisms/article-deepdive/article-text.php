b524d49926961cef0eab2d5d36ed6406fd716f3a
mariotorres <mario@maniak.com.mx>
2021-01-25 11:18:41 -0600

    Separate components

---
+++ views/organisms/article-deepdive/article-text.php
@@ -0,0 +1,24 @@
+<section class="article-text grid-container">
+    <div class="grid-x">
+        <div class="article-text-container cell small-8 small-offset-3 large-7 large-offset-2">
+            <p class="text-block">
+                For fostering Peace and Social Cohesion in Pujehun and Moyamba districts, communities score the Government of Sierra Leone,
+                the United Nations Development Programme (UNDP) and the World Food Programme (WFP) over 80% in the Multi-Stakeholder Project
+                supported by the Peacebuilding Fund (PBF).</p>
+            <p class="text-block">
+                Residents (youth, women, chiefs and other district and chiefdom authorities) of Malen and Makpele chiefdoms of Pujehun District
+                and of Lower Banta and Upper Banta Chiefdoms in Moyamba district recently rated the Government of Sierra Leone (GoSL), UNDP and
+                WFP Multi Stakeholder Platform (MSP) Project “over 80% successful” as a resource based and local conflict resolution mechanism
+                that promotes peace and social cohesion. This finding was revealed by district stakeholders during a two-day spot-check visit to
+                the project chiefdoms led by UNDP’s Resident Representative-Dr. Samuel Doe, where his team engaged with community members, local
+                and traditional leaders of the areas to learn about the outcomes of the <a href="#">project.</a>
+
+            </p>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
---
+++ views/organisms/article-deepdive/authors.php
@@ -0,0 +1,34 @@
+<?php
+
+$imgPath = '../../../assets/images/placeholder/article-deepdive'
+
+?>
+<section class="article-authors grid-container">
+    <div class="grid-x center-children">
+        <div class="authors-container cell large-8">
+            <div class="authors-title heading h5">
+                Author(s)
+            </div>
+            <div class="authors-information">
+                <div class="author-item">
+                    <div class="author-image">
+                        <img src="<?= "$imgPath/author-jane-doe.png" ?>" alt="">
+                    </div>
+                    <div class="author-info">
+                        <div class="author-name medium-copy">Jane Doe</div>
+                        <div class="job-position medium-copy">Title Organization</div>
+                    </div>
+                </div>
+                <div class="author-item">
+                    <div class="author-image">
+                        <img src="<?= "$imgPath/john-smith-author.png" ?>" alt="">
+                    </div>
+                    <div class="author-info">
+                        <div class="author-name medium-copy">John Smith</div>
+                        <div class="job-position medium-copy">Title Organization</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
