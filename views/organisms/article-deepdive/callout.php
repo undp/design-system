2d10a318d6bd131248143242a6c2a58f381ff17b
mariotorres <mario@maniak.com.mx>
2021-01-25 13:02:28 -0600

    add callout markup and style

---
+++ views/organisms/article-deepdive/callout.php
@@ -0,0 +1,12 @@
+<section class="callout green-background">
+    <div class="quote-text">
+        <h3 class="heading h3">
+            “It’s almost one year now, we have not witnessed road blockades or any act of violence in the Pujehun District
+            emanating from land related disputes or dissatisfied youth groups or a community member. We are grateful to
+            UNDP and WFP and the Government of Sierra Leone.”
+        </h3>
+        <div class="quote-author">
+            <h5 class="heading h5">Jitta Kanneh, Mammy Queen of Makpele Chiefdom.</h5>
+        </div>
+    </div>
+</section>
