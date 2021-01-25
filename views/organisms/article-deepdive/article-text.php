0828dee68ebbe5a9b4abeaceb87c8e27974ee464
mariotorres <mario@maniak.com.mx>
2021-01-25 16:39:02 -0600

    remove class from markup

--- views/organisms/article-deepdive/article-text.php
+++ views/organisms/article-deepdive/article-text.php
@@ -1,11 +1,11 @@
 <section class="article-text grid-container">
     <div class="grid-x">
         <div class="article-text-container cell small-12 large-7 large-offset-2">
-            <p class="text-block">
+            <p>
                 For fostering Peace and Social Cohesion in Pujehun and Moyamba districts, communities score the Government of Sierra Leone,
                 the United Nations Development Programme (UNDP) and the World Food Programme (WFP) over 80% in the Multi-Stakeholder Project
                 supported by the Peacebuilding Fund (PBF).</p>
-            <p class="text-block">
+            <p>
                 Residents (youth, women, chiefs and other district and chiefdom authorities) of Malen and Makpele chiefdoms of Pujehun District
                 and of Lower Banta and Upper Banta Chiefdoms in Moyamba district recently rated the Government of Sierra Leone (GoSL), UNDP and
                 WFP Multi Stakeholder Platform (MSP) Project “over 80% successful” as a resource based and local conflict resolution mechanism
