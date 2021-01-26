8d7484a57d844b1a0290010cb3c44ad6aeb47691
Carlos Bazana <bazana@maniak.com.mx>
2021-01-25 21:51:31 -0600

    Match news centre cards styles with designs

--- views/organisms/content-cards/news-centre-cards.php
+++ views/organisms/content-cards/news-centre-cards.php
@@ -3,15 +3,16 @@
 <section class="news-centre-cards">
     <div class="grid-container">
         <div class="grid-x">
-            <div class="cell large-3">
+            <div class="cell medium-4 large-3">
                 <?php View::render('partials/news-centre/cards-filter') ?>
+                <div class="flex-container active-filters show-for-large" data-active-filters></div>
             </div>
 
-            <div class="cell large-9 search-results cards-container">
+            <div class="cell medium-8 large-9 search-results cards-container">
                 <?php View::render('partials/news-centre/cards-results') ?>
 
                 <div class="cta-button">
-                    <button class="btn blue">
+                    <button class="btn blue load-more hide">
                         View More
                     </button>
                 </div>
--- views/partials/news-centre/cards-filter.php
+++ views/partials/news-centre/cards-filter.php
@@ -3,59 +3,11 @@
 use helpers\View;
 
 
-View::render('partials/select', [
-    'name' => 'location',
-    'placeholder' => 'Location',
-    'options' => [
-        [
-            'id' => 'location-1',
-            'label' => 'Location 1'
-        ],
-        [
-            'id' => 'location-2',
-            'label' => 'Location 2'
-        ],
-        [
-            'id' => 'location-3',
-            'label' => 'Location 3'
-        ]
-    ]
-]);
-
-View::render('partials/select', [
-    'name' => 'office',
-    'placeholder' => 'Office',
-    'options' => [
-        [
-            'id' => 'office-1',
-            'label' => 'Office 1'
-        ],
-        [
-            'id' => 'office-2',
-            'label' => 'Office 2'
-        ],
-        [
-            'id' => 'office-3',
-            'label' => 'Office 3'
-        ]
-    ]
-]);
-
-View::render('partials/select', [
-    'name' => 'publication',
-    'placeholder' => 'Publication',
-    'options' => [
-        [
-            'id' => 'publication-1',
-            'label' => 'Publication 1'
-        ],
-        [
-            'id' => 'publication-2',
-            'label' => 'Publication 2'
-        ],
-        [
-            'id' => 'publication-3',
-            'label' => 'Publication 3'
-        ]
-    ]
+View::render('partials/multi-select', [
+    'id' => 'content-type-select',
+    'class' => 'content-type-select',
+    'ariaLabel' => 'Content Types',
+    'title' => 'Content Types',
+    'dataType' => 'content-type',
+    'dataIdentifier' => ''
 ]);
