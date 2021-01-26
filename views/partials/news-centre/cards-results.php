bc82e4f4df409cda8e967623ca3ea445417bad5b
Carlos Bazana <bazana@maniak.com.mx>
2021-01-25 21:51:54 -0600

    Add multiple cards for demonstration purposes

--- views/partials/news-centre/cards-results.php
+++ views/partials/news-centre/cards-results.php
@@ -2,7 +2,7 @@
 
 
 <div class="grid-x grid-margin-x cards-results">
-    <div class="cell large-4 medium-6 card-item">
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="news">
         <?php
         View::render('molecules/cards/single-content-card-body', [
             'tag' => 'Content tag',
@@ -13,7 +13,7 @@
         ])
         ?>
     </div>
-    <div class="cell large-4 medium-6 card-item">
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="publications">
         <?php
         View::render('molecules/cards/single-content-card-accent-color', [
             'tag' => 'Content tag',
@@ -24,7 +24,7 @@
         ])
         ?>
     </div>
-    <div class="cell large-4 medium-6 card-item">
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="blogs">
         <?php
         View::render('molecules/cards/single-content-card-image', [
             'tag' => 'Content tag',
@@ -35,7 +35,7 @@
         ])
         ?>
     </div>
-    <div class="cell large-4 medium-6 card-item">
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="blogs">
         <?php
         View::render('molecules/cards/single-content-card-image', [
             'tag' => 'Content tag',
@@ -46,7 +46,7 @@
         ])
         ?>
     </div>
-    <div class="cell large-4 medium-6 card-item">
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="news">
         <?php
         View::render('molecules/cards/single-content-card-body', [
             'tag' => 'Content tag',
@@ -57,7 +57,139 @@
         ])
         ?>
     </div>
-    <div class="cell large-4 medium-6 card-item">
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="publications">
+        <?php
+        View::render('molecules/cards/single-content-card-accent-color', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'UN calls for comprehensive debt standstill in all developing countries.',
+            'cta' => 'Read More',
+            'color' => 'accent-green'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="news">
+        <?php
+        View::render('molecules/cards/single-content-card-body', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'UNDP at the Paris Peace Forum 2020.',
+            'description' => 'Three UNDP projects are among the one hundred “Solutions for Peace” selected, to be featured in the ‘Space for Solutions’.',
+            'cta' => 'Read More'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="publications">
+        <?php
+        View::render('molecules/cards/single-content-card-accent-color', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'UN calls for comprehensive debt standstill in all developing countries.',
+            'cta' => 'Read More',
+            'color' => 'accent-green'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="blogs">
+        <?php
+        View::render('molecules/cards/single-content-card-image', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'A vineyard with a storied past receives an injection of new life.',
+            'image' => '/assets/images/placeholder/cards/single-content-card-image.png',
+            'cta' => 'Explore'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="blogs">
+        <?php
+        View::render('molecules/cards/single-content-card-image', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'In Costa Rica, rural women grow their own businesses.',
+            'image' => '/assets/images/placeholder/cards/single-content-card-image.png',
+            'cta' => 'Read More'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="news">
+        <?php
+        View::render('molecules/cards/single-content-card-body', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => '“We are a forgotten population.”',
+            'description' => 'Using social media to help marginalized groups get HIV services during COVID-19',
+            'cta' => 'Read More'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="publications">
+        <?php
+        View::render('molecules/cards/single-content-card-accent-color', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'UN calls for comprehensive debt standstill in all developing countries.',
+            'cta' => 'Read More',
+            'color' => 'accent-green'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="news">
+        <?php
+        View::render('molecules/cards/single-content-card-body', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'UNDP at the Paris Peace Forum 2020.',
+            'description' => 'Three UNDP projects are among the one hundred “Solutions for Peace” selected, to be featured in the ‘Space for Solutions’.',
+            'cta' => 'Read More'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="publications">
+        <?php
+        View::render('molecules/cards/single-content-card-accent-color', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'UN calls for comprehensive debt standstill in all developing countries.',
+            'cta' => 'Read More',
+            'color' => 'accent-green'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="blogs">
+        <?php
+        View::render('molecules/cards/single-content-card-image', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'A vineyard with a storied past receives an injection of new life.',
+            'image' => '/assets/images/placeholder/cards/single-content-card-image.png',
+            'cta' => 'Explore'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="blogs">
+        <?php
+        View::render('molecules/cards/single-content-card-image', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => 'In Costa Rica, rural women grow their own businesses.',
+            'image' => '/assets/images/placeholder/cards/single-content-card-image.png',
+            'cta' => 'Read More'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="news">
+        <?php
+        View::render('molecules/cards/single-content-card-body', [
+            'tag' => 'Content tag',
+            'link' => $_SERVER['REQUEST_URI'],
+            'title' => '“We are a forgotten population.”',
+            'description' => 'Using social media to help marginalized groups get HIV services during COVID-19',
+            'cta' => 'Read More'
+        ])
+        ?>
+    </div>
+    <div class="cell large-4 medium-6 card-item hide" data-content-type="publications">
         <?php
         View::render('molecules/cards/single-content-card-accent-color', [
             'tag' => 'Content tag',
