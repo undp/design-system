8796520f5f12603db28d044950a83a913fbe44ee
Carlos Bazana <bazana@maniak.com.mx>
2021-01-26 16:07:50 -0600

    Issue #578 - Add missing component variation

--- views/organisms/article-deepdive/image-with-caption.php
+++ views/organisms/article-deepdive/image-with-caption.php
@@ -1,12 +1,22 @@
 <?php
-    $fullWidth = isset($fullWidth) ? $fullWidth : true;
-    $captionClasses = $fullWidth ? 'large-7 large-offset-2 full-width' : 'medium-2 medium-offset-0 split-row';
+$captionClasses = 'large-7 large-offset-2';
+$imageSize = isset($imageSize) ? $imageSize : 'large';
+$imageContainerClasses = 'scroll-track scale-up delay-2';
+
+if ($imageSize === 'small') {
+    $captionClasses = 'medium-2 medium-offset-0';
+    $imageContainerClasses = 'medium-4 medium-offset-2';
+}
+
+if ($imageSize === 'medium') {
+    $imageContainerClasses = 'large-7 large-offset-2';
+}
 ?>
 
-<section class="image-with-caption grid-container <?= $fullWidth ? 'full-width' : 'split-row' ?> <?= $classes ?? '' ?>">
+<section class="image-with-caption grid-container <?= $imageSize ?> <?= $classes ?? '' ?>">
     <div class="grid-x overflow-hidden">
 
-        <div class="image-container cell small-12 <?= $fullWidth ? 'scroll-track scale-up delay-2' : 'medium-4 medium-offset-2' ?>">
+        <div class="image-container cell small-12 <?= $imageContainerClasses ?>">
             <div
                 class="featured-image"
                 style="background-image: url('<?= $image ?? '' ?>')">
@@ -14,19 +24,19 @@
         </div>
 
         <?php if(isset($caption) && isset($imageAuthor)): ?>
-        <div class="image-caption cell small-8 small-offset-3 scroll-track opacity-only delay-2 <?= $captionClasses ?>">
-            <div class="image-alt">
-                <p class="medium-copy">
-                    <?= $caption ?? '' ?>
-                </p>
+            <div class="image-caption cell small-8 small-offset-3 scroll-track opacity-only delay-2 <?= $captionClasses ?>">
+                <div class="image-alt">
+                    <p class="medium-copy">
+                        <?= $caption ?? '' ?>
+                    </p>
+                </div>
+                <div class="image-author">
+                    <p class="medium-copy">
+                        <span class="show-for-medium">Photo:<br></span>
+                        <b><?= $imageAuthor ?? '' ?></b>
+                    </p>
+                </div>
             </div>
-            <div class="image-author">
-                <p class="medium-copy">
-                    <span class="show-for-medium">Photo:<br></span>
-                    <?= $imageAuthor ?? '' ?>
-                </p>
-            </div>
-        </div>
         <?php endif ?>
     </div>
 </section>
