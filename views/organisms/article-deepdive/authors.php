e7fc405d1190cafcec1256cddb1d76086bb7e976
Diego Rodríguez <diego@maniak.com.mx>
2021-01-28 14:22:12 -0600

    Changes to authors cards and links

--- views/organisms/article-deepdive/authors.php
+++ views/organisms/article-deepdive/authors.php
@@ -4,18 +4,24 @@
     <div class="grid-x">
         <div class="authors-container cell small-10 small-offset-1 large-offset-2 large-7">
             <div class="authors-title heading h5">
-                Author(s)
+                Author<?= count($authors) > 1? '(s)' : ''?>
             </div>
             <div class="authors-information">
                 <?php foreach ($authors as $i => $author) : ?>
                     <div class="author-item scroll-track left-right delay-<?= $i+2 ?>">
                         <div class="author-image">
+                            <a href="<?= $author['pageLink'] ?>">
                             <?php if ($author['image']) : ?>
                                 <img src="<?= $author['image'] ?>" alt="">
                             <?php endif; ?>
+                            </a>
                         </div>
                         <div class="author-info">
-                            <div class="author-name medium-copy"><?= $author['name'] ?></div>
+                            <div class="author-name medium-copy">
+                                <a href="<?= $author['pageLink'] ?>">
+                                <?= $author['name'] ?>
+                                </a>
+                            </div>
                             <div class="job-position medium-copy">
                                 <?= $author['title'] ?><br/>
                                 <?= $author['organization'] ?>
