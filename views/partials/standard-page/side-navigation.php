0e7349bc79aa46f1007931968ff8773aa2fb8b92
Carlos Bazana <bazana@maniak.com.mx>
2021-02-02 10:00:33 -0600

    Issue #639 - Build markup and styles for desktop side navigation

---
+++ views/partials/standard-page/side-navigation.php
@@ -0,0 +1,32 @@
+
+
+<div class="side-navigation <?= $classes ?? '' ?>">
+    <ul class="side-menu sticky">
+
+        <?php foreach($menuItems as $index => $menuItem) : ?>
+            <?php
+                $itemClasses = $index === 1 ? 'active' : '';
+                $itemClasses .= isset($menuItem['subMenuItems']) && $menuItem['subMenuItems'] ? ' has-children' : '';
+            ?>
+            <li class="side-menu-item flex-container flex-dir-column <?= $itemClasses ?>">
+                <a href="<?= $menuItem['link'] ?>"><?= $menuItem['name'] ?></a>
+
+                <?php if(isset($menuItem['subMenuItems'])) : ?>
+                    <ul class="side-submenu">
+
+                        <?php foreach($menuItem['subMenuItems'] as $subMenuItem) : ?>
+                            <li class="side-submenu-item">
+                                <a href="<?= $subMenuItem['link'] ?>">
+                                    <?= $subMenuItem['name'] ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+
+                    </ul>
+                <?php endif; ?>
+
+            </li>
+        <?php endforeach; ?>
+
+    </ul>
+</div>
