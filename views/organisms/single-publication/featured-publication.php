1433cace21d861100140857db72a2860062c41a4
mariotorres <mario@maniak.com.mx>
2021-02-02 15:23:43 -0600

    Add download icon

--- views/organisms/single-publication/featured-publication.php
+++ views/organisms/single-publication/featured-publication.php
@@ -14,8 +14,14 @@ use helpers\View;
                 <div class="publication-social">
                     <div class="grid-x social-container">
                         <div class="cell large-6">
-                            <div class="download">Download</div>
-                            <div class="icon-download"></div>
+                            <div class="download-btn flex-container align-center">
+                                <div class="download">Download</div>
+                                <div class="icon-download">
+                                    <div class="icon-item">
+                                        <?php Svg::render('icon-download') ?>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="cell large-6">
                             <div class="social-icons">
@@ -34,19 +40,24 @@ use helpers\View;
             <div class="publication-text-container">
                 <h2 class="heading h2">Publications</h2>
                 <div class="tag tag-text">Featured content tag</div>
-                <div class="large-copy publication-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt</div>
-                <div class="medium-copy publication-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                <div class="large-copy publication-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod
+                    tempor incididunt
+                </div>
+                <div class="medium-copy publication-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
                     in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</br></br>
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                    laborum</br></br>
 
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                     laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
                     voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
                 <div class="tags-container">
                     <div class="tag tag-item">Tag</div>
                     <div class="tag tag-item">Tag</div>
