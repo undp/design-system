1f0f69c17591d4209f6c8816cb21a44cb4557bf6
Diego Rodríguez <diego@maniak.com.mx>
2021-02-03 14:19:37 -0600

    List markup for download modal

--- views/organisms/single-publication/modal-publication-download.php
+++ views/organisms/single-publication/modal-publication-download.php
@@ -1,12 +1,75 @@
 <?php
 use helpers\View;
+use helpers\Svg;
 ?>
 
-<div class="modal-publication-download hide">
+<div class="modal-publication-download">
     <div class="modal-content">
+        <button class="btn-close">
+            <?php Svg::render('icon-close') ?>
+        </button>
         <div class="image-container">
             <?php View::render('molecules/images/publication-image', ['image' => '/assets/images/placeholder/publication-1.png']); ?>
         </div>
-        <div class="download-controls"></div>
+        <div class="download-controls">
+            <ul class="chapter-list">
+                <li class="chapter-item">
+                    <p class="chapter-title">Lorem Ipsum Title Goes Here</p>
+                    <p class="chapter-metadata">PDF (1.9 MB)</p>
+                    <label class="checkbox-item">
+                        <input type="checkbox" value="">
+                        <span class="checkmark"></span>
+                    </label>
+                </li>
+                <li class="chapter-item">
+                    <p class="chapter-title">Lorem Ipsum Title Goes Here</p>
+                    <p class="chapter-metadata">PDF (1.9 MB)</p>
+                    <label class="checkbox-item">
+                        <input type="checkbox" value="">
+                        <span class="checkmark"></span>
+                    </label>
+                </li>
+                <li class="chapter-item">
+                    <p class="chapter-title">Lorem Ipsum Title Goes Here</p>
+                    <p class="chapter-metadata">PDF (1.9 MB)</p>
+                    <label class="checkbox-item">
+                        <input type="checkbox" value="">
+                        <span class="checkmark"></span>
+                    </label>
+                </li>
+                <li class="chapter-item">
+                    <p class="chapter-title">Lorem Ipsum Title Goes Here</p>
+                    <p class="chapter-metadata">PDF (1.9 MB)</p>
+                    <label class="checkbox-item">
+                        <input type="checkbox" value="">
+                        <span class="checkmark"></span>
+                    </label>
+                </li>
+                <li class="chapter-item">
+                    <p class="chapter-title">Lorem Ipsum Title Goes Here</p>
+                    <p class="chapter-metadata">PDF (1.9 MB)</p>
+                    <label class="checkbox-item">
+                        <input type="checkbox" value="">
+                        <span class="checkmark"></span>
+                    </label>
+                </li>
+                <li class="chapter-item">
+                    <p class="chapter-title">Lorem Ipsum Title Goes Here</p>
+                    <p class="chapter-metadata">PDF (1.9 MB)</p>
+                    <label class="checkbox-item">
+                        <input type="checkbox" value="">
+                        <span class="checkmark"></span>
+                    </label>
+                </li>
+                <li class="chapter-item">
+                    <p class="chapter-title">Lorem Ipsum Title Goes Here</p>
+                    <p class="chapter-metadata">PDF (1.9 MB)</p>
+                    <label class="checkbox-item">
+                        <input type="checkbox" value="">
+                        <span class="checkmark"></span>
+                    </label>
+                </li>
+            </ul>
+        </div>
     </div>
 </div>
