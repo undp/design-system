a0b9dae034c66969fc85f24a49e3fd12a790e34e
mariotorres <mario@maniak.com.mx>
2021-02-03 10:26:01 -0600

    Fix Card size

--- views/molecules/cards/publication-card.php
+++ views/molecules/cards/publication-card.php
@@ -1,33 +1,32 @@
 <?php
 $imageExist = isset($image) && !empty($image);
 ?>
-<section>
-    <a href="#" class="publication-card <?= $type ?? 'hover-publication'  ?> <?= !$imageExist? 'no-image' : ''?> <?= $color ?? ''  ?>">
-        <div class="hover-slide">
-            <?php if($imageExist): ?>
-                <div class="publication">
-                    <div class="publication-image">
-                        <div class="image"><img src="<?= $image ?>" alt=""></div>
-                    </div>
+<a href="#"
+   class="publication-card <?= $type ?? 'hover-publication' ?> <?= !$imageExist ? 'no-image' : '' ?> <?= $color ?? '' ?>">
+    <div class="hover-slide">
+        <?php if ($imageExist): ?>
+            <div class="publication">
+                <div class="publication-image">
+                    <div class="image"><img src="<?= $image ?>" alt=""></div>
                 </div>
-            <?php endif; ?>
+            </div>
+        <?php endif; ?>
+    </div>
+    <article class="content">
+        <div class="tag"> <?= $tag ?? '' ?></div>
+        <div class="title-description">
+            <div class="heading h5">
+                <?= $title ?? '' ?>
+            </div>
+            <div class="medium-copy body-copy">
+                <?= $description ?? '' ?>
+            </div>
         </div>
-        <article class="content">
-            <div class="tag"> <?= $tag ?? '' ?></div>
-            <div class="title-description">
-                <div class="heading h5">
-                    <?= $title ?? '' ?>
-                </div>
-                <div class="medium-copy body-copy">
-                    <?= $description ?? '' ?>
-                </div>
+        <div class="cta">
+            <div class="text-link arrow-2">
+                <span><?= $cta ?? 'Read more' ?></span>
+                <?= file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg"); ?>
             </div>
-            <div class="cta">
-                <div class="text-link arrow-2">
-                    <span><?= $cta ?? 'Read more'?></span>
-                    <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
-                </div>
-            </div>
-        </article>
-    </a>
-</section>
+        </div>
+    </article>
+</a>
--- views/organisms/single-publication/featured-publication.php
+++ views/organisms/single-publication/featured-publication.php
@@ -1,68 +1,28 @@
-<?php
-
-use helpers\Svg;
-use helpers\View;
-
-?>
-<div class="grid-container featured-publication">
-    <div class="grid-x">
-        <div class="cell large-offset-1 large-4 medium-6 medium-offset-0">
-            <div class="publication">
-                <div class="publication-image">
-                    <img class="image lazy" data-src="/assets/images/placeholder/publication-1.png">
-                </div>
-                <div class="publication-social">
-                    <div class="grid-x social-container">
-                        <div class="cell large-6">
-                            <div class="download-btn flex-container">
-                                <div class="download">Download</div>
-                                <div class="icon-download">
-                                    <div class="icon-item">
-                                        <?php Svg::render('icon-download') ?>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="cell large-6">
-                            <div class="social-icons">
-                                <div class="icon-item"><?php Svg::render('icon-facebook') ?></div>
-                                <div class="icon-item"><?php Svg::render('icon-twitter') ?></div>
-                                <div class="icon-item"><?php Svg::render('icon-instagram') ?></div>
-                                <div class="icon-item"><?php Svg::render('icon-linkedin') ?></div>
-                                <div class="icon-item"><?php Svg::render('icon-youtube') ?></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="featured-publication">
+    <div class="publication-text-container">
+        <h2 class="heading h2">Publications</h2>
+        <div class="tag tag-text">Featured content tag</div>
+        <div class="large-copy publication-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod
+            tempor incididunt
         </div>
-        <div class="cell large-offset-1 large-5 medium-6 medium-offset-0 small-offset-1 small-11">
-            <div class="publication-text-container">
-                <h2 class="heading h2">Publications</h2>
-                <div class="tag tag-text">Featured content tag</div>
-                <div class="large-copy publication-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod
-                    tempor incididunt
-                </div>
-                <div class="medium-copy publication-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                    in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                    laborum</br></br>
+        <div class="medium-copy publication-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum</br></br>
 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </div>
-                <div class="tags-container">
-                    <div class="tag tag-item">Tag</div>
-                    <div class="tag tag-item">Tag</div>
-                </div>
-            </div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div class="tags-container">
+            <div class="tag tag-item">Tag</div>
+            <div class="tag tag-item">Tag</div>
         </div>
     </div>
 </div>
