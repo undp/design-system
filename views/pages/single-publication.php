0e60118081a4743797fd3378bfec87640c42f810
Carlos Bazana <bazana@maniak.com.mx>
2021-02-03 22:40:05 -0600

    Misc - Fixes in code indentation & spacings

--- views/pages/single-publication.php
+++ views/pages/single-publication.php
@@ -10,32 +10,34 @@ use helpers\View;
 <?php View::render('layout/navigation/main') ?>
 
 <div class="single-publication">
-    <?php  ?>
     <?php View::render('organisms/single-publication/modal-publication-download'); ?>
 
     <div class="grid-container">
         <div class="grid-x grid-margin-x">
             <div class="cell large-offset-1 large-4 medium-6 medium-offset-0">
                 <?php
-                View::render('partials/breadcrumb', [
-                    'links' => [
-                        ['link' => '/single-publication', 'name' => 'Publications'],
-                    ]
-                ])
+                    View::render('partials/breadcrumb', [
+                        'links' => [
+                            ['link' => '/single-publication', 'name' => 'Publications'],
+                        ]
+                    ]);
+
+                    View::render('molecules/cards/publication-featured-card', [
+                        'image' => '/assets/images/placeholder/publication-1.png'
+                    ]);
                 ?>
-                <?php View::render('molecules/cards/publication-featured-card', [
-                    'image' => '/assets/images/placeholder/publication-1.png'
-                ]); ?>
             </div>
-            <div class="cell large-offset-1 large-6 medium-6 medium-offset-0">
+            <div class="cell large-offset-1 large-5 medium-6 medium-offset-0">
                 <?php View::render('organisms/single-publication/featured-publication-text'); ?>
 
 
-                <div class="related-publications" data-view-more-less data-load-step="8">
+                <div class="related-publications" data-load-step="8" data-view-more-less>
                     <div class="heading h2 related-title">Related Publications</div>
+
                     <div class="grid-x grid-margin-x related-items-content cards-container">
                         <?php View::render('partials/single-publication/publication-cards'); ?>
                     </div>
+
                     <div class="cta-button flex-container">
                         <button class="btn blue view-more" data-view-more>
                             View More
