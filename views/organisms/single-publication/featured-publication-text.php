a4c98e12013ddaa508191cff5de1a1fb79199903
Diego Rodríguez <diego@maniak.com.mx>
2021-02-07 16:46:37 -0600

    Ad more text to 2nd publication

--- views/organisms/single-publication/featured-publication-text.php
+++ views/organisms/single-publication/featured-publication-text.php
@@ -2,23 +2,11 @@
     <div class="publication-text-container">
         <h2 class="heading h2">Publications</h2>
         <div class="tag tag-text">Featured content tag</div>
-        <div class="large-copy publication-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-            do eiusmod
-            tempor incididunt
+        <div class="large-copy publication-title">
+            <?= $title ?>
         </div>
-        <div class="medium-copy publication-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-            do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum</br></br>
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div class="medium-copy publication-body">
+            <?= $descriptionText ?>
         </div>
         <div class="tags-container">
             <div class="tag tag-item">Tag</div>
--- views/pages/single-publication.php
+++ views/pages/single-publication.php
@@ -1,5 +1,7 @@
 <?php
+
 use helpers\View;
+
 ?>
 
 <!-- Page Headers -->
@@ -11,52 +13,66 @@ use helpers\View;
 
 <div class="single-publication">
     <?php
-        View::render('organisms/single-publication/modal-publication-download', [
-            'image' => '/assets/images/placeholder/publication-1.png',
-            'languageOptions' => [
-                ['id' => 'pasto-1', 'value' => 'pasto', 'label' => 'Pasto'],
-                ['id' => 'dari-1', 'value' => 'dari', 'label' => 'Dari'],
-                ['id' => 'english-1', 'value' => 'en', 'label' => 'English'],
-                ['id' => 'albanian-1', 'value' => 'albanian', 'label' => 'Albanian'],
-                ['id' => 'arabic-1', 'value' => 'arabic', 'label' => 'Arabic'],
-                ['id' => 'portuguese-1', 'value' => 'portuguese', 'label' => 'Portuguese']
-            ],
-            'chapters' => [
-                ['title' => 'Lorem Ipsum Title Goes Here - English', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'en'],
-                ['title' => 'Lorem Ipsum Title Goes Here - English', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'en'],
-                ['title' => 'Lorem Ipsum Title Goes Here - Pasto', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'pasto'],
-                ['title' => 'Lorem Ipsum Title Goes Here - Dari', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'dari'],
-                ['title' => 'Lorem Ipsum Title Goes Here - Albanian', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'albanian'],
-                ['title' => 'Lorem Ipsum Title Goes Here - Arabic', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'arabic'],
-                ['title' => 'Lorem Ipsum Title Goes Here - Portuguese', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'portuguese']
-            ]
-        ]);
+    View::render('organisms/single-publication/modal-publication-download', [
+        'image' => '/assets/images/placeholder/publication-1.png',
+        'languageOptions' => [
+            ['id' => 'pasto-1', 'value' => 'pasto', 'label' => 'Pasto'],
+            ['id' => 'dari-1', 'value' => 'dari', 'label' => 'Dari'],
+            ['id' => 'english-1', 'value' => 'en', 'label' => 'English'],
+            ['id' => 'albanian-1', 'value' => 'albanian', 'label' => 'Albanian'],
+            ['id' => 'arabic-1', 'value' => 'arabic', 'label' => 'Arabic'],
+            ['id' => 'portuguese-1', 'value' => 'portuguese', 'label' => 'Portuguese']
+        ],
+        'chapters' => [
+            ['title' => 'Lorem Ipsum Title Goes Here - English', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'en'],
+            ['title' => 'Lorem Ipsum Title Goes Here - English', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'en'],
+            ['title' => 'Lorem Ipsum Title Goes Here - Pasto', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'pasto'],
+            ['title' => 'Lorem Ipsum Title Goes Here - Dari', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'dari'],
+            ['title' => 'Lorem Ipsum Title Goes Here - Albanian', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'albanian'],
+            ['title' => 'Lorem Ipsum Title Goes Here - Arabic', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'arabic'],
+            ['title' => 'Lorem Ipsum Title Goes Here - Portuguese', 'metadata' => 'PDF (1.9 MB)', 'lang' => 'portuguese']
+        ]
+    ]);
     ?>
 
     <div class="grid-container">
         <div class="grid-x grid-margin-x">
             <div class="cell large-offset-1 large-4 medium-6 medium-offset-0">
                 <?php
-                    View::render('partials/breadcrumb', [
-                        'links' => [
-                            ['link' => '/single-publication', 'name' => 'Publications'],
-                        ]
-                    ]);
+                View::render('partials/breadcrumb', [
+                    'links' => [
+                        ['link' => '/single-publication', 'name' => 'Publications'],
+                    ]
+                ]);
 
-                    View::render('molecules/cards/publication-featured-card', [
-                        'image' => '/assets/images/placeholder/publication-1.png'
-                    ]);
+                View::render('molecules/cards/publication-featured-card', [
+                    'image' => '/assets/images/placeholder/publication-1.png'
+                ]);
                 ?>
             </div>
             <div class="cell large-offset-1 medium-6">
-                <?php View::render('organisms/single-publication/featured-publication-text'); ?>
+                <?php View::render('organisms/single-publication/featured-publication-text', [
+                    'title' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+                    'descriptionText' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                        laborum</br></br>
+            
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                ]); ?>
 
 
                 <div class="related-publications" data-load-step="8" data-view-more-less>
                     <div class="heading h2 related-title">Related Publications</div>
 
                     <div class="grid-x grid-margin-x related-items-content cards-container">
-                        <?php View::render('partials/single-publication/publication-cards'); ?>
+                        <?php View::render('organisms/single-publication/publication-cards'); ?>
                     </div>
 
                     <div class="cta-button flex-container">
