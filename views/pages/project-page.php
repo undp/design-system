53837d59e24341d7aaeb32c1d898750a27e47f45
mariotorres <mario@maniak.com.mx>
2021-02-11 15:03:42 -0600

    Create separated files for project page components

--- views/pages/project-page.php
+++ views/pages/project-page.php
@@ -47,25 +47,20 @@ use helpers\Svg;
     <section class="headline-text">
         <div class="grid-container">
             <div class="grid-x">
-                <div class="cell large-offset-1 small-offset-1 small-10">
-                    <div class="heading h2 heading-text">
-                        United Nations Pacific</br>
-                        Regional Anti-Corruption Project
-                    </div>
-                </div>
-                <div class="cell large-offset-3 large-7 copy-text small-offset-1 small-10">
-                    <div class="big-copy">
-                        The United Nations Pacific Regional Anti-Corruption Project is a joint initiative of the United
-                        Nations
-                        Office on Drugs and Crime and the United Nations Development Programme, a unique partnership to
-                        prevent
-                        and fight corruption in 14 Pacific Island countries and the territory of Tokelau.</br></br>
-                        UN-PRAC aims to strengthen PICs’ national integrity systems. This is to promote ‘clean’
-                        governments and create an enabling environment to increase trade, business, investment and
-                        sustainable development in the Pacific. In turn, this will enhance the delivery of equitable and
-                        high-quality services to all Pacific Islanders.
-                    </div>
-                </div>
+                <?php
+                View::render('partials/project-page/headline-text', [
+                    'title' => 'United Nations Pacific</br>Regional Anti-Corruption Project',
+                    'copy' => 'The United Nations Pacific Regional Anti-Corruption Project is a joint initiative of the United
+                                Nations
+                                Office on Drugs and Crime and the United Nations Development Programme, a unique partnership to
+                                prevent
+                                and fight corruption in 14 Pacific Island countries and the territory of Tokelau.</br></br>
+                                UN-PRAC aims to strengthen PICs’ national integrity systems. This is to promote ‘clean’
+                                governments and create an enabling environment to increase trade, business, investment and
+                                sustainable development in the Pacific. In turn, this will enhance the delivery of equitable and
+                                high-quality services to all Pacific Islanders.'
+                ]);
+                ?>
             </div>
         </div>
     </section>
@@ -75,33 +70,11 @@ use helpers\Svg;
             <div class="grid-x">
                 <div class="cell large-offset-1 large-11 small-10 small-offset-1">
                     <div class="grid-x">
-                        <div class="cell large-2">
-                            <div class="heading h2"> What</br> We do</div>
-                        </div>
-                        <div class="cell large-offset-2 large-7 featured-copy">
-                            <div class="big-copy">
-                                The goal of the project is to promote and strengthen measures to prevent and fight
-                                corruption more efficiently and effectively in the Pacific. This aligns with the purpose
-                                of the United Nations Convention against Corruption (UNCAC) and contributes to the
-                                achievement of Sustainable Development Goal (SDG) 16 on Peace, Justice and Strong
-                                Institutions.
-                                </br></br>
-                                At the end of the project, we expect to achieve the following results:</br></br>
-                                <ul>
-                                    <li>
-                                        PICs more effectively implement UNCAC and work towards the achievement of SDG
-                                        16.
-                                    </li>
-                                    <li>
-                                        PICs strengthen broader governance frameworks in line with UNCAC and SDG 16.
-                                    </li>
-                                    <li>
-                                        PICs’ state and non-state actors more effectively engage and partner to prevent
-                                        and fight corruption.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                        <?php
+                        View::render('partials/project-page/featured-callout', [
+                        'title' => 'What</br> We do'
+                        ]);
+                        ?>
                     </div>
                 </div>
             </div>
@@ -148,68 +121,7 @@ use helpers\Svg;
     <section class="listed-stats">
         <div class="grid-container">
             <div class="grid-x">
-                <div class="cell large-offset-1 large-10 small-10 small-offset-1 heading-stats-text">
-                    <div class="heading h2">Impact</div>
-                </div>
-                <div class="cell large-offset-1 large-10 small-10 small-offset-1 heading-stats-items">
-                    <div class="grid-x grid-margin-x">
-                        <div class="cell medium-2 stat-item-container">
-                            <div class="stat-item">
-                                <div class="heading-item medium-copy">Status</div>
-                                <div class="body-text-item big-copy">Ongoing</div>
-                            </div>
-                            <div class="stat-item">
-                                <div class="heading-item medium-copy">Start date</div>
-                                <div class="body-text-item big-copy">2012</div>
-                            </div>
-                            <div class="stat-item">
-                                <div class="heading-item medium-copy">End date</div>
-                                <div class="body-text-item big-copy">2022</div>
-                            </div>
-                            <div class="stat-item">
-                                <div class="heading-item medium-copy">End date</div>
-                                <div class="body-text-item big-copy">Effective</br>  Governance</div>
-                            </div>
-                        </div>
-                        <div class="cell medium-3 stat-item-container">
-                            <div class="stat-item">
-                                <div class="heading-item medium-copy">Project Office</div>
-                                <div class="body-text-item big-copy">UNDP Pacific Office in Fiji</div>
-                            </div>
-                            <div class="stat-item">
-                                <div class="heading-item medium-copy">Implementing partner</div>
-                                <div class="body-text-item big-copy">
-                                    United Nations Development </br> Programme</br></br>
-                                    United Nations Office on Drugs </br> and Crime
-                                </div>
-                            </div>
-                        </div>
-                        <div class="cell medium-4 big-column stat-item-container">
-                            <div class="stat-item">
-                                <div class="heading-item medium-copy">Donors</div>
-                                <div class="body-text-item big-copy">
-                                    United Nations Development </br> Programme
-                                    </br></br>
-                                    Australian Government (DFAT)
-                                    </br></br>
-                                    New Zealand Aid Programme
-                                </div>
-                            </div>
-                        </div>
-                        <div class="cell medium-3 stat-item-container">
-                            <div class="stat-item">
-                                <div class="heading-item medium-copy">Contribution Amount</div>
-                                <div class="body-text-item big-copy">
-                                    Phase I, 2012-2016</br>
-                                    <p class="blue-link">USD 4,302,968 (DFAT)</p>
-                                    </br></br>
-                                    Phase II, 2016-2021:
-                                    <p class="blue-link">USD 5,359,088 (DFAT)</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php View::render('partials/project-page/listed-stats') ?>
             </div>
         </div>
     </section>
