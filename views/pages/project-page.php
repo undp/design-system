dfd169d95255d8527a7e08925dbeac856363e852
mariotorres <mario@maniak.com.mx>
2021-02-11 11:30:02 -0600

    Add Listed stats on Project Page

--- views/pages/project-page.php
+++ views/pages/project-page.php
@@ -44,7 +44,7 @@ $imgPath = '../../../assets/images/placeholder/about-us';
         </div>
     </section>
 
-    <div class="section headline-text">
+    <section class="headline-text">
         <div class="grid-container">
             <div class="grid-x">
                 <div class="cell large-offset-1 small-offset-1 small-10">
@@ -68,7 +68,7 @@ $imgPath = '../../../assets/images/placeholder/about-us';
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <section class="featured-callout">
         <div class="grid-container">
@@ -108,8 +108,7 @@ $imgPath = '../../../assets/images/placeholder/about-us';
         </div>
     </section>
 
-
-    <div class="related-table">
+    <section class="related-table">
         <div class="grid-container">
             <div class="grid-x">
                 <div class="cell large-offset-1 large-11 small-10 small-offset-1">
@@ -117,14 +116,16 @@ $imgPath = '../../../assets/images/placeholder/about-us';
                 </div>
                 <div class="cell large-offset-3 large-7 small-10 small-offset-1 link-list big-copy">
                     <div class="link-item">
-                        <a href="#"> Country Profiles - outcomes of the Mechanism for the Review of Implementation of the United
-                        Nations Convention against Corruption</a>
+                        <a href="#"> Country Profiles - outcomes of the Mechanism for the Review of Implementation of
+                            the United
+                            Nations Convention against Corruption</a>
                     </div>
                     <div class="link-item">
                         <a href="#"> Legislative Guide for the Implementation of UNCAC</a>
                     </div>
                     <div class="link-item">
-                        <a href="#">Mechanism for the Review of Implementation of the United Nations Convention against Corruption</a>
+                        <a href="#">Mechanism for the Review of Implementation of the United Nations Convention against
+                            Corruption</a>
                     </div>
                     <div class="link-item">
                         <a href="#">Pacific Youth Anti-Corruption Advocate’s Toolkit</a>
@@ -133,7 +134,8 @@ $imgPath = '../../../assets/images/placeholder/about-us';
                         <a href="#">Technical Guide to the United Nations Convention against Corruption</a>
                     </div>
                     <div class="link-item">
-                        <a href="#">Travaux Préparatoires of the negotiations for the elaboration of the United Nations Convention against Corruption</a>
+                        <a href="#">Travaux Préparatoires of the negotiations for the elaboration of the United Nations
+                            Convention against Corruption</a>
                     </div>
                     <div class="link-item">
                         <a href="#href">United Nations Convention against Corruption (UNCAC)</a>
@@ -141,7 +143,76 @@ $imgPath = '../../../assets/images/placeholder/about-us';
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+
+    <section class="listed-stats">
+        <div class="grid-container">
+            <div class="grid-x">
+                <div class="cell large-offset-1 large-10 small-10 small-offset-1 heading-stats-text">
+                    <div class="heading h2">Impact</div>
+                </div>
+                <div class="cell large-offset-1 large-10 small-10 small-offset-1 heading-stats-items">
+                    <div class="grid-x grid-margin-x">
+                        <div class="cell medium-2 stat-item-container">
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">Status</div>
+                                <div class="body-text-item big-copy">Ongoing</div>
+                            </div>
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">Start date</div>
+                                <div class="body-text-item big-copy">2012</div>
+                            </div>
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">End date</div>
+                                <div class="body-text-item big-copy">2022</div>
+                            </div>
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">End date</div>
+                                <div class="body-text-item big-copy">Effective</br>  Governance</div>
+                            </div>
+                        </div>
+                        <div class="cell medium-3 stat-item-container">
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">Project Office</div>
+                                <div class="body-text-item big-copy">UNDP Pacific Office in Fiji</div>
+                            </div>
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">Implementing partner</div>
+                                <div class="body-text-item big-copy">
+                                    United Nations Development </br> Programme</br></br>
+                                    United Nations Office on Drugs </br> and Crime
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cell medium-4 big-column stat-item-container">
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">Donors</div>
+                                <div class="body-text-item big-copy">
+                                    United Nations Development </br> Programme
+                                    </br></br>
+                                    Australian Government (DFAT)
+                                    </br></br>
+                                    New Zealand Aid Programme
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cell medium-3 stat-item-container">
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">Contribution Amount</div>
+                                <div class="body-text-item big-copy">
+                                    Phase I, 2012-2016</br>
+                                    <p class="blue-link">USD 4,302,968 (DFAT)</p>
+                                    </br></br>
+                                    Phase II, 2016-2021:
+                                    <p class="blue-link">USD 5,359,088 (DFAT)</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 
 <!-- Footer -->
