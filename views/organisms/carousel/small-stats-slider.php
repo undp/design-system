967f4778f0b22849a2418d3bb7fa96d815f6ab97
Carlos Bazana <bazana@maniak.com.mx>
2021-02-12 17:40:52 -0600

    Issue #899 - Adjust mobile styles for small stats slider

---
+++ views/organisms/carousel/small-stats-slider.php
@@ -0,0 +1,34 @@
+<?php use helpers\View; ?>
+
+
+<div class="small-stats-slider">
+    <div class="grid-container grid-x overflow-hidden">
+        <div class="cell small-10 small-offset-1">
+            <h2 class="heading h2"><?= $heading ?></h2>
+        </div>
+        <div class="cell">
+            <div class="dynamic-slider cell">
+
+                <div class="scroll-track left-right delay-1 flex-container flex-dir-column">
+                    <div class="glide__track" data-glide-el="track">
+                        <ul class="glide__slides">
+                            <?php foreach($cards as $card) : ?>
+                                <li class="glide__slide">
+                                    <?php
+                                        View::render('molecules/stats/small-card', [
+                                            'title' => $card['title']
+                                        ])
+                                    ?>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+
+                    <div class="bullets-container">
+                        <div class="glide__bullets" data-glide-el="controls[nav]"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
