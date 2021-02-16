a3f37a39b2166bbf24b186ff617fedb7b771062c
mariotorres <mario@maniak.com.mx>
2021-02-16 13:20:50 -0600

    Expand grey box on hero - country page - Big screens

--- views/organisms/hero/country-hero-3.php
+++ views/organisms/hero/country-hero-3.php
@@ -2,8 +2,9 @@
 use helpers\Svg;
 ?>
 <section class="country-hero-3">
-    <div class="left-bar"></div>
+
     <div class="grid-container hero-container">
+        <div class="left-bar"></div>
         <div class="left-bar-mobile"></div>
         <div class="grid-x">
             <div class="cell large-3 large-offset-1">
