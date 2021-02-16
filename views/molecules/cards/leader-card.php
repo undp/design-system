b7bd201ab6463524741ca107e8b783cde4d7820a
Carlos Bazana <bazana@maniak.com.mx>
2021-02-16 17:31:32 -0600

    WIP

--- views/molecules/cards/leader-card.php
+++ views/molecules/cards/leader-card.php
@@ -1,26 +1,60 @@
 <?php use helpers\Svg; ?>
 
-<a href="#" class="leader-card grid-x">
-    <div class="cell medium-8 leader-info flex-container flex-dir-column">
-        <h5 class="heading h5">
-            <?= $name ?? '' ?>
-        </h5>
-        <div class="leader-titles">
-            <?php foreach ($titles as $title) : ?>
-                <p class="medium-copy">
-                    <?= $title ?>
-                </p>
-            <?php endforeach; ?>
-        </div>
+<div class="leader-card">
+    <a href="#" class="content-wrapper grid-x" data-modal-trigger="some">
+        <div class="cell medium-8 leader-info flex-container flex-dir-column">
+            <h5 class="heading h5">
+                <?= $name ?? '' ?>
+            </h5>
+            <div class="leader-titles">
+                <?php foreach ($titles as $title) : ?>
+                    <p class="medium-copy">
+                        <?= $title ?>
+                    </p>
+                <?php endforeach; ?>
+            </div>
 
-        <div class="cta-container">
-            <div class="text-link arrow-2">
-                <span>Read more</span>
-                <?php Svg::render('arrows/btn-arrow-red-secondary') ?>
+            <div class="cta-container">
+                <div class="text-link arrow-2">
+                    <span>Read more</span>
+                    <?php Svg::render('arrows/btn-arrow-red-secondary') ?>
+                </div>
+            </div>
+        </div>
+        <div class="cell medium-4 leader-photo">
+            <img src="<?= $image ?? '' ?>" alt="Mourad Wahba Photo">
+        </div>
+    </a>
+</div>
+
+<div id="some" class="modal" data-modal>
+    <div class="modal-content">
+        <button class="btn-close" data-modal-close>
+            <?php Svg::render('icon-close') ?>
+        </button>
+        <div class="grid-container grid-x">
+            <div class="cell medium-8 leader-info flex-container flex-dir-column">
+                <h5 class="heading h5">
+                    <?= $name ?? '' ?>
+                </h5>
+                <div class="leader-titles">
+                    <?php foreach ($titles as $title) : ?>
+                        <p class="medium-copy">
+                            <?= $title ?>
+                        </p>
+                    <?php endforeach; ?>
+                </div>
+
+                <div class="cta-container">
+                    <div class="text-link arrow-2">
+                        <span>Read more</span>
+                        <?php Svg::render('arrows/btn-arrow-red-secondary') ?>
+                    </div>
+                </div>
+            </div>
+            <div class="cell medium-4 leader-photo">
+                <img src="<?= $image ?? '' ?>" alt="Mourad Wahba Photo">
             </div>
         </div>
     </div>
-    <div class="cell medium-4 leader-photo">
-        <img src="<?= $image ?? '' ?>" alt="Mourad Wahba Photo">
-    </div>
-</a>
+</div>
--- views/molecules/cards/small-leader-card.php
+++ views/molecules/cards/small-leader-card.php
@@ -1,25 +1,27 @@
 <?php use helpers\Svg; ?>
 
-<a href="<?= $link ?? '' ?>" class="small-leader-card">
-    <img src="<?= $image ?? '' ?>" alt="Mourad Wahba Photo">
+<div class="small-leader-card">
+    <a href="<?= $link ?? '' ?>" class="content-wrapper">
+        <img src="<?= $image ?? '' ?>" alt="Photo of <?= $name ?? '' ?>">
 
-    <div class="leader-info flex-container flex-dir-column">
-        <h5 class="heading h5">
-            <?= $name ?? '' ?>
-        </h5>
-        <div class="leader-titles">
-            <?php foreach($titles as $title) : ?>
-                <p class="medium-copy">
-                    <?= $title ?>
-                </p>
-            <?php endforeach; ?>
-        </div>
+        <div class="leader-info flex-container flex-dir-column">
+            <h5 class="heading h5">
+                <?= $name ?? '' ?>
+            </h5>
+            <div class="leader-titles">
+                <?php foreach($titles as $title) : ?>
+                    <p class="medium-copy">
+                        <?= $title ?>
+                    </p>
+                <?php endforeach; ?>
+            </div>
 
-        <div class="cta-container">
-            <div class="text-link arrow-2">
-                <span>Read more</span>
-                <?php Svg::render('arrows/btn-arrow-red-secondary')?>
+            <div class="cta-container">
+                <div class="text-link arrow-2">
+                    <span>Read more</span>
+                    <?php Svg::render('arrows/btn-arrow-red-secondary')?>
+                </div>
             </div>
         </div>
-    </div>
-</a>
+    </a>
+</div>
