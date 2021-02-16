6737cf0d2fe1e1189157aa399afe71a7f3a04faf
Carlos Bazana <bazana@maniak.com.mx>
2021-02-16 17:39:09 -0600

    Hotfix - Project page's impact style adjustments

--- views/organisms/stats/listed-stats.php
+++ views/organisms/stats/listed-stats.php
@@ -1,107 +1,104 @@
-<section class="listed-stats scroll-track left-right delay-1">
-    <div class="grid-container">
-        <div class="grid-x">
-            <div class="cell small-10 small-offset-1 heading-stats-text">
-                <div class="heading h2">Impact</div>
-            </div>
+<section class="listed-stats">
+    <div class="grid-x grid-container scroll-track left-right delay-1">
+        <div class="cell small-10 small-offset-1 heading-stats-text">
+            <div class="heading h2">Impact</div>
+        </div>
 
-            <div class="cell large-offset-1 large-10 heading-stats-items">
-                <div class="grid-x">
-                    <div class="cell large-2 medium-6 stat-item-container">
-                        <div class="grid-x">
-                            <div class="cell large-12 medium-8 small-6 first-item">
-                                <div class="stat-item">
-                                    <div class="heading-item medium-copy">Status</div>
-                                    <div class="body-text-item big-copy">Ongoing</div>
-                                </div>
-                                <div class="stat-item">
-                                    <div class="heading-item medium-copy">Start date</div>
-                                    <div class="body-text-item big-copy">2012</div>
-                                </div>
+        <div class="cell large-offset-1 large-10 heading-stats-items">
+            <div class="grid-x">
+                <div class="cell large-2 medium-6 stat-item-container">
+                    <div class="grid-x">
+                        <div class="cell large-12 medium-8 small-6 first-item">
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">Status</div>
+                                <div class="body-text-item big-copy">Ongoing</div>
                             </div>
-                            <div class="cell large-12 medium-4 small-6">
-                                <div class="stat-item">
-                                    <div class="heading-item medium-copy">End date</div>
-                                    <div class="body-text-item big-copy">2022</div>
-                                </div>
-                                <div class="stat-item">
-                                    <div class="heading-item medium-copy">End date</div>
-                                    <div class="body-text-item big-copy">Effective</br>  Governance</div>
-                                </div>
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">Start date</div>
+                                <div class="body-text-item big-copy">2012</div>
                             </div>
                         </div>
-                    </div>
-                    <div class="cell large-3 large-offset-0 medium-offset-2 medium-4 small-12 stat-item-container">
-                        <div class="stat-item">
-                            <div class="heading-item medium-copy">Project Office</div>
-                            <div class="body-text-item big-copy">
-                                <a href="#" class="animated-underline"> UNDP Pacific Office in Fiji</a>
+                        <div class="cell large-12 medium-4 small-6">
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">End date</div>
+                                <div class="body-text-item big-copy">2022</div>
                             </div>
-                        </div>
-                        <div class="stat-item">
-                            <div class="heading-item medium-copy">Implementing partner</div>
-                            <div class="body-text-item big-copy">
-                                United Nations Development </br> Programme</br></br>
-                                United Nations Office on Drugs </br> and Crime
+                            <div class="stat-item">
+                                <div class="heading-item medium-copy">End date</div>
+                                <div class="body-text-item big-copy">Effective</br>  Governance</div>
                             </div>
-                        </div>
-                    </div>
-
-                    <div class="cell medium-4 big-column stat-item-container">
-                        <div class="stat-item">
-                            <div class="heading-item medium-copy">Donors</div>
-                            <div class="body-text-item big-copy">
-                                United Nations Development </br> Programme
-                                </br></br>
-                                Australian Government (DFAT)
-                                </br></br>
-                                New Zealand Aid Programme
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="cell large-3 medium-6 stat-item-container">
-                        <div class="stat-item">
-                            <div class="grid-x">
-                                <div class="cell large-12 medium-6">
-                                    <div class="heading-item medium-copy">Contribution Amount</div>
-                                    <div class="body-text-item big-copy">
-                                        Phase I, 2012-2016</br>
-                                        <p class="blue-link">USD 4,302,968 (DFAT)</p>
-                                        </br>
-                                        Phase II, 2016-2021:
-                                        <p class="blue-link">USD 5,359,088 (DFAT)</p>
-                                        </br>
-                                        2020-2022
-                                        <p class="blue-link">NZD 4,000,000 (New Zeland Aid Programme</p>
-                                        </br>
-                                    </div>
-                                </div>
-                                <div class="cell large-12 large-offset-0 medium-4 medium-offset-2">
-                                    <div class="heading-item medium-copy">Delivery in previous Fiscal year</div>
-                                    </br>
-                                    <div class="body-text-item big-copy">
-                                        <div><span>2020:</span> <span class="blue-link"> USD 133,201</span></div>
-                                        </br>
-                                        <div><span>2019:</span> <span class="blue-link">USD 577,950 </span></div>
-                                        </br>
-                                        <div><span>2018:</span> <span class="blue-link">USD 318,113 </span></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="cell">
-                        <div class="cta-button flex-container">
-                            <button class="btn blue">
-                                Full project information
-                            </button>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div class="cell large-3 large-offset-0 medium-offset-2 medium-4 small-12 stat-item-container">
+                    <div class="stat-item">
+                        <div class="heading-item medium-copy">Project Office</div>
+                        <div class="body-text-item big-copy">
+                            <a href="#" class="animated-underline"> UNDP Pacific Office in Fiji</a>
+                        </div>
+                    </div>
+                    <div class="stat-item">
+                        <div class="heading-item medium-copy">Implementing partner</div>
+                        <div class="body-text-item big-copy">
+                            United Nations Development </br> Programme</br></br>
+                            United Nations Office on Drugs </br> and Crime
+                        </div>
+                    </div>
+                </div>
 
+                <div class="cell medium-4 big-column stat-item-container">
+                    <div class="stat-item">
+                        <div class="heading-item medium-copy">Donors</div>
+                        <div class="body-text-item big-copy">
+                            United Nations Development </br> Programme
+                            </br></br>
+                            Australian Government (DFAT)
+                            </br></br>
+                            New Zealand Aid Programme
+                        </div>
+                    </div>
+                </div>
+
+                <div class="cell large-3 medium-6 stat-item-container">
+                    <div class="stat-item">
+                        <div class="grid-x">
+                            <div class="cell large-12 medium-6">
+                                <div class="heading-item medium-copy">Contribution Amount</div>
+                                <div class="body-text-item big-copy">
+                                    Phase I, 2012-2016</br>
+                                    <p class="blue-link">USD 4,302,968 (DFAT)</p>
+                                    </br>
+                                    Phase II, 2016-2021:
+                                    <p class="blue-link">USD 5,359,088 (DFAT)</p>
+                                    </br>
+                                    2020-2022
+                                    <p class="blue-link">NZD 4,000,000 (New Zeland Aid Programme</p>
+                                    </br>
+                                </div>
+                            </div>
+                            <div class="cell large-12 large-offset-0 medium-4 medium-offset-2">
+                                <div class="heading-item medium-copy">Delivery in previous Fiscal year</div>
+                                </br>
+                                <div class="body-text-item big-copy">
+                                    <div><span>2020:</span> <span class="blue-link"> USD 133,201</span></div>
+                                    </br>
+                                    <div><span>2019:</span> <span class="blue-link">USD 577,950 </span></div>
+                                    </br>
+                                    <div><span>2018:</span> <span class="blue-link">USD 318,113 </span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="cell">
+                    <div class="cta-button flex-container">
+                        <button class="btn blue">
+                            Full project information
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
