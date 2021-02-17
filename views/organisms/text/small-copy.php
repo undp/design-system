c5eac51ac4cfe18a53921d28c38ec8cbb74ca065
mariotorres <mario@maniak.com.mx>
2021-02-16 18:19:17 -0600

    Add style for small copy component

--- views/organisms/text/small-copy.php
+++ views/organisms/text/small-copy.php
@@ -3,12 +3,30 @@
         <div class="cell small-offset-1 small-11">
             <div class="heading h2 component-heading">Copy Lockup</div>
             <div class="heading h3 component-subheading">Location</div>
-                <div class="grid-x">
-                    <div class="cell large-2 copy-item">
-                        <div class="medium-copy">USA</div>
+                <div class="grid-x grid-margin-x">
+                    <div class="cell large-3 copy-item">
+                        <div class="medium-copy title-item">USA</div>
                         <div class="big-copy">One United Nations Plaza · New York, NY 10017</div>
-                        <div class="big-copy">Phone: (212) 906-5000</div>
-                        <div class="big-copy">Email: HQ@undp.org</div>
+                        <div class="big-copy"><span class="headline">Phone:</span> (212) 906-5000</div>
+                        <div class="big-copy"><span class="headline">Email:</span> HQ@undp.org</div>
+                    </div>
+                    <div class="cell large-3 copy-item">
+                        <div class="medium-copy title-item">USA</div>
+                        <div class="big-copy">One United Nations Plaza · New York, NY 10017</div>
+                        <div class="big-copy"><span class="headline">Phone:</span> (212) 906-5000</div>
+                        <div class="big-copy"><span class="headline">Email:</span> HQ@undp.org</div>
+                    </div>
+                    <div class="cell large-3 copy-item">
+                        <div class="medium-copy title-item">USA</div>
+                        <div class="big-copy">One United Nations Plaza · New York, NY 10017</div>
+                        <div class="big-copy"><span class="headline">Phone:</span> (212) 906-5000</div>
+                        <div class="big-copy"><span class="headline">Email:</span> HQ@undp.org</div>
+                    </div>
+                    <div class="cell large-3 copy-item">
+                        <div class="medium-copy title-item">USA</div>
+                        <div class="big-copy">One United Nations Plaza · New York, NY 10017</div>
+                        <div class="big-copy"><span class="headline">Phone:</span> (212) 906-5000</div>
+                        <div class="big-copy"><span class="headline">Email:</span> HQ@undp.org</div>
                     </div>
                 </div>
             </div>
