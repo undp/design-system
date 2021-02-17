2921938d6640c1c5d04c4b11352953e03068e0e1
mariotorres <mario@maniak.com.mx>
2021-02-16 19:57:52 -0600

    Tab selector style - active states

--- views/organisms/text/selector.php
+++ views/organisms/text/selector.php
@@ -4,15 +4,33 @@
             <div class="heading h2 component-heading">Section Header</div>
             <div class="heading h3 component-subheading">Location</div>
             <div class="selector-tabs">
-                <div class="tab-item big-copy active">North America</div>
-                <div class="tab-item big-copy active">North America</div>
-                <div class="tab-item big-copy active">North America</div>
+                <div class="tab-item big-copy">North America</div>
+                <div class="tab-item big-copy active">Europe</div>
+                <div class="tab-item big-copy">Africa</div>
             </div>
         </div>
         <div class="selector-panel active">
-            <div class="grid-x">
+            <div class="grid-x grid-margin-x">
                 <div class="cell large-3 country-item">
                     <div class="heading">Italy</div>
+                    <div class="big-copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Egestas sed sed risus pretium quam
+                        vulputate dignissim suspendisse in. Aliquam faucibus purus in massa tempor nec</div>
+                </div>
+                <div class="cell large-3 country-item">
+                    <div class="heading">Germany</div>
+                    <div class="big-copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Egestas sed sed risus pretium quam
+                        vulputate dignissim suspendisse in. Aliquam faucibus purus in massa tempor nec</div>
+                </div>
+                <div class="cell large-3 country-item">
+                    <div class="heading">Spain</div>
+                    <div class="big-copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Egestas sed sed risus pretium quam
+                        vulputate dignissim suspendisse in. Aliquam faucibus purus in massa tempor nec</div>
+                </div>
+                <div class="cell large-3 country-item">
+                    <div class="heading">Hungary</div>
                     <div class="big-copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Egestas sed sed risus pretium quam
                         vulputate dignissim suspendisse in. Aliquam faucibus purus in massa tempor nec</div>
