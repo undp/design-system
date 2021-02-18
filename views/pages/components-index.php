680f2f54cbe5e9cd2e76c63104e37db2af27878e
Carlos Bazana <bazana@maniak.com.mx>
2021-02-18 14:47:12 -0600

    Misc - Style changes in components index page

--- views/pages/components-index.php
+++ views/pages/components-index.php
@@ -7,29 +7,43 @@
 <!-- Navigation -->
 <?php View::render('layout/navigation/main') ?>
 
-<main class="project-page">
-    <div class="grid-container grid-x component-categories">
+<main class="components-index">
+    <section class="grid-container scroll-track left-right delay-1">
+        <div class="grid-x">
+            <div class="cell small-12 large-9 large-offset-1">
+                <?php
+                View::render('partials/breadcrumb', [
+                    'links' => [
+                        ['link' => '/components', 'name' => 'Components']
+                    ],
+                    'classes' => 'accent-red'
+                ])
+                ?>
+            </div>
+        </div>
+    </section>
+
+    <section class="grid-container grid-x component-categories">
         <div class="cell medium-10 medium-offset-1">
-            <h2 class="heading h2">Component Categories:</h2>
+
+            <h3 class="heading h3">Component Categories</h3>
+
             <ul class="categories big-copy">
                 <li>
-                    <a href="/components/leadership" class="animated-underline">
-                        Leadership
-                    </a>
+                    <a href="/components/leadership" class="animated-underline">Leadership</a>
                 </li>
                 <li>
-                    <a href="/components/text" class="animated-underline">
-                        Text
-                    </a>
+                    <a href="/components/text" class="animated-underline">Text</a>
                 </li>
                 <li>
-                    <a href="/components/stats" class="animated-underline">
-                        Stats
-                    </a>
+                    <a href="/components/stats" class="animated-underline">Stats</a>
+                </li>
+                <li>
+                    <a href="/components/photo" class="animated-underline">Photo</a>
                 </li>
             </ul>
         </div>
-    </div>
+    </section>
 </main>
 
 <!-- Footer -->
