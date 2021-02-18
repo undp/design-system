3fdb91ac36e92525e08111933e2910cc8c73294e
Carlos Bazana <bazana@maniak.com.mx>
2021-02-18 17:24:02 -0600

    Issue #970 - Build photo details overlay molecule

---
+++ views/molecules/photo/photo-details-overlay.php
@@ -0,0 +1,14 @@
+
+<div class="photo-details-overlay">
+    <img class="photo" src="<?= $image ?? '' ?>" alt="">
+
+    <div class="details-overlay flex-container flex-dir-column">
+        <p class="medium-copy">
+            On average, UNDP supports an election somewhere in the world every two weeks.
+        </p>
+        <p class="medium-copy flex-container flex-dir-column">
+            UNDP Taiwan
+            <strong>Jason Smith</strong>
+        </p>
+    </div>
+</div>
