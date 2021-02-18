ed9dd9456bca95908967b94dddd13bf3b5168ee4
Carlos Bazana <bazana@maniak.com.mx>
2021-02-17 18:09:26 -0600

    WIP

--- views/organisms/photo/parallax-gallery.php
+++ views/organisms/photo/parallax-gallery.php
@@ -1,10 +1,15 @@
 <?php $imagePath = '/assets/images/placeholder/galleries' ?>
 
-<section class="parallax-gallery overflow-hidden">
-    <div class="grid-container grid-x grid-margin-x gs--parallax-container">
-        <div class="text-container cell medium-6 scroll-track-center">
+<section class="parallax-gallery">
+    <div class="grid-container grid-x grid-margin-x gs--parallax-container scroll-track-center">
+
+        <div class="cell scroll-track left-right delay-2">
+            <h3 class="heading h3 hide-for-medium">UNDP Hope</h3>
+        </div>
+
+        <div class="text-container cell medium-6">
             <div class="track left-right delay-4">
-                <h3 class="heading h3">UNDP Hope</h3>
+                <h3 class="heading h3 show-for-medium">UNDP Hope</h3>
 
                 <p class="medium-copy">
                     UNDP works in about 170 countries and territories, helping to achieve the eradication of poverty,
@@ -21,7 +26,7 @@
                 </div>
             </div>
         </div>
-        <div class="cell medium-6">
+        <div class="cell medium-6 overflow-hidden scroll-track opacity-only delay-2">
             <div class="gallery-images grid-x grid-margin-x">
                 <div class="cell small-6 gs--parallax-column">
                     <img src="<?= "$imagePath/gallery-parallax01@2x.jpg" ?>" alt="">
