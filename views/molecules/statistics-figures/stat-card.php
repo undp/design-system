d8f4e854bc057ea836078ff51582f2a9d81874d6
Carlos Bazana <bazana@maniak.com.mx>
2021-02-19 13:09:05 -0600

    Issue #980 - Add medium stat card styles

--- views/molecules/statistics-figures/stat-card.php
+++ views/molecules/statistics-figures/stat-card.php
@@ -1,12 +1,12 @@
 <?php
-$sizes = ['small', 'large'];
+$sizes = ['small', 'medium', 'large'];
 $cardSize = isset($size) && in_array($size, $sizes) ? $size : 'small';
 ?>
 
 
 <div class="stat-card <?= $cardSize ?> flex-container">
     <div class="card-content">
-        <?php if ($cardSize === 'large') : ?>
+        <?php if ($cardSize !== 'small') : ?>
             <h3 class="heading h3"><?= $heading ?? '' ?></h3>
         <?php endif; ?>
 
