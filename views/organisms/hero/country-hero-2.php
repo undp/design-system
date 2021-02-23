e5eb5e77bea59b13fff5e7cea392fa9d6a29bb09
mariotorres <mario@maniak.com.mx>
2021-02-23 11:42:51 -0600

    remove hover animation to hero country option 2

--- views/organisms/hero/country-hero-2.php
+++ views/organisms/hero/country-hero-2.php
@@ -19,56 +19,16 @@ use helpers\Svg;
 
         <div class="grid-x grid-margin-x images-block">
             <div class="cell large-2 small-3 image-item">
-                <div class="gradient"></div>
                 <img src="/assets/images/placeholder/hero/image-block-hero-1.jpg" alt="">
-                <div class="description">
-                    <div class="card-description-text">Women earn only 77 cents for every dollar that men get for the same work</div>
-                    <div class="cta">
-                        <div class="text-link arrow-2">
-                            <span>Explore</span>
-                            <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div class="cell large-3 small-3 image-item">
-                <div class="gradient"></div>
                 <img src="/assets/images/placeholder/hero/image-block-hero-2.jpg" alt="">
-                <div class="description">
-                    <div class="card-description-text">Women earn only 77 cents for every dollar that men get for the same work</div>
-                    <div class="cta">
-                        <div class="text-link arrow-2">
-                            <span>Explore</span>
-                            <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div class="cell large-2 small-3 image-item">
-                <div class="gradient"></div>
                 <img src="/assets/images/placeholder/hero/image-block-hero-3.jpg" alt="">
-                <div class="description">
-                    <div class="card-description-text">Women earn only 77 cents for every dollar that men get for the same work</div>
-                    <div class="cta">
-                        <div class="text-link arrow-2">
-                            <span>Explore</span>
-                            <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div class="cell large-3 small-3 image-item">
-                <div class="gradient"></div>
                 <img src="/assets/images/placeholder/hero/image-block-hero-4.jpg" alt="">
-                <div class="description">
-                    <div class="card-description-text">Women earn only 77 cents for every dollar that men get for the same work</div>
-                    <div class="cta">
-                        <div class="text-link arrow-2">
-                            <span>Explore</span>
-                            <?=  file_get_contents("assets/images/arrows/btn-arrow-red-secondary.svg");  ?>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div class="cell large-2 cta-button">
                 <button class="btn">
