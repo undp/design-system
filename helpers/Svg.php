3b346d228accced846ab0497882b5f354cfbce76
Carlos Bazana <bazana@maniak.com.mx>
2021-02-23 12:55:11 -0600

    Hotfix - Fix relative path in svg helper

--- helpers/Svg.php
+++ helpers/Svg.php
@@ -8,11 +8,13 @@ class Svg
     public static function render($svgName, $imgTag = false, $alt = '' , $path = 'assets/images/')
     {
         $fullPath = $path . $svgName . '.svg';
-        if (file_exists($fullPath)) {
 
+        if (file_exists($fullPath)) {
             if ($imgTag){
+                $fullPath = '/' . $fullPath;
                 echo "<img src='$fullPath' alt='$alt'>";
-            }else{
+            }
+            else{
                 echo file_get_contents($fullPath);
             }
         }
