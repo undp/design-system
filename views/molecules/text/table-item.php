2a15628cee6ed868982eebeb2f1b4124921e69e1
mariotorres <mario@maniak.com.mx>
2021-02-23 17:56:28 -0600

    Add min height to content text

--- views/molecules/text/table-item.php
+++ views/molecules/text/table-item.php
@@ -4,10 +4,11 @@
         <?php View::render('molecules/photo/publication-image', ['image' => $image]); ?>
     <?php endif; ?>
     <div class="item-text-content">
-        <div class="big-copy"><?= $title ?? '' ?></div>
-        <a href="#" class="text-link arrow-3">
+        <div class="big-copy title-text"><?= $title ?? '' ?></div>
+
+        <a href="#" class="text-link arrow-3" <?= $attributes ?? '' ?>>
             <?= $btnText ?? '' ?>
-            <?=  file_get_contents("assets/images/arrows/btn-arrow-external.svg");  ?>
+            <?=  file_get_contents($btnIcon ?? 'assets/images/arrows/btn-arrow-external.svg');  ?>
         </a>
     </div>
 </div>
