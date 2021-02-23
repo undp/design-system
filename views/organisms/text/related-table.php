f0fdce646b69e058e495184ee96849ed07a23962
mariotorres <mario@maniak.com.mx>
2021-02-22 19:16:11 -0600

    Change design from related table on project page

--- views/organisms/text/related-table.php
+++ views/organisms/text/related-table.php
@@ -1,39 +1,62 @@
-
+<?php use helpers\View;?>
 <section class="related-table scroll-track left-right delay-1">
     <div class="grid-container">
         <div class="grid-x">
             <div class="cell large-offset-1 large-11 small-10 small-offset-1">
                 <div class="heading h2">Related Materials</div>
             </div>
-            <div class="cell large-offset-3 large-7 small-10 small-offset-1 link-list big-copy">
-                <div class="link-item">
-                    <a href="#" class="animated-underline"> Country Profiles - outcomes of the Mechanism for the
+            <div class="cell large-offset-3 large-6 small-10 small-offset-1 link-list big-copy">
+                <?php
+                View::render('molecules/text/table-item', [
+                    'title' => 'Country Profiles - outcomes of the Mechanism for the
                         Review of Implementation of
                         the United
-                        Nations Convention against Corruption</a>
+                        Nations Convention against Corruption'
+                ]);
+                ?>
+                <div class="grid-x grid-margin-x">
+                    <div class="cell large-6">
+                        <?php
+                        View::render('molecules/text/table-item', [
+                            'title' => 'Legislative Guide for the Implementation of UNCAC'
+                        ]);
+                        ?>
+                    </div>
+                    <div class="cell large-6">
+                        <?php
+                        View::render('molecules/text/table-item', [
+                            'title' => 'Pacific Youth Anti-Corruption Advocate’s Toolkit'
+                        ]);
+                        ?>
+                    </div>
                 </div>
-                <div class="link-item">
-                    <a href="#" class="animated-underline"> Legislative Guide for the Implementation of UNCAC</a>
-                </div>
-                <div class="link-item">
-                    <a href="#" class="animated-underline">Mechanism for the Review of Implementation of the United
+                <?php
+                View::render('molecules/text/table-item', [
+                    'title' => 'Mechanism for the Review of Implementation of the United
                         Nations Convention against
-                        Corruption</a>
-                </div>
-                <div class="link-item">
-                    <a href="#" class="animated-underline">Pacific Youth Anti-Corruption Advocate’s Toolkit</a>
-                </div>
-                <div class="link-item">
-                    <a href="#" class="animated-underline">Technical Guide to the United Nations Convention against
-                        Corruption</a>
-                </div>
-                <div class="link-item">
-                    <a href="#" class="animated-underline">Travaux Préparatoires of the negotiations for the
+                        Corruption'
+                ]);
+                ?>
+                <?php
+                View::render('molecules/text/table-item', [
+                    'title' => 'Travaux Préparatoires of the negotiations for the
                         elaboration of the United Nations
-                        Convention against Corruption</a>
-                </div>
-                <div class="link-item">
-                    <a href="#" class="animated-underline">United Nations Convention against Corruption (UNCAC)</a>
+                        Convention against Corruption'
+                ]);
+                ?>
+                <?php
+                View::render('molecules/text/table-item', [
+                    'title' => 'Technical Guide to the United Nations Convention against Corruption'
+                ]);
+                ?>
+                <div class="grid-x grid-margin-x">
+                    <div class="cell large-6">
+                        <?php
+                        View::render('molecules/text/table-item', [
+                            'title' => 'United Nations Convention against Corruption (UNCAC)'
+                        ]);
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
