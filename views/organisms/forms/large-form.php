db4741371cbc5da3a2416baea8f868223d815f8c
Carlos Bazana <bazana@maniak.com.mx>
2021-02-24 17:03:07 -0600

    Issue #1081 - Iterate on input label styles

--- views/organisms/forms/large-form.php
+++ views/organisms/forms/large-form.php
@@ -8,13 +8,13 @@
 
                 <div class="input-group">
                     <div class="input-wrapper">
-                        <label for="first-name"></label>
-                        <input id="first-name" class="input custom-input" type="text" placeholder="First Name" aria-label="First Name" disabled>
+                        <label for="first-name">First Name</label>
+                        <input id="first-name" class="input custom-input" type="text" placeholder="First Name" aria-label="First Name">
                     </div>
 
                     <div class="input-wrapper">
-                        <input class="input custom-input error" type="text" placeholder="Last Name" aria-label="Last Name" required>
-                        <label for="first-name">First Name</label>
+                        <label for="last-name" class="required">Last Name</label>
+                        <input id="last-name" class="input custom-input error" type="text" placeholder="Last Name" aria-label="Last Name" required>
 
                         <ul class="input-errors">
                             <li>Error</li>
@@ -23,7 +23,7 @@
                 </div>
 
                 <div class="input-group">
-                    <input class="input custom-input" type="text" placeholder="Email" aria-label="Email" required>
+                    <input class="input custom-input" type="text" placeholder="Email" aria-label="Email" disabled>
                 </div>
 
                 <button type="submit" class="btn">
