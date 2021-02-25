4fa3c2dacbddda41d02574f3867a8fef02646631
mariotorres <mario@maniak.com.mx>
2021-02-24 18:28:17 -0600

    Fix table mobile

--- views/organisms/text/table.php
+++ views/organisms/text/table.php
@@ -5,7 +5,7 @@
                 <div class="heading h2 component-heading">Table</div>
             </div>
         </div>
-        <div class="grid-x grid-margin-x columns-content">
+        <div class="grid-x grid-margin-x columns-content table-desktop">
             <div class="cell large-3 large-offset-0 small-offset-1 small-9 column-table">
                 <div class="title medium-copy">Name</div>
                 <div class="table-item">Limya ELTAYEB</div>
@@ -33,6 +33,42 @@
                 </div>
                 <div class="table-item">
                     <a href="" class="animated-underline link-item">Nuno.Queiros@undp.org</a>
+                </div>
+            </div>
+        </div>
+        <div class="mobile-table">
+            <div class="grid-x columns-content">
+                <div class="cell large-3 large-offset-0 small-offset-1 small-9 column-table">
+                    <div class="table-item">
+                        <div class="title medium-copy">Name</div>
+                        <div class="name-item">Limya ELTAYEB</div>
+                    </div>
+                    <div class="table-item">
+                        <div class="title medium-copy">title</div>
+                        <div class="name-item">UNDP Resident Representative</div>
+                    </div>
+                    <div class="table-item">
+                        <div class="title medium-copy">Email</div>
+                        <div class="name-item">
+                            <a href="" class="animated-underline link-item">Nuno.Queiros@undp.org</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="cell large-3 large-offset-0 small-offset-1 small-9 column-table">
+                    <div class="table-item">
+                        <div class="title medium-copy">Name</div>
+                        <div class="name-item">Limya ELTAYEB</div>
+                    </div>
+                    <div class="table-item">
+                        <div class="title medium-copy">title</div>
+                        <div class="name-item">UNDP Resident Representative</div>
+                    </div>
+                    <div class="table-item">
+                        <div class="title medium-copy">Email</div>
+                        <div class="name-item">
+                            <a href="" class="animated-underline link-item">Nuno.Queiros@undp.org</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
