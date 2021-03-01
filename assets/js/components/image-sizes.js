fd94e0f69326128ace2011a14ad996bfc71b9c77
mariotorres <mario@maniak.com.mx>
2021-03-01 13:39:57 -0600

    Remove console log

--- assets/js/components/image-sizes.js
+++ assets/js/components/image-sizes.js
@@ -3,7 +3,6 @@ import ImageSize from '../classes/ImageSize'
 const init = function() {
     const imageSize = new ImageSize();
     imageSize.init();
-    console.log('here');
 }
 
 export default init;
