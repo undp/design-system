d438692ee3d669ece5ed2fc98bdd61e00e2eec48
mariotorres <mario@maniak.com.mx>
2021-03-04 16:33:55 -0600

    Rework table component - remove double markup

--- views/organisms/text/table.php
+++ views/organisms/text/table.php
@@ -5,71 +5,89 @@
                 <div class="heading h2 component-heading">Table</div>
             </div>
         </div>
-        <div class="grid-x grid-margin-x columns-content table-desktop scroll-track left-right delay-1">
-            <div class="cell large-3 large-offset-1 medium-4 medium-offset-0 small-offset-1 small-9 column-table">
-                <div class="title medium-copy">Name</div>
-                <div class="table-item">Limya ELTAYEB</div>
-                <div class="table-item">Limya ELTAYEB</div>
-                <div class="table-item">Limya ELTAYEB</div>
-                <div class="table-item">Limya ELTAYEB</div>
-            </div>
-            <div class="cell large-3 medium-4 medium-offset-0 small-offset-1 small-9 column-table">
-                <div class="title medium-copy">Title</div>
-                <div class="table-item">UNDP Resident Representative</div>
-                <div class="table-item">Deputy Resident Representative</div>
-                <div class="table-item">Operations Specialist</div>
-                <div class="table-item">Programme Specialist- Democratic Governance</div>
-            </div>
-            <div class="cell large-4 medium-4 medium-offset-0 small-offset-1 small-9 column-table">
-                <div class="title medium-copy">Email</div>
-                <div class="table-item">
-                    <a href="" class="animated-underline link-item">limya.eltayeb@undp.org</a>
-                </div>
-                <div class="table-item">
-                    <a href="" class="animated-underline link-item">Nuno.Queiros@undp.org</a>
-                </div>
-                <div class="table-item">
-                    <a href="" class="animated-underline link-item">Alma.Mustafaraj@undp.org</a>
-                </div>
-                <div class="table-item">
-                    <a href="" class="animated-underline link-item">Nuno.Queiros@undp.org</a>
-                </div>
-            </div>
-        </div>
-        <div class="mobile-table">
-            <div class="grid-x columns-content">
-                <div class="cell large-3 large-offset-0 small-offset-1 small-9 column-table">
-                    <div class="table-item">
+
+        <div class="grid-x">
+            <div class="cell large-11 large-offset-1 medium-offset-0 medium-12 small-offset-1 small-9 columns-content">
+                <div class="grid-x grid-margin-x row-element hide-for-small-only">
+                    <div class="cell large-3 medium-3">
                         <div class="title medium-copy">Name</div>
-                        <div class="name-item">Limya ELTAYEB</div>
                     </div>
-                    <div class="table-item">
-                        <div class="title medium-copy">title</div>
-                        <div class="name-item">UNDP Resident Representative</div>
+                    <div class="cell large-3 medium-4">
+                        <div class="title medium-copy">Title</div>
                     </div>
-                    <div class="table-item">
+                    <div class="cell large-4 medium-5">
                         <div class="title medium-copy">Email</div>
-                        <div class="name-item">
-                            <a href="" class="animated-underline link-item">Nuno.Queiros@undp.org</a>
+                    </div>
+                </div>
+
+                <div class="grid-x grid-margin-x row-element">
+                    <div class="cell large-3 medium-3 row-item">
+                        <div class="title medium-copy show-for-small-only">Name</div>
+                        <div class="text-content">Limya ELTAYEB</div>
+                    </div>
+                    <div class="cell large-3 medium-4 row-item">
+                        <div class="title medium-copy show-for-small-only">Title</div>
+                        <div class="text-content">UNDP Resident Representative</div>
+                    </div>
+                    <div class="cell large-4 medium-5 row-item">
+                        <div class="title medium-copy show-for-small-only">Email</div>
+                        <div class="text-content">
+                            <a href="" class="animated-underline link-item">limya.eltayeb@undp.org</a>
                         </div>
                     </div>
                 </div>
-                <div class="cell large-3 large-offset-0 small-offset-1 small-9 column-table">
-                    <div class="table-item">
-                        <div class="title medium-copy">Name</div>
-                        <div class="name-item">Limya ELTAYEB</div>
+
+                <div class="grid-x grid-margin-x row-element">
+                    <div class="cell large-3 medium-3 row-item">
+                        <div class="title medium-copy show-for-small-only">Name</div>
+                        <div class="text-content">Limya ELTAYEB</div>
                     </div>
-                    <div class="table-item">
-                        <div class="title medium-copy">title</div>
-                        <div class="name-item">UNDP Resident Representative</div>
+                    <div class="cell large-3 medium-4 row-item">
+                        <div class="title medium-copy show-for-small-only">Title</div>
+                        <div class="text-content">UNDP Resident Representative</div>
                     </div>
-                    <div class="table-item">
-                        <div class="title medium-copy">Email</div>
-                        <div class="name-item">
-                            <a href="" class="animated-underline link-item">Nuno.Queiros@undp.org</a>
+                    <div class="cell large-4 medium-5 row-item">
+                        <div class="title medium-copy show-for-small-only">Email</div>
+                        <div class="text-content">
+                            <a href="" class="animated-underline link-item">limya.eltayeb@undp.org</a>
                         </div>
                     </div>
                 </div>
+
+                <div class="grid-x grid-margin-x row-element">
+                    <div class="cell large-3 medium-3 row-item">
+                        <div class="title medium-copy show-for-small-only">Name</div>
+                        <div class="text-content">Limya ELTAYEB</div>
+                    </div>
+                    <div class="cell large-3 medium-4 row-item">
+                        <div class="title medium-copy show-for-small-only">Title</div>
+                        <div class="text-content">UNDP Resident Representative</div>
+                    </div>
+                    <div class="cell large-4 medium-5 row-item">
+                        <div class="title medium-copy show-for-small-only">Email</div>
+                        <div class="text-content">
+                            <a href="" class="animated-underline link-item">limya.eltayeb@undp.org</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="grid-x grid-margin-x row-element">
+                    <div class="cell large-3 medium-3 row-item">
+                        <div class="title medium-copy show-for-small-only">Name</div>
+                        <div class="text-content">Limya ELTAYEB</div>
+                    </div>
+                    <div class="cell large-3 medium-4 row-item">
+                        <div class="title medium-copy show-for-small-only">Title</div>
+                        <div class="text-content">UNDP Resident Representative</div>
+                    </div>
+                    <div class="cell large-4 medium-5 row-item">
+                        <div class="title medium-copy show-for-small-only">Email</div>
+                        <div class="text-content">
+                            <a href="" class="animated-underline link-item">limya.eltayeb@undp.org</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
