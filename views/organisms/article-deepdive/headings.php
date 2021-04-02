c0f2174d904147068315d2b54b844b2cda3dc472
Jura Khrapunov <jura@khrapunov.com>
2021-04-02 12:35:51 -0400

    Add H5 subheader, resolves #2

--- views/organisms/article-deepdive/headings.php
+++ views/organisms/article-deepdive/headings.php
@@ -4,11 +4,17 @@
     <div class="grid-x overflow-hidden">
 
         <div class="cell small-12 large-9 large-offset-1">
-            <h2 class="article-title heading h2">
+            <h2 class="heading h2">
                 <?= $title ?? '' ?>
             </h2>
+        </div>
 
+        <div class="cell small-12 large-7 large-offset-1">
+            <h5 class="heading h5">
+                <?= $subtitle ?? '' ?>
+            </h5>
             <div class="posted-date tag">Posted on <?= $postedDate ?? '' ?></div>
         </div>
+
     </div>
 </section>
