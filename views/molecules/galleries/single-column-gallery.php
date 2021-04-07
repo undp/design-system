9acaa4f2e1ddc7490c3aa1886d080db8fad1deeb
Carlos Bazana <bazana@maniak.com.mx>
2021-04-07 13:08:57 -0500

    Issue #1400 - Refactor parallax gallery images into smaller modules

---
+++ views/molecules/galleries/single-column-gallery.php
@@ -0,0 +1,5 @@
+
+<div class="cell single-column-gallery <?= $classes ?? '' ?>">
+    <img src="<?= $image1 ?>" alt="">
+    <img src="<?= $image2 ?>" alt="">
+</div>
--- views/pages/molecules/galleries.php
+++ views/pages/molecules/galleries.php
@@ -16,7 +16,24 @@ $imagePath = '/assets/images/placeholder/galleries';
         <div class="grid-x grid-margin-x">
 
             <div class="cell medium-10 medium-offset-1 component-categories">
-                
+                <h5 class="heading h5">Image Gallery - Single Column</h5>
+
+                <div class="grid-x">
+                    <?php
+                        View::render('molecules/galleries/single-column-gallery', [
+                            'classes' => 'small-6 medium-3',
+                            'image1' => "$imagePath/gallery-parallax03@2x.jpg",
+                            'image2' => "$imagePath/gallery-image-tall@2x.jpg",
+                        ]);
+                    ?>
+                </div>
+            </div>
+
+
+            <div class="cell medium-5 medium-offset-1 component-categories">
+                <h5 class="heading h5">Image Gallery - Double Column</h5>
+
+                <?php View::render('molecules/galleries/double-column-gallery'); ?>
             </div>
 
         </div>
