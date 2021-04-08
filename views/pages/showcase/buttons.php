fb5e198d200fff7b944e6905d2d424fc103b65a3
Carlos Bazana <bazana@maniak.com.mx>
2021-04-08 11:48:09 -0500

    Issue #1395 - Fix buttons alignment + add tooltip to separator variations

--- views/pages/showcase/buttons.php
+++ views/pages/showcase/buttons.php
@@ -96,68 +96,94 @@ use helpers\View;
             <div class="cell medium-10 medium-offset-1 large-8 large-offset-2 component-categories">
                 <h5 class="heading h5">Breaks And Lines</h5>
 
-                <hr class="separator-line gray"><br>
-                <hr class="separator-line"><br>
-                <hr class="separator-line medium"><br>
-                <hr class="separator-line large"><br>
-                <hr class="separator-line medium red"><br>
+                <div class="displays-inline" data-tooltip>
+                    <hr class="separator-line gray">
+                    <span class="tooltip" style="top: auto;">Regular separator - Gray</span>
+                </div>
+                <br>
+                <div class="displays-inline" data-tooltip>
+                    <hr class="separator-line">
+                    <span class="tooltip" style="top: auto;">Regular separator</span>
+                </div>
+                <br>
+                <div class="displays-inline" data-tooltip>
+                    <hr class="separator-line medium">
+                    <span class="tooltip" style="top: auto;">Medium Sized Separator</span>
+                </div>
+                <br>
+                <div class="displays-inline" data-tooltip>
+                    <hr class="separator-line large">
+                    <span class="tooltip" style="top: auto;">Large Sized Separator</span>
+                </div>
+                <br>
+                <div class="displays-inline" data-tooltip>
+                    <hr class="separator-line medium red">
+                    <span class="tooltip" style="top: auto;">Medium Sized Separator - Red</span>
+                </div>
+                <br>
             </div>
 
-            <div class="cell medium-4 medium-offset-1 large-3 large-offset-2 component-categories">
-                <h5 class="heading h5">Language Select</h5>
+            <div class="cell medium-10 medium-offset-1 large-8 large-offset-2 component-categories">
 
-                <?php View::render('molecules/buttons/language-dropdown'); ?>
+                <div class="grid-x grid-margin-x">
+                    <div class="cell small-6 medium-4 large-3 component-categories">
+                        <h5 class="heading h5">Language Select</h5>
+
+                        <?php View::render('molecules/buttons/language-dropdown'); ?>
+                    </div>
+
+                    <div class="cell small-6 medium-4 large-3 component-categories">
+                        <h5 class="heading h5">Hamburger Menu</h5>
+
+                        <?php View::render('molecules/buttons/hamburger-menu'); ?>
+                    </div>
+
+                    <div class="cell small-6 medium-4 large-3 component-categories">
+                        <h5 class="heading h5">Close Out</h5>
+
+                        <?php View::render('molecules/buttons/close-out'); ?>
+                    </div>
+
+                    <div class="cell small-6 medium-4 large-3 component-categories">
+                        <h5 class="heading h5">Check Boxes</h5>
+
+                        <?php
+                        View::render('molecules/buttons/checkbox-item', [
+                            'value' => 'example-checkbox'
+                        ]);
+                        ?>
+                    </div>
+
+                    <div class="cell small-6 medium-4 large-3 component-categories">
+                        <h5 class="heading h5">Radio Buttons</h5>
+
+                        <?php
+                        View::render('molecules/buttons/radio-button', [
+                            'value' => 'example-rb'
+                        ]);
+                        ?>
+                    </div>
+
+                    <div class="cell small-6 medium-4 large-3 component-categories">
+                        <h5 class="heading h5">Download Button</h5>
+
+                        <?php View::render('molecules/buttons/download-btn'); ?>
+                    </div>
+
+                    <div class="cell small-6 medium-4 large-3 component-categories">
+                        <h5 class="heading h5">Global</h5>
+
+                        <?php View::render('molecules/buttons/global-icon'); ?>
+                    </div>
+
+                    <div class="cell small-6 medium-4 large-3 component-categories">
+                        <h5 class="heading h5">Search</h5>
+
+                        <?php View::render('molecules/buttons/search-icon'); ?>
+                    </div>
+                </div>
             </div>
 
-            <div class="cell medium-4 large-3 component-categories">
-                <h5 class="heading h5">Hamburger Menu</h5>
-
-                <?php View::render('molecules/buttons/hamburger-menu'); ?>
-            </div>
-
-            <div class="cell medium-4 large-3 component-categories">
-                <h5 class="heading h5">Close Out</h5>
-
-                <?php View::render('molecules/buttons/close-out'); ?>
-            </div>
-
-            <div class="cell medium-4 medium-offset-1 large-3 large-offset-2 component-categories">
-                <h5 class="heading h5">Check Boxes</h5>
-
-                <?php
-                    View::render('molecules/buttons/checkbox-item', [
-                        'value' => 'example-checkbox'
-                    ]);
-                ?>
-            </div>
-
-            <div class="cell medium-4 large-3 component-categories">
-                <h5 class="heading h5">Radio Buttons</h5>
-
-                <?php
-                    View::render('molecules/buttons/radio-button', [
-                        'value' => 'example-rb'
-                    ]);
-                ?>
-            </div>
-
-            <div class="cell medium-4 large-3 component-categories">
-                <h5 class="heading h5">Download Button</h5>
-
-                <?php View::render('molecules/buttons/download-btn'); ?>
-            </div>
-
-            <div class="cell medium-4 medium-offset-1 large-3 large-offset-2 component-categories">
-                <h5 class="heading h5">Global</h5>
-
-                <?php View::render('molecules/buttons/global-icon'); ?>
-            </div>
-
-            <div class="cell medium-4 large-3 component-categories">
-                <h5 class="heading h5">Search</h5>
-
-                <?php View::render('molecules/buttons/search-icon'); ?>
-            </div>
         </div>
     </section>
 
