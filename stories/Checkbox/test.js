4afc9bc6771a3f9abcdb75e457c0164f785a1151
harish kumar <harish.kumar@srijan.net>
2021-09-20 18:25:04 +0530

    clean-up extra code

--- stories/Checkbox/test.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('checkmark').onclick = function() { //asign a function
-        console.log("hi")
-        };
-  
-  });
