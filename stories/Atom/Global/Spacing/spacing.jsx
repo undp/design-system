061af8b4ae1f62c404be00c94b4aa79956c02816
pallavikadam-srijan <pallavi.kadam@srijan.net>
2021-09-23 13:47:18 +0530

    Defined spacing format

---
+++ stories/Atom/Global/Spacing/spacing.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import './spacing.scss'
+
+export const Spacing = ({}) => {
+  return (
+    <div className="spacing-wrapper">
+        <div className="spacing__row">
+          <div className="spacing__title Primary">Spacing</div>
+          <div className="spacing__define">
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-01"></div>
+                <div className="spacing__label">spacing-01</div>
+                <div className="spacing__size">2px</div>
+                <div className="spacing__box1"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-02"></div>
+                <div className="spacing__label">spacing-02</div>
+                <div className="spacing__size">4px</div>
+                <div className="spacing__box2"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-03"></div>
+                <div className="spacing__label">spacing-03</div>
+                <div className="spacing__size">8px</div>
+                <div className="spacing__box3"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-04"></div>
+                <div className="spacing__label">spacing-04</div>
+                <div className="spacing__size">12px</div>
+                <div className="spacing__box4"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-05"></div>
+                <div className="spacing__label">spacing-05</div>
+                <div className="spacing__size">16px</div>
+                <div className="spacing__box5"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-06"></div>
+                <div className="spacing__label">spacing-06</div>
+                <div className="spacing__size">24px</div>
+                <div className="spacing__box6"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-07"></div>
+                <div className="spacing__label">spacing-07</div>
+                <div className="spacing__size">32px</div>
+                <div className="spacing__box7"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-08"></div>
+                <div className="spacing__label">spacing-08</div>
+                <div className="spacing__size">40px</div>
+                <div className="spacing__box8"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-09"></div>
+                <div className="spacing__label">spacing-09</div>
+                <div className="spacing__size">48px</div>
+                <div className="spacing__box9"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-10"></div>
+                <div className="spacing__label">spacing-10</div>
+                <div className="spacing__size">64px</div>
+                <div className="spacing__box10"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-11"></div>
+                <div className="spacing__label">spacing-11</div>
+                <div className="spacing__size">80px</div>
+                <div className="spacing__box11"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-12"></div>
+                <div className="spacing__label">spacing-12</div>
+                <div className="spacing__size">96px</div>
+                <div className="spacing__box12"></div>
+            </div>
+            <div className="spacing__name">
+                <div className="spacing__card p-spacing-13"></div>
+                <div className="spacing__label">spacing-13</div>
+                <div className="spacing__size">160px</div>
+                <div className="spacing__box13"></div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+};
