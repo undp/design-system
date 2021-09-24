fc0148d2950ce80db74ac924db229df32ca740b9
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-09-24 15:20:53 +0530

    Lists atom conflicts resolve

---
+++ stories/Atom/Lists/Descriptionlist.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './lists.scss';
+
+export const Descriptionlist = ({ data }) => {
+  return (
+    <dl>
+      {data.map(function(item) {
+        return (
+          <>
+          <dt>{item.label}</dt>
+          <dd>{item.text}</dd>
+          </>
+        )})}
+    </dl>
+  );
+};
