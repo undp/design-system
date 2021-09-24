fb42b128906a7cbcc63da9bbc36875c7be03401f
Surbhi Gupta <sgupta9091@gmail.com>
2021-09-24 13:14:02 +0530

    UND-77 Updating the storybook logo

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'UNDP storybook',
+  brandUrl: 'https://www.undp.org/',
+  brandImage: './images/logo.svg',
+});
