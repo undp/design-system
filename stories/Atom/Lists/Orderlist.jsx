78d9aedb0127429f9402dd28f4443e3ffbbfd539
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-09-24 15:20:53 +0530

    Lists atom conflicts resolve

---
+++ stories/Atom/Lists/Orderlist.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './lists.scss'
+
+export const Orderlist = ({ data }) => {
+  return (
+    <ol>
+      {data.map(function(item) {
+        return (
+        <li className="list-item">{item.text}</li>
+        )})}
+    </ol>
+  );
+};
