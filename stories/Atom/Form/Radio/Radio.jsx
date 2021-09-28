30c0c224936c12c85c5e6505fe3935b00b2037b7
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-09-28 12:58:26 +0530

    Fixed Radio Button Checked

--- stories/Atom/Form/Radio/Radio.jsx
+++ stories/Atom/Form/Radio/Radio.jsx
@@ -3,9 +3,15 @@ import './radio.scss';
 
 export const Radio = ({ label }) => {
   return (
-    <div className="radio-wrapper">
-      <input type="radio" id="edit-radio" className="custom-control-input"/>
-      <label id="checkmark" for="edit-radio" name="radioone" className="custom-control-label">{ label }</label>
-    </div>
+    <>
+      <div className="radio-wrapper">
+        <input type="radio" id="undp" name="fav_language" value="undp" className="custom-control-input"/>
+        <label for="undp" className="custom-control-label"><span className="radio_border"></span>{ label }</label>
+      </div>
+      <div className="radio-wrapper">
+        <input type="radio" id="undp2" name="fav_language" value="undp2" className="custom-control-input"/>
+        <label for="undp2" className="custom-control-label"><span className="radio_border"></span>{ label }</label>
+      </div>
+    </>
   );
 };
