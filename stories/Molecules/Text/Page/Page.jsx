48ebf0de7b0bff9736151f2221ed9f9c82ebfb01
harish kumar <harish.kumar@srijan.net>
2021-09-28 17:22:19 +0530

    UND-15 page block white

--- stories/Molecules/Text/Page/Page.jsx
+++ stories/Molecules/Text/Page/Page.jsx
@@ -3,9 +3,9 @@ import './page.scss';
 import { H4 } from '../../../Atom/Base-typography/Heading/h4';
 import { H2 } from '../../../Atom/Base-typography/Heading/h2';
 
-export const Page = ({ headerText, countryText}) => {
+export const Page = ({ headerText, countryText, style }) => {
   return (
-    <div className='Page-title' > 
+    <div className={['Page-title',`Page-title--${style}`].join(' ')}  > 
       <H4 label={headerText} />
       <H2 label={countryText} />
     </div>
