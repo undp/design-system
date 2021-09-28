79915c075732aaa6161b28c44762d7906ff4dd2e
pallavikadam-srijan <pallavi.kadam@srijan.net>
2021-09-28 15:37:49 +0530

    Defined text inputs

---
+++ stories/Atom/Input-group/textarea_input.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './input_group.scss';
+import { Error_caption } from './error';
+import { Label_caption } from './label';
+
+export const Textareacomponent = ({ id, inputfor, placeholder, required, mode, labelText, errorText, disabled }) => {
+  return (
+    <form className= 'input-group'>
+      <div className={[`label__message ${mode}`]}>
+        <Label_caption label= { labelText } />
+      </div>
+      <textarea
+        id={id}
+        inputfor="textarea"
+        name="first-name"
+        placeholder={placeholder}
+        cols="40"
+        rows="10"
+        wrap="soft"
+        labelText={labelText}
+        disabled={disabled}
+        required={required}
+        className={[`input-type ${inputfor}-field ${mode}`].join(' ')}
+      />
+      <div className={[`${mode}`]}>
+        <Error_caption label={ errorText}/>
+      </div>
+    </form>
+  );
+};
