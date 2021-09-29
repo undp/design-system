c69d8c36aa9895c78d1258f0ff323f57e0ca88b5
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-09-29 15:15:09 +0530

    Small copy and Project details define

---
+++ stories/Molecules/Text/ProjectDetails/ProjectDetails.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './projectdetails.scss';
+
+export const Projectdetails = ({ startName, startText, endName, endText, statusName, statusText }) => {
+    return (
+        <div className='projectdata'>
+            <div className='projectdata__col'>
+                <span className="projectdata__label">{startName}</span>
+                <span className="projectdata__text">{startText}</span>
+            </div>
+            <div className='projectdata__col'>
+                <span className="projectdata__label">{endName}</span>
+                <span className="projectdata__text">{endText}</span>
+            </div>
+            <div className='projectdata__col'>
+                <span className="projectdata__label">{statusName}</span>
+                <span className="projectdata__text">{statusText}</span>
+            </div>
+        </div>
+    );
+};
