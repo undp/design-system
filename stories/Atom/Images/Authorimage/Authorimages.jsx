f4ecee5279278157a95b0f4f38d4bc247e9781c0
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-09-30 21:30:00 +0530

    Image atom with single jsx file

--- stories/Atom/Images/Authorimage/Authorimages.jsx
+++ stories/Atom/Images/Authorimage/Authorimages.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './authorimage.scss';
 import user from '../../../assets/images/user.svg';
 
-export const Large = ({ src, alt }) => {
+export const Authorimg = ({ src, alt, size }) => {
     const image = {
         src: user,
         alt: 'user',
     }
     return (
-        <div className="author__img author__img--large">
+        <div className={['author__img', `author__img--${size}`].join(' ')}>
             <img src={image.src} alt={image.alt} />
         </div>
     );
