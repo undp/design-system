e8cc41a68a8130534b28852398097385bc951557
harish kumar <harish.kumar@srijan.net>
2021-09-30 18:05:25 +0530

    UND-100-Accessibility-Issue

--- stories/Molecules/Text/Post/Post.jsx
+++ stories/Molecules/Text/Post/Post.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+//import React from 'react';
+import React, { useState } from 'react';
 import './post.scss';
 import { H2 } from '../../../Atom/Base-typography/Heading/h2';
 import { H5 } from '../../../Atom/Base-typography/Heading/h5';
@@ -8,8 +9,8 @@ export const Post = ({ headerText, countryText, descriptionText }) => {
   return (
     <div className='post-wrapper' >  
      <H2 label={headerText} />
-     <H5 label={countryText} />
-     <H6 label={descriptionText} />
+     <H6 label={descriptionText} />   
+     {countryText && <H5 label={countryText} />}
     </div>
   );
 };
