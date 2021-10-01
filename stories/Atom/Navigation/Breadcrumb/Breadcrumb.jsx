a61863fd87cd59c87a312e28f2b8844cd5b0cdeb
harish kumar <harish.kumar@srijan.net>
2021-10-01 18:35:23 +0530

    Breadcrumb jsx files

--- stories/Atom/Navigation/Breadcrumb/Breadcrumb.jsx
+++ stories/Atom/Navigation/Breadcrumb/Breadcrumb.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './breadcrumb.scss';
 
-export const Breadcrumb = ({ style, text, ...props }) => {
+export const Breadcrumb = ({ text }) => {
   return (
-    <span className={['breadcrumb_item',`breadcrumb_item--${style}`].join(' ')}   {...props}><a href="#">{text}</a></span>
+    <a href="#" className={'breadcrumb__item'}>{text}</a>
   );
 };
--- stories/Atom/Navigation/Breadcrumb/BreadcrumbLast.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './breadcrumb.scss';
-
-export const BreadcrumbLast = ({ style, text, ...props }) => {
-  return (
-      <span className={['breadcrumb_item',`breadcrumb_item--${style}`].join(' ')}   {...props}>{text}</span>
-  );
-};
