c7d962e4184ed4a1ecacea419fa02f5d9a32f098
harish kumar <harish.kumar@srijan.net>
2021-10-01 18:07:32 +0530

    UND-114-logo-jsx

--- stories/Atom/Global/Logo/logo.jsx
+++ stories/Atom/Global/Logo/logo.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import logo from '../../../assets/images/logo.svg'
 import './logo.scss';
 
-export const Logo = ({ src, alt,  ...props }) => {
-  const image = {
-    src: logo,
-    alt: 'UNDP Logo',
-  };
+export const Logo = ({ src, alt }) => {
   return (
-    <img src={image.src} alt={image.alt} className="logo"/>
+    <img src={src} alt={alt} className="logo"/>
   );
 };
