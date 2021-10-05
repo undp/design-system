efa5a8dbf2696689106f0e6b2d857c2d32723ec7
harish kumar <harish.kumar@srijan.net>
2021-10-05 15:46:57 +0530

    USD-3 grid system layout

--- stories/Molecules/Text/SectionHeader/SectionHeader.jsx
+++ stories/Molecules/Text/SectionHeader/SectionHeader.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './sectionheader.scss';
 import { Heading } from '../../../Atom/Base-typography/Heading/Heading';
 
 export const SectionHeader = ({ headerText, descriptionText}) => {
   return (
-    <div className='header-wrapper' >  
+    <div className='header__wrapper' >  
         <Heading type='2' label={headerText} />
         <Heading type='4' label={descriptionText} />
     </div>
