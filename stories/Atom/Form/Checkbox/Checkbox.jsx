20d7dbe4281703fa49b9a931ef1a9fd1bed6e959
Surbhi Gupta <sgupta9091@gmail.com>
2021-10-11 13:35:37 +0530

    UND-80 Implementing Multiselect

--- stories/Atom/Form/Checkbox/Checkbox.jsx
+++ stories/Atom/Form/Checkbox/Checkbox.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './checkbox.scss';
 
-export const Checkbox = ({ label }) => {
+export const Checkbox = ({ label, value, id }) => {
   return (
     <div className="checkbox-wrapper">
-      <input type="checkbox" id="edit-checkbox" className="custom-control-input"/>
-      <label id="checkmark" for="edit-checkbox" name="checkboxone" className="custom-control-label">{ label }</label>
+      <input type="checkbox" id={id} className="custom-control-input" value={ value } />
+      <label htmlFor={id} name="checkboxone" className="custom-control-label">{ label }</label>
     </div>
   );
 };
