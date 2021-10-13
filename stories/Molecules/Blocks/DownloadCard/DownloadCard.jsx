41d5ee28492652c4f1a5d9a467869c1b05d4b199
harish kumar <harish.kumar@srijan.net>
2021-10-13 13:45:15 +0530

    download cards

---
+++ stories/Molecules/Blocks/DownloadCard/DownloadCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './DownloadCard.scss';
+import '../../../assets/scss/_grid.scss';
+import { Publicationthumb } from '../../../Atom/Cards/Publication-thumbnail/PublicationThumbnail';
+import { Cardthumb } from '../../../Atom/Cards/Card-thumbnail/CardThumbnail';
+import { Icondownload } from '../../../Atom/Global/Icons/download';
+
+export const DownloadCard = ({ image, image2, title, format, download, ...args }) => {
+  return (
+    <div class="download-card">
+      {image && <Publicationthumb />}
+      {image2 && <Cardthumb />}
+      <div class="download-card__description">
+        <div className="download-card__title">{title}</div>
+        <div className="download-card__format">{format}</div>
+        <div className="download-card__download">{download} <Icondownload /></div>
+      </div>
+    </div>
+  );
+};
