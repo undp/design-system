35ef9a1be0cdb7f18bf81b6042f544107af9fe84
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-10-28 13:39:45 +0530

    Fixed hero organisms spacing

--- stories/Organism/Blocks/Heros/Global-Homepage-Hero/HomepageHero.jsx
+++ stories/Organism/Blocks/Heros/Global-Homepage-Hero/HomepageHero.jsx
@@ -1,33 +1,38 @@
 import React, { useEffect } from 'react';
-import "./homepagehero.scss";
+import './homepagehero.scss';
 import viewport from '../../../../assets/js/viewport';
-import BackgroundImg from "../../../../assets/images/field.jpg";
-import BackgroundVideo from "../../../../assets/video/video_sample.mp4";
-import { Video } from "../../../../Atom/Video/Video";
-import { Heading } from '.././../../../Atom/Base-typography/Heading/Heading';
+import BackgroundImg from '../../../../assets/images/field.jpg';
+import BackgroundVideo from '../../../../assets/video/video_sample.mp4';
+import { Video } from '../../../../Atom/Video/Video';
+import { Heading } from '../../../../Atom/Base-typography/Heading/Heading';
 import { CtaButton } from '../../../../Atom/Buttons-and-labels/Cta_button/CtaButton';
 
-export const Homepagehero = ({ title, content, button, variant, ...args }) => {
+export const Homepagehero = ({
+  title, content, button, variant, ...args
+}) => {
   useEffect(() => {
-    viewport('.hero__title');
-    viewport('.hero__subtitle');
-  }, [])
+    viewport('.hero-wrapper__title');
+    viewport('.hero-wrapper__subtitle');
+  }, []);
   return (
-    <div className='hero__wrapper'>
-      {variant === 'video' ?
-        <div className='hero__background'>
-          <Video src={BackgroundVideo} width='100%' height='100%'/>
-        </div>
-        :
-        <div className="hero__background overlay-grey">
-          <img src={BackgroundImg} alt={BackgroundImg} />
-        </div>}
-      <div className="hero__container">
+    <div className="hero-wrapper">
+      {variant === 'video'
+        ? (
+          <div className="hero-wrapper__background">
+            <Video src={BackgroundVideo} width="100%" height="100%" />
+          </div>
+        )
+        : (
+          <div className="hero-wrapper__background overlay-grey">
+            <img src={BackgroundImg} alt={BackgroundImg} />
+          </div>
+        )}
+      <div className="hero-wrapper__container">
         <div className="grid-x">
-          <div className="cell large-7 medium-9 hero__content"  id ="header">
-            <Heading type='1' className='hero__title color-white left-right' label={title}/>
-            <Heading type='4' className='hero__subtitle color-white left-right' label={content} />
-            <CtaButton label1={button}/>
+          <div className="cell large-7 medium-9 hero-wrapper__content" id="header">
+            <Heading type="1" className="hero-wrapper__title color-white left-right" label={title} />
+            <Heading type="4" className="hero-wrapper__subtitle color-white left-right" label={content} />
+            <CtaButton label1={button} />
           </div>
         </div>
       </div>
@@ -36,5 +41,5 @@ export const Homepagehero = ({ title, content, button, variant, ...args }) => {
 };
 
 Homepagehero.defaultProps = {
-  variant: "image",
+  variant: 'image',
 };
--- stories/Organism/Blocks/Heros/Internal-Page-Heros/PageHeroOption.jsx
+++ stories/Organism/Blocks/Heros/Internal-Page-Heros/PageHeroOption.jsx
@@ -1,34 +1,36 @@
-import React, { useEffect } from "react";
-import "./pagehero.scss";
-import viewport from "../../../../assets/js/viewport";
-import BackgroundImg from "../../../../assets/images/garden.jpg";
-import BackgroundVideo from "../../../../assets/video/video_sample.mp4";
-import { Video } from "../../../../Atom/Video/Video";
-import { Heading } from ".././../../../Atom/Base-typography/Heading/Heading";
-import { Breadcrumbcomponent } from "../../../../Molecules/Navbar/Breadcrumb/Breadcrumb-link";
+import React, { useEffect } from 'react';
+import './pagehero.scss';
+import viewport from '../../../../assets/js/viewport';
+import BackgroundImg from '../../../../assets/images/garden.jpg';
+import BackgroundVideo from '../../../../assets/video/video_sample.mp4';
+import { Video } from '../../../../Atom/Video/Video';
+import { Heading } from '../../../../Atom/Base-typography/Heading/Heading';
+import { Breadcrumbcomponent } from '../../../../Molecules/Navbar/Breadcrumb/Breadcrumb-link';
 
-export const PageHeroOption = ({ data, title, content, active, variant }) => {
+export const PageHeroOption = ({
+  data, title, content, active, variant,
+}) => {
   useEffect(() => {
-    viewport(".pagehero__option_title");
-    viewport(".pagehero__option_subtitle");
+    viewport('.pagehero-option__title');
+    viewport('.pagehero-option__subtitle');
   }, []);
   return (
-    <div className="pagehero__option">
+    <div className="pagehero-option">
       <div className="grid-x">
         <div className="cell large-5 medium-6">
-          <div className="page__hero_content">
+          <div className="pagehero__content">
             <Breadcrumbcomponent data={data} active={active} />
-            <Heading type="4" className="pagehero__option_title left-right" label={content}/>
-            <Heading type="2" className="pagehero__option_subtitle left-right" label={title}/>
+            <Heading type="4" className="pagehero-option__title left-right" label={content} />
+            <Heading type="2" className="pagehero-option__subtitle left-right" label={title} />
           </div>
         </div>
         <div className="cell large-7 medium-6 hero_right">
-          {variant === "video" ? (
-            <div className="hero_right_box">
-              <Video src={BackgroundVideo} width='100%' height='100%'/>
+          {variant === 'video' ? (
+            <div className="pagehero_right_box">
+              <Video src={BackgroundVideo} width="100%" height="100%" />
             </div>
           ) : (
-            <div className="hero_right_box">
+            <div className="pagehero_right_box">
               <img src={BackgroundImg} alt={BackgroundImg} />
             </div>
           )}
