b1957dfb981f4a35e92fc02cb13a1d137a571d30
pallavikadam-srijan <pallavi.kadam@srijan.net>
2021-10-29 11:59:52 +0530

    Added text organisms

--- stories/Molecules/Navbar/Breadcrumb/Breadcrumb-link.jsx
+++ stories/Molecules/Navbar/Breadcrumb/Breadcrumb-link.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './breadcrumb-link.scss';
+import viewport from '../../../assets/js/viewport';
 import { Breadcrumb } from '../../../Atom/Navigation/Breadcrumb/Breadcrumb';
 
 export const Breadcrumbcomponent = ({ data, active }) => {
+  useEffect(() => {
+    viewport('.breadcrumb__wrapper');
+  }, []);
   const lastIndex = data.length - 1;
   return (
-    <ul className={['breadcrumb__wrapper', `breadcrumb__wrapper--${active}`].join(' ')}>
+    <ul className={['breadcrumb__wrapper left-right', `breadcrumb__wrapper--${active}` ].join(' ')}>
       {data.map((item, i) => {
         if (i === lastIndex) {
           return (
--- stories/Molecules/Text/Post/Post.jsx
+++ stories/Molecules/Text/Post/Post.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import './post.scss';
-import { Heading } from '../../../Atom/Base-typography/Heading/Heading';
+import React, { useEffect } from "react";
+import "./post.scss";
+import viewport from "../../../assets/js/viewport";
+import { Heading } from "../../../Atom/Base-typography/Heading/Heading";
 
-export const Post = ({ headerText, countryText, descriptionText }) => (
-  <div className="post-wrapper">
-    <Heading type="2" label={headerText} />
-    {countryText && <Heading type="5" label={countryText} />}
-    <Heading type="6" label={descriptionText} />
-  </div>
-);
+export const Post = ({ headerText, countryText, descriptionText }) => {
+  useEffect(() => {
+    viewport(".post-wrapper");
+  }, []);
+  return (
+    <div className="post-wrapper left-right">
+      <Heading type="2" label={headerText} />
+      {countryText && <Heading type="5" label={countryText} />}
+      <Heading type="6" label={descriptionText} />
+    </div>
+  );
+};
