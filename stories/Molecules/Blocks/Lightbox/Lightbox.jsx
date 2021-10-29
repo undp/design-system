74b568f027e0acd6f87b1d7ce6c9ea4760bdc283
harish kumar <harish.kumar@srijan.net>
2021-10-29 16:45:34 +0530

    modal light box blank

---
+++ stories/Molecules/Blocks/Lightbox/Lightbox.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./lightbox.scss";
+export const Lightbox = ({}) => (
+  <div id="myModal" className="modal">   
+    <div className="modal-content">
+    </div>
+  </div>
+);
