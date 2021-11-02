0675f494bc430c38aaea59f312a2806b772419ec
Shamimkhan <shamim.khan@srijan.net>
2021-11-02 13:36:26 +0530

    und 47

--- stories/Components/StatsSlider/StatsSlider.jsx
+++ stories/Components/StatsSlider/StatsSlider.jsx
@@ -1,16 +1,35 @@
 import React, { } from 'react';
 import { StatsCards } from '../../Molecules/Blocks/StatsCards/StatsCards';
+import '../../assets/js/glideslider';
+
 import './statsslider.scss';
 
 export const StatsSlider = ({data}) => {
   return (
-      <div className="grid-x grid-margin-x stats-card-row">
-        {data.map((item, index) => {
-          return <StatsCards number ={item.numbername} percent={item.percentname} content={item.text} size='md'/>
-        })}
-        {data.map((item, index) => {
-          return <StatsCards number ={item.numbername} percent={item.percentname} content={item.text} size='md'/>
-        })}
+    <div className="stats-card-conatiner">
+      <div className="stats-parallax-container">
+        <div className="grid-x grid-margin-x stats-card-row">
+          {data.map((item, index) => {
+            return <StatsCards number ={item.numbername} percent={item.percentname} content={item.text} size='sm'/>
+          })}
+        </div>
+        <div className="grid-x grid-margin-x stats-card-row">
+          {data.map((item, index) => {
+            return <StatsCards number ={item.numbername} percent={item.percentname} content={item.text} size='sm'/>
+          })}
+        </div>
       </div>
+      <div className="stats-card-slider">
+        <div class="glide">
+          <div class="glide__track" data-glide-el="track">
+            <ul class="glide__slides">
+              <li class="glide__slide">0</li>
+              <li class="glide__slide">1</li>
+              <li class="glide__slide">2</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
