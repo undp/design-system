e0d0b49af000c32923948a55c5be280337e6aaeb
Shamimkhan <shamim.khan@srijan.net>
2021-11-02 17:46:48 +0530

    added js for slider

--- stories/Components/StatsSlider/StatsSlider.jsx
+++ stories/Components/StatsSlider/StatsSlider.jsx
@@ -1,10 +1,11 @@
 import React, { } from 'react';
 import { StatsCards } from '../../Molecules/Blocks/StatsCards/StatsCards';
-import '../../assets/js/glideslider';
-
+import '..//..//assets/js/glideslider.js';
+import '..//..//assets/js/SliderCustom.js';
 import './statsslider.scss';
 
 export const StatsSlider = ({data}) => {
+
   return (
     <div className="stats-card-conatiner">
       <div className="stats-parallax-container">
@@ -20,15 +21,19 @@ export const StatsSlider = ({data}) => {
         </div>
       </div>
       <div className="stats-card-slider">
-        <div class="glide">
-          <div class="glide__track" data-glide-el="track">
-            <ul class="glide__slides">
-              <li class="glide__slide">0</li>
-              <li class="glide__slide">1</li>
-              <li class="glide__slide">2</li>
-            </ul>
-          </div>
+        <div className="glide__bullets" data-glide-el="controls[nav]">
+          <button className="glide__bullet" data-glide-dir="=0"></button>
+          <button className="glide__bullet" data-glide-dir="=1"></button>
+          <button className="glide__bullet" data-glide-dir="=2"></button>
         </div>
+        <div className="glide__track" data-glide-el="track">
+          <ul className="glide__slides">
+            {data.map((item, index) => {
+              return  <li className="glide__slide" key={index}><StatsCards number ={item.numbername} percent={item.percentname} content={item.text} size='sm'/></li>
+            })}
+          </ul>
+        </div>
+        <div className="glide__track" data-glide-el="track">...</div>
       </div>
     </div>
   );
---
+++ stories/assets/js/SliderCustom.js
@@ -0,0 +1,8 @@
+const glide = new Glide('.stats-card-slider', {
+  peek: {
+    before: 0,
+    after: 60
+  }
+});
+
+glide.mount();
