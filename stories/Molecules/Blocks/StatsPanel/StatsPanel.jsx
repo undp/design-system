8bd915b1b6cfee70e53a31990f4671d8aee92ee8
Shamimkhan <shamim.khan@srijan.net>
2021-11-03 17:32:53 +0530

    Stats cards grid

--- stories/Molecules/Blocks/StatsPanel/StatsPanel.jsx
+++ stories/Molecules/Blocks/StatsPanel/StatsPanel.jsx
@@ -3,17 +3,15 @@ import './statspanel.scss';
 import BackgroundImg from '../../../assets/images/card2.jpg';
 
 export const StatsPanel = ({ number, percent, content }) => (
-  <div className="grid-x">
-    <div className="cell medium-3 statspanel">
-      <div
-        className="statspanel__image"
-        style={{ backgroundImage: `url(${BackgroundImg})` }}
-      />
-      <div className="statspanel__box">
-        <span className="statspanel__number">{number}</span>
-        <span className="statspanel__per">{percent}</span>
-        <p>{content}</p>
-      </div>
+  <div className="cell medium-3 statspanel">
+    <div
+      className="statspanel__image"
+      style={{ backgroundImage: `url(${BackgroundImg})` }}
+    />
+    <div className="statspanel__box">
+      <div className="statspanel__number">{number}</div>
+      <div className="statspanel__per">{percent}</div>
+      <p>{content}</p>
     </div>
   </div>
 );
---
+++ stories/Organism/Stats/Stats.jsx
@@ -0,0 +1,15 @@
+import React, { Component, useEffect } from 'react';
+import { StatsPanel } from '../../Molecules/Blocks/StatsPanel/StatsPanel';
+import { StatsFun } from '../../assets/js/Stats-custom';
+import './stats.scss';
+
+export const Stats = ({ data }) => {
+  useEffect(() => {
+    StatsFun('.stats-grid .statspanel');
+  }, []);
+  return (
+    <div className="grid-x stats-grid">
+      {data.map((item, index) => <StatsPanel number={item.number} percent={item.percent} content={item.content} key={index} />)}
+    </div>
+  );
+};
