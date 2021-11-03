c2290ef664db10d30009fb8c9c3553c76a3e27da
pallavikadam-srijan <pallavi.kadam@srijan.net>
2021-11-03 13:57:07 +0530

    Added download row

---
+++ stories/Molecules/DownloadRow/DownloadRow.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './downloadrow.scss';
+import { Checkbox } from '../../Atom/Form/Checkbox/Checkbox';
+
+export const DownloadRow = ({
+  title, subtitle, value, ...args
+}) => (
+  <div className="selectbox-container">
+    <div className="selectbox-container__data">
+      <div className="selectbox-container__data--headertext">{title}</div>
+      <div className="selectbox-container__data--subtext">{subtitle}</div>
+    </div>
+    {args.type === 'Checkbox' && <Checkbox value={value} /> }
+    {args.type === 'Download' && <span className="selectbox-container__chevron download-icon" /> }
+    {args.type === 'External_link' && <span className="selectbox-container__chevron external-link" /> }
+  </div>
+);
+
+DownloadRow.defaultProps = {
+  type: 'Checkbox',
+};
