31204ef27fc54d066748854017bfcdd5be17ee1d
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-11-03 17:41:25 +0530

    Footer Navigation

---
+++ stories/Organism/Footer/Footer.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import './footer.scss';
+import { FooterLogo } from '../../Molecules/FooterNavigation/FooterLogo/FooterLogo';
+import { SignUp } from '../../Molecules/Form/SignUp';
+import { FooterLists } from '../../Molecules/FooterNavigation/FooterLists/FooterLists';
+import { FooterConditions } from '../../Molecules/FooterNavigation/FooterConditions/FooterConditions';
+import { FooterIcons } from '../../Molecules/FooterNavigation/FooterIcons/FooterIcons';
+import { P } from '../../Atom/Base-typography/Paragraph/Paragraph';
+import { accordion } from '../../assets/js/accordion_custom';
+
+export const Footer = ({
+  headerText, headerText2, style, alt, src, srctwo, element, type, required, mode, label, button, errorText, placeholder, menutitle, copyright, data, menudata, ...args
+}) => {
+  useEffect(() => {
+    accordion('.footer__heading', '.footer__panel', 'active');
+  }, []);
+  return (
+    <footer className={['footer', `footer--${args.color}`].join(' ')}>
+      <div className="grid-x">
+        <div className="cell medium-10 footer__wrapper">
+          <div className="grid-x footer__head">
+            <div className="cell medium-5">
+              {args.color === 'default' ? (
+                <FooterLogo src={src} headerText={headerText} headerText2={headerText2} alt={alt} style={args.color} />
+              ) : (
+                <FooterLogo src={srctwo} headerText={headerText} headerText2={headerText2} alt={alt} style={args.color} />
+              )}
+            </div>
+            <div className="cell medium-5 footer__newsletter">
+              <SignUp
+                element={element}
+                type={type}
+                required={required}
+                mode={mode}
+                label={label}
+                button={button}
+                errorText={errorText}
+                placeholder={placeholder}
+                variant={args.color}
+              />
+            </div>
+          </div>
+          <div className=" grid-x grid-margin-x footer__bottom">
+            <FooterLists data={data} headerText={menutitle} variant={args.color} />
+            <FooterLists data={data} headerText={menutitle} variant={args.color} />
+            <FooterLists data={data} headerText={menutitle} variant={args.color} />
+            <FooterLists data={data} headerText={menutitle} variant={args.color} />
+            <div className="cell medium-4">
+              <FooterConditions footerdata={menudata} variant={args.color} />
+              <FooterIcons variant={args.color} />
+            </div>
+          </div>
+          <div className="footer__foot">
+            <P label={copyright} />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+Footer.defaultProps = {
+  color: 'default',
+};
---
+++ stories/Organism/Footer/FooterTwo.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import './footer.scss';
+import { FooterLogo } from '../../Molecules/FooterNavigation/FooterLogo/FooterLogo';
+import { FooterConditions2 } from '../../Molecules/FooterNavigation/FooterConditions2/FooterConditions2';
+import { FooterIcons } from '../../Molecules/FooterNavigation/FooterIcons/FooterIcons';
+import { P } from '../../Atom/Base-typography/Paragraph/Paragraph';
+
+export const Footertwo = ({
+  headerText, headerText2, style, alt, src, srctwo, element, type, required, mode, label, button, errorText, placeholder, menutitle, copyright, data, menudata, ...args
+}) => (
+  <footer className={['footer', `footer--${args.color}`].join(' ')}>
+    <div className="grid-x">
+      <div className="cell medium-10 footer__wrapper">
+        <div className="grid-x footer__head footer__head--two">
+          <div className="cell medium-5">
+            {args.color === 'default' ? (
+              <FooterLogo src={src} headerText={headerText} headerText2={headerText2} alt={alt} style={args.color} />
+            ) : (
+              <FooterLogo src={srctwo} headerText={headerText} headerText2={headerText2} alt={alt} style={args.color} />
+            )}
+          </div>
+          <div className="cell medium-5 show-for-large">
+            <FooterIcons variant={args.color} />
+          </div>
+        </div>
+        <div className=" grid-x grid-margin-x footer__bottom footer__bottom--two">
+          <div className="cell medium-5">
+            <P label={copyright} />
+          </div>
+          <div className="cell medium-7">
+            <FooterConditions2 footerdata2={menudata} variant={args.color} />
+            <div className="show-for-small">
+              <FooterIcons variant={args.color} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+
+Footertwo.defaultProps = {
+  color: 'default',
+};
