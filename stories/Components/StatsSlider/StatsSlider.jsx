ec543b22336b1a2e8dc9370e05ac035fdd5c0361
Shamimkhan <shamim.khan@srijan.net>
2021-11-08 13:48:46 +0530

    add js liberary

--- stories/Components/StatsSlider/StatsSlider.jsx
+++ stories/Components/StatsSlider/StatsSlider.jsx
@@ -1,41 +1,28 @@
 import React, { Component, useEffect } from 'react';
 import { StatsCards } from '../../Molecules/Blocks/StatsCards/StatsCards';
-import { StatsSliderFun } from '../../assets/js/SliderCustom';
+import { StatsSliderFun,CardParallaxFun } from '../../assets/js/SliderCustom';
 import './statsslider.scss';
 
 export const StatsSlider = ({data}) => {
   useEffect(() => {
     StatsSliderFun();
+    CardParallaxFun();
   },[])
   return (
-    <div className="stats-card-conatiner">
-      <div className="stats-parallax-container">
-        <div className="grid-x grid-margin-x stats-card-row">
-          {data.slice(0, 3).map((item, index) => {
-            return <StatsCards number ={item.numbername} percent={item.percentname} content={item.text} size='sm'/>
-          })}
-        </div>
-        <div className="grid-x grid-margin-x stats-card-row">
-          {data.slice(3, 6).map((item, index) => {
-            return <StatsCards number ={item.numbername} percent={item.percentname} content={item.text} size='sm'/>
-          })}
-        </div>
+    <div className="stats-card-slider stats-card-grid stats-card-parallax">
+      <div className="glide__bullets" data-glide-el="controls[nav]">
+        <button className="glide__bullet" data-glide-dir="=0"></button>
+        <button className="glide__bullet" data-glide-dir="=1"></button>
+        <button className="glide__bullet" data-glide-dir="=2"></button>
+        <button className="glide__bullet" data-glide-dir="=3"></button>
+        <button className="glide__bullet" data-glide-dir="=4"></button>
+        <button className="glide__bullet" data-glide-dir="=5"></button>
       </div>
-      <div className="stats-card-slider">
-        <div className="glide__bullets" data-glide-el="controls[nav]">
-          <button className="glide__bullet" data-glide-dir="=0"></button>
-          <button className="glide__bullet" data-glide-dir="=1"></button>
-          <button className="glide__bullet" data-glide-dir="=2"></button>
-          <button className="glide__bullet" data-glide-dir="=3"></button>
-          <button className="glide__bullet" data-glide-dir="=4"></button>
-          <button className="glide__bullet" data-glide-dir="=5"></button>
-        </div>
-        <div className="glide__track" data-glide-el="track">
-          <ul className="glide__slides">
-            {data.map((item, index) => {
-              return  <li className="glide__slide" key={index}><StatsCards number ={item.numbername} percent={item.percentname} content={item.text} size='sm'/></li>
-            })}
-          </ul>
+      <div className="glide__track" data-glide-el="track">
+        <div className="glide__slides">
+          {data.map((item, index) => {
+            return  <div className="glide__slide" key={index}><StatsCards number ={item.numbername} percent={item.percentname} content={item.text} size='sm'/></div>
+          })}
         </div>
       </div>
     </div>
--- stories/assets/js/SliderCustom.js
+++ stories/assets/js/SliderCustom.js
@@ -1,10 +1,41 @@
 export function StatsSliderFun(){
-  new Glide('.stats-card-slider', {
-    autoplay: false,
-    gap:20,
-    peek: {
-      before: 0,
-      after: 70
+  $(window).bind("load resize", function () {
+    if ($(window).width() < 768) {
+      new Glide('.stats-card-slider', {
+        autoplay: false,
+        gap:20,
+        peek: {
+          before: 0,
+          after: 70
+        }
+      }).mount();
     }
-  }).mount();
+  });
+}
+
+export function CardParallaxFun(){
+    if ($(window).width() > 767) {
+      var divs = $(".stats-card-grid .glide__slide");
+      for(var i = 0; i < divs.length; i+=3) {
+          divs.slice(i, i+3).wrapAll("<div class='grid-x'></div>");
+      }
+      gsap.registerPlugin(ScrollTrigger);
+      const showDemo = () => {
+      gsap.utils.toArray('.stats-card-parallax').forEach((n, index) => {
+          const w = n.querySelector('.grid-x');
+          const [x, xEnd] = (index % 2) ? ['100%', (w.scrollWidth - n.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
+          gsap.fromTo(w, {  x  }, {
+          x: xEnd,
+          scrollTrigger: {
+              scrub: !0,
+              ease: "SlowMo",
+              trigger: n,
+              start: "top bottom",
+              end: "bottom top"
+          }
+          });
+      });
+      }
+      showDemo();
+    }
 }
