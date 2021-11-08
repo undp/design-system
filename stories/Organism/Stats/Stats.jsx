52ecf1dc01cb7ff39274ba2a7d5368d712f00eb3
Shamimkhan <shamim.khan@srijan.net>
2021-11-08 14:48:51 +0530

    css remove

--- stories/Organism/Stats/Stats.jsx
+++ stories/Organism/Stats/Stats.jsx
@@ -1,7 +1,6 @@
 import React, { Component, useEffect } from 'react';
 import { StatsPanel } from '../../Molecules/Blocks/StatsPanel/StatsPanel';
 import { StatsFun } from '../../assets/js/Stats-custom';
-import './stats.scss';
 
 export const Stats = ({ data }) => {
   useEffect(() => {
