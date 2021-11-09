d2fedbfb4eb2aaaebad511e8f2ffac3cdd7ce0d2
harish kumar <harish.kumar@srijan.net>
2021-11-09 19:31:08 +0530

    bio card issues

--- stories/Molecules/Blocks/BioCard/BioCard.jsx
+++ stories/Molecules/Blocks/BioCard/BioCard.jsx
@@ -1,60 +1,96 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import './biocard.scss';
-import '../../../assets/scss/_grid.scss';
-import viewport from '../../../assets/js/viewport';
-import { ModalFun } from '../../../assets/js/modal_custom';
-import { Heading } from '../../../Atom/Base-typography/Heading/Heading';
-import { P } from '../../../Atom/Base-typography/Paragraph/Paragraph';
-import { Ctalink } from '../../../Atom/Buttons-and-labels/Cta_link/Cta_link';
-import { Lightbox } from '../../../Molecules/Blocks/Lightbox/Lightbox';
-
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import "./biocard.scss";
+import "../../../assets/scss/_grid.scss";
+import viewport from "../../../assets/js/viewport";
+import { ModalFun } from "../../../assets/js/modal_custom";
+import { Heading } from "../../../Atom/Base-typography/Heading/Heading";
+import { P } from "../../../Atom/Base-typography/Paragraph/Paragraph";
+import { Ctalink } from "../../../Atom/Buttons-and-labels/Cta_link/Cta_link";
+import { Lightbox } from "../../../Molecules/Blocks/Lightbox/Lightbox";
 
 export const size_options = {
-  large: 'large-12 ',
-  medium: 'large-3 bio-card--medium'
+  large: "large-12",
+  medium: "large-3",
 };
 
 export const hover_color_options = {
-  yellow: '',
-  green: 'green',
-  red: 'red',
-  azure: 'azure',
+  yellow: "",
+  green: "green",
+  red: "red",
+  azure: "azure",
 };
 
 export const BioCard = ({
-  contenttile, contentname, descriptionText, type, button, cell, image, image2, descriptionText2, ...args
+  contenttile,
+  contentname,
+  descriptionText,
+  type,
+  button,
+  cell,
+  image,
+  image2,
+  descriptionText2,
+  large,
+  medium,
+  ...args
 }) => {
   useEffect(() => {
-    viewport('.bio-card__description');
+    viewport(".bio-card__description");
     ModalFun();
   }, []);
   return (
     <>
-    <div className="grid-x grid-margin-x">
-      <div className={['cell', `${size_options[`${args.size}`]}`].join(' ')}>
-        <a id='bio-card' className={['bio-card', `${hover_color_options[`${args.hovercolor}`]}`].join(' ')}>
-          <article className="bio-card__content large-7">
-            <div className="bio-card__description left-right ">
-              <Heading type="5" label={contentname} />
-              <P label={descriptionText} />
-            </div>
-            <div className="bio-cta">
-              <Ctalink label={button} variant="Arrow" />
-            </div>
-          </article>
-          <div className="card-thumbnail__image large-5">
-            <img src={image} alt={image} />
-          </div>
-        </a>
+      <div className="grid-x grid-margin-x">
+        <div className={["cell", `${size_options[`${args.size}`]}`].join(" ")}>
+          {args.size === "large" && (
+            <a id="bio-card"
+              className={[ "grid-x grid-margin-x bio-card",`${hover_color_options[`${args.hovercolor}`]}`,].join(" ")}>
+              <article className="bio-card__content large-7">
+                <div className="bio-card__description left-right ">
+                  <Heading type="5" label={contentname} />
+                  <P label={descriptionText} />
+                </div>
+                <div className="bio-cta">
+                  <Ctalink label={button} variant="Arrow" />
+                </div>
+              </article>
+              <div className="card-thumbnail__image large-5">
+                <img src={image} alt={image} />
+              </div>
+            </a>
+          )}
+
+          {args.size === "medium" && (
+            <a id="bio-card"
+              className={[  "bio-card medium", `${hover_color_options[`${args.hovercolor}`]}`, ].join(" ")} >
+              <div className="card-thumbnail__image ">
+                <img src={image} alt={image} />
+              </div>
+              <article className="bio-card__content ">
+                <div className="bio-card__description left-right ">
+                  <Heading type="5" label={contentname} />
+                  <P label={descriptionText} />
+                </div>
+                <div className="bio-cta">
+                  <Ctalink label={button} variant="Arrow" />
+                </div>
+              </article>
+            </a>
+          )}
+        </div>
       </div>
-    </div>
-    <Lightbox image={image2} contentname={contentname} descriptionText={descriptionText} descriptionText2={descriptionText2}   />
-  </>
+      <Lightbox
+        image={image2}
+        contentname={contentname}
+        descriptionText={descriptionText}
+        descriptionText2={descriptionText2}
+      />
+    </>
   );
 };
 
 BioCard.defaultProps = {
-  size: 'medium',
-  hovercolor: 'yellow',
+  size: "medium",
+  hovercolor: "yellow",
 };
