63966ac2487607b05ac5022a61f131db582d46bb
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-11-12 14:18:34 +0530

    Parallax Card Two

--- stories/assets/js/SliderCustom.js
+++ stories/assets/js/SliderCustom.js
@@ -1,50 +1,3 @@
-export function StatsSliderFun(ele, gapele) {
-  $(window).bind('load resize orientationChange', () => {
-    if ($(window).width() <= 767) {
-      let direc = 'ltr';
-      if ($('html').attr('dir') == 'rtl') {
-        direc = 'rtl';
-      }
-      new Glide(ele, {
-        autoplay: false,
-        gap: gapele,
-        direction: direc,
-        peek: {
-          before: 0,
-          after: 70,
-        },
-      }).mount();
-      $('.stats-card-parallax .grid-x .glide__slide,.stats-card-parallax .glide__slide').unwrap();
-    }
-  });
-}
-export function CardParallaxFun() {
-  $(window).bind('load resize orientationchange', () => {
-    if ($(window).width() >= 768) {
-      const cardWrap = $('.stats-card-grid .glide__slide');
-      if (cardWrap.parent('.glide__slides').length) {
-        for (let i = 0; i < cardWrap.length; i += 3) {
-          cardWrap.slice(i, i + 3).wrapAll("<div class='stats-card-parallax'><div class='grid-x'></div></div>");
-        }
-      }
-      gsap.registerPlugin(ScrollTrigger);
-      gsap.utils.toArray('.stats-card-parallax').forEach((n, index) => {
-        const w = n.querySelector('.grid-x');
-        const [x, xEnd] = (index % 2) ? ['100%', ((w.scrollWidth * 1.5) - n.offsetWidth) * -1] : [(w.scrollWidth) * -1, '16%'];
-        gsap.fromTo(w, { x }, {
-          x: xEnd,
-          scrollTrigger: {
-            scrub: !0,
-            ease: 'SlowMo',
-            trigger: n,
-            duration: 5,
-          },
-        });
-      });
-    }
-  });
-}
-
 /* Glide Slider start */
 export function GlideSlider(ele, gapele) {
   let direc = 'ltr';
@@ -62,6 +15,7 @@ export function GlideSlider(ele, gapele) {
   });
   const glidefuntion = function () {
     if ($(window).width() <= 767) {
+      $('.stats-card-parallax .glide__slide, .stats-card-parallax .grid-x').unwrap();
       glide.mount();
     } else {
       glide.destroy();
@@ -73,3 +27,53 @@ export function GlideSlider(ele, gapele) {
     .smartresize(glidefuntion);
 }
 /* Glide Slider end */
+
+/* Parallax Effect Vertical Position start */
+export function ParallaxEffect(ele, yele, triggerclass, scrubtime, cssdiv) {
+  const parallaxfuntion = function () {
+    if ($(window).width() >= 768) {
+      const cardWrap = $('.stats-card-grid .glide__slide');
+      if (cardWrap.parent('.glide__slides').length) {
+        for (let i = 0; i < cardWrap.length; i += 3) {
+          cardWrap.slice(i, i + 3).wrapAll("<div class='stats-card-parallax'><div class='grid-x'></div></div>");
+        }
+      }
+      // Start two row vertical parallax
+      gsap.registerPlugin(ScrollTrigger);
+      gsap.utils.toArray('.stats-card-parallax').forEach((n, index) => {
+        const w = n.querySelector('.grid-x');
+        const [x, xEnd] = (index % 2) ? ['100%', ((w.scrollWidth * 1.5) - n.offsetWidth) * -1] : [(w.scrollWidth) * -1, '20%'];
+        gsap.fromTo(w, { x }, {
+          x: xEnd,
+          scrollTrigger: {
+            scrub: 2,
+            ease: 'SlowMo',
+            trigger: n,
+            duration: 5,
+            start: 'top bottom',
+            end: 'bottom top',
+          },
+        });
+      });
+      // End two row vertical parallax
+      gsap.to(ele, {
+        y: yele,
+        scrollTrigger: {
+          trigger: triggerclass,
+          start: 'top bottom',
+          end: 'bottom top',
+          scrub: scrubtime,
+          ease: 'slow',
+        },
+      });
+    } else {
+      gsap.killTweensOf(ele);
+      $(cssdiv).css({ transform: '' });
+    }
+  };
+  $(window)
+    .off('load orientationChanged', parallaxfuntion)
+    .on('load orientationChanged', parallaxfuntion)
+    .smartresize(parallaxfuntion);
+}
+/* Parallax Effect Vertical Position end */
