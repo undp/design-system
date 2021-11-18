756152cda7b51afa167f864b93c6db035ef74508
harish kumar <harish.kumar@srijan.net>
2021-11-18 11:52:21 +0530

    content card update

--- stories/Molecules/Blocks/ContentCard/ContentCardWithOutImage.jsx
+++ stories/Molecules/Blocks/ContentCard/ContentCardWithOutImage.jsx
@@ -1,38 +1,24 @@
 import React from 'react';
-import './singlecontent.scss';
+import './contentcard.scss';
 import '../../../assets/scss/_grid.scss';
 import { Heading } from '../../../Atom/Base-typography/Heading/Heading';
 import { Ctalink } from '../../../Atom/Buttons-and-labels/Cta_link/Cta_link';
 import { P } from '../../../Atom/Base-typography/Paragraph/Paragraph';
 
-export const image_options = {
-  true: ' ',
+export const emphasize_options = {
+  true: 'card-yellow',
   false: 'card',
 };
 
-export const emphasize_options = {
-  true: 'card-yellow',
-  false: '',
-};
-
-export const SingleContent = ({
-  data, Image, Emphasize, Hovercolors
+export const ContentCardWithOutImage = ({
+  data, Emphasize, Hovercolors
 }) => (
   <div className="grid-x grid-margin-x">
     {data.map((item, index) => (
       <div key={index} className="cell medium-4">
-        <a href={item.link} className={['singlecard', `${image_options[`${Image}`]}`, `${emphasize_options[`${Emphasize}`]}`, `${[`${Hovercolors}`]}`,].join(' ')}>
+        <a href={item.link} className={['contentcard', `${emphasize_options[`${Emphasize}`]}`, `${[`${Hovercolors}`]}`,].join(' ')}>
           <Heading  type="6" label={item.contenttile} />
-          {Image === 'true'
-            ? (
-              <div className="card-thumbnail__image">
-                <img src={item.imgback} alt={item.imgback} />
-              </div>
-            )
-            : (
-              <></>
-            )}
-          <div className="singlecard__caption">
+          <div className="contentcard__caption">
             {Emphasize === 'true'
               ? (
                 <Heading type="4" label={item.contentnametwo} />
@@ -40,11 +26,9 @@ export const SingleContent = ({
               : (
                 <></>
               )}
-
-
-            {Emphasize === 'false' && Image === 'false'
+            {Emphasize === 'false' 
               ? (
-                <div className="singlecard__caption--cart">
+                <div className="contentcard__caption--cart">
                   <Heading type="5" label={item.contentname} />
                   <P label={item.paragraph} />
                 </div>
@@ -52,17 +36,6 @@ export const SingleContent = ({
               : (
                 <></>
               )}
-
-            {Emphasize === 'false' && Image === 'true'
-              ? (
-                <>
-                  <Heading type="5" label={item.contentname} />
-                </>
-              )
-              : (
-                <></>
-              )}
-
             <Ctalink label={item.button} variant="Space" />
           </div>
         </a>
@@ -71,8 +44,7 @@ export const SingleContent = ({
   </div>
 );
 
-SingleContent.defaultProps = {
-  Image: 'true',
-  Emphasize: 'false',
+ContentCardWithOutImage.defaultProps = {
+  Emphasize: 'true',
   Hovercolors: 'yellow'
 };
