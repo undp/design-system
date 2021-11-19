d267420fd40f69e623994fb6d96fbb952feb49e7
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-11-19 15:21:12 +0530

    Fixed Footer countrycard parallax

--- stories/Molecules/FooterNavigation/FooterIcons/FooterIcons.jsx
+++ stories/Molecules/FooterNavigation/FooterIcons/FooterIcons.jsx
@@ -3,11 +3,11 @@ import './footerIcons.scss';
 
 export const FooterIcons = ({ ...args }) => (
   <ul className={['footer__icons', `${args.variant}`].join(' ')}>
-    <li><a href="#" className="facebook" /></li>
-    <li><a href="#" className="linkedIn" /></li>
-    <li><a href="#" className="instragram" /></li>
-    <li><a href="#" className="twitter" /></li>
-    <li><a href="#" className="youtube" /></li>
+    <li><a href="#" className="facebook">facebook</a></li>
+    <li><a href="#" className="linkedIn">linkedIn</a></li>
+    <li><a href="#" className="instragram">instragram</a></li>
+    <li><a href="#" className="twitter">twitter</a></li>
+    <li><a href="#" className="youtube">youtube</a></li>
   </ul>
 );
 
