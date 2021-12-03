6e5f621ea8c3cafbc858903e4ea57f0cb17440f2
pallavikadam-srijan <pallavi.kadam@srijan.net>
2021-12-03 10:21:07 +0530

    Worked on animation and spacing

--- stories/Molecules/Blocks/TweetCard/TweetCard.jsx
+++ stories/Molecules/Blocks/TweetCard/TweetCard.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import viewport from '../../../assets/js/viewport';
 import './tweetcard.scss';
 import '../../../assets/scss/_grid.scss';
 import { Ctalink } from '../../../Components/UIcomponents/Buttons/Cta_link/Cta_link';
 import { P } from '../../../Atom/Base-typography/Paragraph/Paragraph';
 
-export const TweetCard = ({ data }) => (
-  <div className="grid-x grid-margin-x tweet-card-main">
-    {data.map((item, index) => (
-      <div key={index} className="cell medium-3 tweet-card">
-        <a href={item.link} target="_blank" rel="noreferrer">
-          <img src={item.imgback} alt={item.imgback} />
-          <div className="tweet-card__header">
-            <P className="tweet-card__header--title" label={item.contenttile} />
-            <P className="tweet-card__header--subtitle" label={item.contentsubtitle} />
-          </div>
-        </a>
-        <div className="tweet-card__body">
-          <P className="tweet-card__body--description" label={item.descriptiontext} />
-          <P className="tweet-card__body--date" label={item.contentdate} />
+export const TweetCard = ({
+  delaytime,
+  link,
+  imgback,
+  contenttile,
+  contentsubtitle,
+  descriptiontext,
+  contentdate,
+  button,
+}) => {
+  useEffect(() => {
+    viewport('.tweet-card');
+  }, []);
+  return (
+    <div className={`cell medium-3 tweet-card left-right delay-${delaytime}`}>
+      <a href={link} target="_blank" rel="noreferrer">
+        <img src={imgback} alt={imgback} />
+        <div className="tweet-card__header">
+          <P className="tweet-card__header--title" label={contenttile} />
+          <P className="tweet-card__header--subtitle" label={contentsubtitle} />
         </div>
-        <Ctalink label={item.button} variant="Space" />
+      </a>
+      <div className="tweet-card__body">
+        <P className="tweet-card__body--description" label={descriptiontext} />
+        <P className="tweet-card__body--date" label={contentdate} />
       </div>
-    ))}
-  </div>
-);
+      <Ctalink label={button} variant="Space" />
+    </div>
+  );
+};
