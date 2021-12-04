b10f9e0380a9e36679d7fa7f3c14cc577f87077d
KuldevS <6522351+kuldev@users.noreply.github.com>
2021-12-04 15:07:24 +0530

    Update ContentCard.jsx

--- stories/Organism/Blocks/ContentCard/ContentCard.jsx
+++ stories/Organism/Blocks/ContentCard/ContentCard.jsx
@@ -3,14 +3,14 @@ import viewport from '../../../assets/js/viewport';
 import { ContentCardImage } from '../../../Components/UIcomponents/Cards/ContentCard/ContentCardImage';
 import { CtaButton } from '../../../Components/UIcomponents/Buttons/Cta_button/CtaButton';
 
-export const ContentCard = ({ data, buttontype,Hovercolors }) => {
+export const ContentCard = ({ data, buttontype, Hovercolors }) => {
   useEffect(() => {
     viewport('.content-card-container');
   }, []);
   return (
     <div className="content-card-container left-right">
       <ContentCardImage data={data}  Hovercolors={Hovercolors}/>
-      <CtaButton label2={buttontype} variant="secondary" />
+      <CtaButton label2={buttontype} variant="secondary"/>
     </div>
   );
 };
