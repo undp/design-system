641191a64194a548580d26af4dc93e7477e88620
KuldevS <6522351+kuldev@users.noreply.github.com>
2021-12-04 15:33:32 +0530

    Update Pagination.jsx

--- stories/Components/Navigationcomponents/Pagination/Pagination.jsx
+++ stories/Components/Navigationcomponents/Pagination/Pagination.jsx
@@ -3,17 +3,17 @@ import './pagination.scss';
 
 export const Pagination = ({ text, text2 }) => (
   <ul className="pagination-wrapper">
-    <li className="page__item disabled"><a href="#" aria-label="true">Previous</a></li>
+    <li className="page__item disabled"><a href="#" role="button" aria-disabled="true">Previous</a></li>
     <li className="page__item">
       {text}
       <span>
         {' '}
-        <a href="#">1 </a>
+        <a href="#">1</a>
       </span>
       {text2}
       <span>
         {' '}
-        <a href="#">123 </a>
+        <a href="#">123</a>
       </span>
     </li>
      <li className="page__item"><a href="#">Next</a></li>
