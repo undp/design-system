2cc6ad837521c0d6cb2b5d806f163875ce9a53fb
Shishir Phadke <shishir.phadke@srijan.net>
2021-12-14 16:50:18 +0530

    [UND-566][DEVELOP]:Logos - Update names and assets - Remove [contenthash].

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,13 +13,23 @@ module.exports = {
     "@storybook/addon-a11y",
   ],
   webpackFinal: async (config) => {
-    // add SCSS support for CSS Modules
+    // Add SCSS support for CSS Modules.
     config.module.rules.push({
       test: /\.scss$/,
       exclude: /node_modules/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
       include: path.resolve(__dirname, '../'),
     });
+
+    // Remove [contenthash] from image name.
+    config.module.rules.push({
+      test: /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|pdf)$/,
+      loader: "file-loader",
+      options: {
+        name: 'static/media/[name].[ext]',
+      },
+    });
+
     return config;
   }
 }
