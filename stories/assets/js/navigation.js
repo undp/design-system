12ea21a250e31215d4d16a5f7946dc2a91eb573c
ashish dwivedi <ashish.dwivedi@srijan.net>
2021-12-15 14:59:44 +0530

    fixed issues

--- stories/assets/js/navigation.js
+++ stories/assets/js/navigation.js
@@ -7,39 +7,36 @@ export const init = (locale) => {
   const mobLinkWrapper = $('.mob-links');
   const submenuli = $('.submenu li');
   const midNav = $('.mid-nav');
+  const megaMenu = $('.show-mega');
+  
   // calling ajax json
   lang = locale === 'en' ? 'english' : locale;
   getData(lang);
 
   menuItem.on('mouseenter', function () {
     const id = $(this).attr('id');
-    $(document).find(`[menu-item-id='${id}']`).siblings().removeClass('show-mega');
-    $(document).find(`[menu-item-id='${id}']`).addClass('show-mega');
-    $('.show-mega').find('.sub-menu-content:first-child').addClass('active-content');
-    $('.show-mega').find('.sub-menu-content:first-child').siblings().removeClass('active-content');
-    $('.show-mega').find('.submenu li:first-child').addClass('active');
-    $('.show-mega').find('.submenu li:first-child').siblings().removeClass('active');
+    const menuItem = $(document).find(`[menu-item-id='${id}']`);
+    menuItem.siblings().removeClass('show-mega');
+    menuItem.addClass('show-mega');
+    megaMenu.find('.sub-menu-content:first-child').addClass('active-content')
+    .siblings().removeClass('active-content');
+    megaMenu.find('.submenu li:first-child').addClass('active').siblings().removeClass('active');
   });
 
   $('.mega-wrapper').mouseleave(function () {
     $(this).find('.mega-nav-option.show-mega').removeClass('show-mega');
   });
 
-  midNav.mouseleave(() => {
-    // $('.mega-nav-option').removeClass('show-mega');
-  });
 
   $('.mega-wrapper').mouseenter(function () {
     $('.mega-wrapper').find(this).addClass('show-mega');
   });
 
   $('.mega-wrapper').on('mouseenter', '.submenu li', function () {
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
+    $(this).addClass('active').siblings().removeClass('active');
     const id = $(this).attr('id');
-    $(this).parents('.mega-wrapper').find(`[data-id='${id}']`).addClass('active-content');
-    $(this).parents('.mega-wrapper').find(`[data-id='${id}']`).siblings()
-      .removeClass('active-content');
+    $(this).parents('.mega-wrapper').find(`[data-id='${id}']`).addClass('active-content')
+    .siblings().removeClass('active-content');
   });
 
   $(document).on('click', '.mob-links .cta__link', function () {
