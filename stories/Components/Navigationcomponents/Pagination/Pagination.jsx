2aa7de00ed74aea97a14ab8c6930d67db44e0ee2
harish kumar <harish.kumar@srijan.net>
2021-12-16 14:00:03 +0530

    feedback

--- stories/Components/Navigationcomponents/Pagination/Pagination.jsx
+++ stories/Components/Navigationcomponents/Pagination/Pagination.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import './pagination.scss';
 
 export const Pagination = ({ text, text2 }) => (
-  <ul className="pagination-wrapper">
-    <li className="page__item disabled"><a href="#" role="button" aria-disabled="true">Previous</a></li>
-    <li className="page__item">
-      {text}
-      <span>
-        {' '}
-        <a href="#">1</a>
-      </span>
-      {text2}
-      <span>
-        {' '}
-        <a href="#">123</a>
-      </span>
-    </li>
-     <li className="page__item"><a href="#">Next</a></li>
-  </ul>
+  <nav aria-label="Pagination" role="navigation">
+    <ul className="pagination">
+      <li className="disabled"><a href="#" role="button" aria-current="true" aria-label="Previous">Previous</a></li>
+      <li >
+        {text}
+        <span>
+          {' '}
+          <a href="#" aria-label="1">1</a>
+        </span>
+        {text2}
+        <span>
+          {' '}
+          <a href="#" aria-label="123">123</a>
+        </span>
+      </li>
+      <li><a href="#" aria-label="Next">Next</a></li>
+    </ul>
+  </nav>
 );
