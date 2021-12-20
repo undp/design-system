56ff8a79c9e71d408c4752714b7c6368933ad829
Shamimkhan <shamim.khan@srijan.net>
2021-12-20 18:52:27 +0530

    Fixed Progress and Input class issue

--- stories/Atom/Navigation/ProgressBarNavigation/ProgressBarNavigation.jsx
+++ stories/Atom/Navigation/ProgressBarNavigation/ProgressBarNavigation.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import './ProgressBarNavigation.scss';
 import '../../../assets/js/scrollingProgressbar';
 
-export const ProgressBarNavigation = ({ }) => (
-  <div className="progress-container">
+export const ProgressBarNavigation = ({ Type }) =>{ 
+
+let type;
+const types = ["Small"];
+type = types.includes(Type) ? Type.toLowerCase() : '';
+
+return(
+  <div className={['progress-container', `${type}`].join(' ')}>
     <div className="progress-bar" />
   </div>
-);
+)};
