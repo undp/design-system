09bad753f5ed4263ec6ab81676a40b36a5b561b7
neelesh-tippe <neelesh.tippe@srijan.net>
2021-12-22 20:00:36 +0530

    Generic page functional issues resolved

--- stories/Templates/GenericPage/GenericPage.jsx
+++ stories/Templates/GenericPage/GenericPage.jsx
@@ -5,164 +5,170 @@ import { ContentCard } from '../../Organism/Blocks/ContentCard/ContentCard';
 import { Sidebar } from '../../Components/Navigationcomponents/Sidebar/Sidebar';
 import { Heading } from '../../Atom/Typography/Heading/Heading';
 import { P } from '../../Atom/Base-typography/Paragraph/Paragraph';
-import { FluidImageSizeCarousel } from '../../Components/UIcomponents/Carousel/FluidImageSizeCarousel/FluidImageSizeCarousel';
+import {FluidImageSizeCarousel} from '../../Components/UIcomponents/Carousel/FluidImageSizeCarousel/FluidImageSizeCarousel';
 import { Footer } from '../../Organism/Footer/Footer';
 import './genericpage.scss';
 import GlobalHeader from '../../Components/Navigationcomponents/Mainnavigation/GlobalHeader/GlobalHeader';
 
-export const GenericPage = ({
-  herodata,
-  title,
-  content,
-  active,
-  imgsource,
-  altname,
-  topHeading,
-  topParagraph,
-  footerData,
-  sidebarCaptionData,
-  ContentCardData,
-  headingMissionVision,
-  paraMissionVision,
-  headingHistory,
-  paraHistory,
-  headingUNcharter,
-  paraUNcharter,
-  carouselData,
-  logo,
-  logowhite,
-  languageswitcherData,
-  navigationData,
-  rightNavigationData,
-  leftNavigationData,
-  locale,
-  locationData,
-  langSelect,
-  backcaption,
-}) => (
-  <>
-    <GlobalHeader
-      backcaption={backcaption}
-      locale={locale}
-      leftNavigationData={leftNavigationData}
-      navigationData={navigationData}
-      rightNavigationData={rightNavigationData}
-      languageswitcherData={languageswitcherData}
-      locationData={locationData}
-      langSelect={langSelect}
-    />
-    <ProgressBarNavigation Colors="yellow" />
-    <div className="grid-x grid-margin-x">
-      <div className="cell small-12 sidebar-width">
-        <div className="column sidebar-mobile">
-          <Sidebar
-            data={sidebarCaptionData.sidebardata}
-            headerText={sidebarCaptionData.headerText}
-            label={sidebarCaptionData.label}
-            active={sidebarCaptionData.active}
-          />
-        </div>
-      </div>
-    </div>
-    <PageHero
-      data={herodata}
-      title={title}
-      content={content}
-      imgsrc={imgsource}
-      imgalt={altname}
-      active={active}
-    />
-    <div className="grid-container fluid content-bottom">
-      <div className="grid-x grid-margin-x margin-top-72">
-        <div className="cell large-3 small-12 sidebar-width">
-          <div className="column sidebar-desktop">
-            <Sidebar
-              data={sidebarCaptionData.sidebardata}
-              headerText={sidebarCaptionData.headerText}
-              label={sidebarCaptionData.label}
-              active={sidebarCaptionData.active}
-            />
-          </div>
-        </div>
-        <div className="cell large-9 small-12">
-          <div className="grid-x grid-margin-x">
-            <div className="cell large-10 small-12">
-              <div className="column">
-                <Heading type="2" label={topHeading} />
-              </div>
-            </div>
-            <div className="cell large-10 small-12 margin-top-32">
-              <div className="column">
-                <P label={topParagraph} />
-              </div>
-            </div>
-            <div className="cell large-12 small-12 margin-top-80">
-              <div className="column">
-                <FluidImageSizeCarousel data={carouselData} />
-              </div>
-            </div>
-            <div className="cell large-9 small-12 margin-top-48">
-              <div className="column">
-                <Heading type="3" label={headingMissionVision} />
-              </div>
-            </div>
-            <div className="cell large-10 small-12">
-              <div className="column">
-                <P label={paraMissionVision} />
-              </div>
-            </div>
 
-            <div className="cell large-9 small-12 margin-top-48">
-              <div className="column">
-                <Heading type="3" label={headingUNcharter} />
-              </div>
+export const GenericPage = ({
+    herodata, title, content, active,imgsource,altname, topHeading, topParagraph, footerData, sidebarCaptionData, ContentCardData, headingMissionVision,
+    paraMissionVision,
+    headingHistory,
+    paraHistory,
+    headingUNcharter,
+    paraUNcharter,
+    carouselData,
+    logo,
+    logowhite,
+    languageswitcherData,
+    navigationData,
+    rightNavigationData,
+    leftNavigationData,
+    locale,
+    locationData,
+    langSelect,
+    backcaption
+  }) => (
+    <div className="generic-page">
+     <GlobalHeader
+        backcaption={backcaption}
+        locale={locale}
+        leftNavigationData={leftNavigationData}
+        navigationData={navigationData}
+        rightNavigationData={rightNavigationData}
+        languageswitcherData={languageswitcherData}
+        locationData={locationData}
+        langSelect={langSelect}
+      ></GlobalHeader>
+      <ProgressBarNavigation Colors='yellow'/>
+        <div class="grid-x grid-margin-x">
+            <div class="cell small-12 sidebar-width">
+                <div class="column sidebar-mobile">
+                    <Sidebar
+                        data={sidebarCaptionData.sidebardata}
+                        headerText={sidebarCaptionData.headerText}
+                        label={sidebarCaptionData.label}
+                        active={sidebarCaptionData.active}
+                    />
+                </div>
             </div>
-            <div className="cell large-10 small-12">
-              <div className="column">
-                <P label={paraUNcharter} />
-              </div>
-            </div>
-            <div className="cell large-9 small-12 margin-top-48">
-              <div className="column">
-                <Heading type="3" label={headingHistory} />
-              </div>
-            </div>
-            <div className="cell large-10 small-12">
-              <div className="column">
-                <P label={paraHistory} />
-              </div>
-            </div>
-            <div className="column margin-top-48">
-              <ContentCard
-                data={ContentCardData.groupcontentdata}
-                buttontype={ContentCardData.buttonname}
-                Hovercolors='yellow'
-              />
-            </div>
-          </div>
         </div>
-      </div>
+        <PageHero 
+            data={herodata} 
+            title={title} 
+            content={content}
+            imgsrc={imgsource}
+            imgalt={altname}
+            active={active}
+        />
+    
+        <div class="grid-container fluid content-bottom">      
+            <div class="grid-x grid-margin-x margin-top-72">
+                <div class="cell large-3 small-12 sidebar-width">
+                    <div class="column sidebar-desktop">
+                        <Sidebar
+                             data={sidebarCaptionData.sidebardata}
+                             headerText={sidebarCaptionData.headerText}
+                             label={sidebarCaptionData.label}
+                             active={sidebarCaptionData.active}
+                        />
+                    </div>
+                </div>
+                <div class="cell large-9 small-12">
+                    <div class="grid-x grid-margin-x">
+                        <div class="cell large-10 small-12">
+                            <div class="column">
+                                <Heading 
+                                    type='2' 
+                                    label={topHeading} 
+                                />
+                            </div>
+                        </div>
+                        <div class="cell large-10 small-12 margin-top-32">
+                            <div class="column">
+                                <P label={topParagraph} />
+                            </div>
+                        </div>
+                        <div class="cell large-12 small-12 margin-top-80">
+                            <div class="column">
+                                <FluidImageSizeCarousel
+                                    data={carouselData}
+                                />
+                            </div>
+                        </div>                        
+                        <div class="cell large-9 small-12 margin-top-80">
+                            <div class="column">
+                                <Heading 
+                                    type='3' 
+                                    label={headingMissionVision} 
+                                />
+                            </div>    
+                        </div>
+                        <div class="cell large-10 small-12">
+                            <div class="column">
+                                <P label={paraMissionVision} />
+                            </div>
+                        </div>
+                        
+                        <div class="cell large-9 small-12 margin-top-48">
+                            <div class="column">
+                                <Heading 
+                                    type='3' 
+                                    label={headingUNcharter} 
+                                />
+                            </div>
+                        </div>
+                        <div class="cell large-10 small-12">
+                            <div class="column">
+                                <P label={paraUNcharter} />
+                            </div>
+                        </div>
+
+                        <div class="cell large-9 small-12 margin-top-48">
+                            <div class="column">
+                                <Heading 
+                                    type='3' 
+                                    label={headingHistory}  
+                                />
+                            </div>
+                        </div>
+                        <div class="cell large-10 small-12">
+                            <div class="column">
+                                <P label={paraHistory} />
+                            </div>
+                        </div>    
+                        <div class="column margin-top-48">
+                            <ContentCard 
+                                data={ContentCardData.groupcontentdata}
+                                buttontype={ContentCardData.buttonname}
+                            />
+                        </div>                    
+                    </div>                    
+                </div>
+            </div>  
+        </div>
+        <Footer
+            src={logo}
+            srctwo={logowhite}
+            headerText={footerData.headerText}
+            headerText2={footerData.headerText2}
+            style="default"
+            alt="UNDP Logo"
+            element="input"
+            type="email"
+            required="required"
+            mode="form-email"
+            errorText={footerData.errorLabel}
+            label={footerData.labelname}
+            button={footerData.buttonname}
+            placeholder={footerData.placeholdername}
+            menutitle={footerData.pageTitle}
+            data={footerData.footerdata}
+            copyright={footerData.copyright}
+            menudata={footerData.menudata}
+            {...{ color: 'blue' }}
+        />
     </div>
-    <Footer
-      src={logo}
-      srctwo={logowhite}
-      headerText={footerData.headerText}
-      headerText2={footerData.headerText2}
-      style="default"
-      alt="UNDP Logo"
-      element="input"
-      type="email"
-      required="required"
-      mode="form-email"
-      errorText={footerData.errorLabel}
-      label={footerData.labelname}
-      button={footerData.buttonname}
-      placeholder={footerData.placeholdername}
-      menutitle={footerData.pageTitle}
-      data={footerData.footerdata}
-      copyright={footerData.copyright}
-      menudata={footerData.menudata}
-      {...{ color: 'blue' }}
-    />
-  </>
-);
+  );
+  
