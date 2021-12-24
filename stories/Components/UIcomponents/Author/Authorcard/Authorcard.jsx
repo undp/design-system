c2069388fe53b46d88b0ddecd1b5f75270e4ba55
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-12-24 18:24:32 +0530

    [UNDP-664][DEVELOP]: Add link in authors

--- stories/Components/UIcomponents/Author/Authorcard/Authorcard.jsx
+++ stories/Components/UIcomponents/Author/Authorcard/Authorcard.jsx
@@ -8,9 +8,9 @@ export const Authorcard = ({
 }) => (
   <div className="grid-x grid-margin-x">
     {data.map((item, index) => (
-      <div className="cell medium-4">
+      <div className="cell medium-4" key={index}>
         <a href={link} className="author-card">
-          <Author data={data} Number="One" image={image} variant="small" alt={alt} title={alt} />
+          <Author data={data} Number="One" image={image} variant="small" alt={alt} title={alt} Link='False'/>
           <P label={para} />
           <span className="cta__link cta--space">{button}</span>
         </a>
