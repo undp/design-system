8f1b99081ac8bb110b3194e7f4d86f8fba1ca30f
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-12-24 18:57:16 +0530

    [UNDP-631][DEVELOP]: Stats panel feedback.

--- stories/Molecules/Blocks/StatsPanel/StatsPanel.jsx
+++ stories/Molecules/Blocks/StatsPanel/StatsPanel.jsx
@@ -1,20 +1,14 @@
 import React, { useEffect } from 'react';
 import './statspanel.scss';
 import {StatsCards} from '../../../Components/UIcomponents/Cards/StatsCards/StatsCards';
-import viewport from '../../../assets/js/viewport';
 
 export const StatsPanel = ({
   number, percent, Hovercolors, content, delaytime, imageback,
 }) => {
-  useEffect(() => {
-    viewport('.statspanel-card');
-  }, []);
   return (
-    <div className={`cell medium-3 statspanel-card left-right delay-${delaytime}`}>
-      <div className={['stats-panel', `${[`${Hovercolors}`]}`].join(' ')}>
-        <img src={imageback} alt={imageback} />
-        <StatsCards number={number} percent={percent}  content={content}/>
-      </div>
+    <div className={['stats-panel', `${[`${Hovercolors}`]}`].join(' ')}>
+      <img src={imageback} alt={imageback} />
+      <StatsCards number={number} percent={percent} content={content}/>
     </div>
   );
 };
