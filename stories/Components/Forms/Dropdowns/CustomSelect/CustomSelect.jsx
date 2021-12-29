c6695a95eacd2572f16e190fd9527b6a5309b416
ashish dwivedi <ashish.dwivedi@srijan.net>
2021-12-29 23:26:14 +0530

    und-748 UAT Feedback - (P0) - Forms | Atoms | Select

--- stories/Components/Forms/Dropdowns/CustomSelect/CustomSelect.jsx
+++ stories/Components/Forms/Dropdowns/CustomSelect/CustomSelect.jsx
@@ -12,25 +12,25 @@ const CustomSelect = ({ text }) => {
         {text}
       </button>
       <ul tabIndex="-1" role="listbox" aria-labelledby="exp_button" data-select-options="">
-        <li id="default-language-select" className="option" role="option" data-value="default">
+        <li role="option" data-value="default">
           <span>All</span>
         </li>
-        <li id="pasto-1" role="option" data-value="pasto">
+        <li role="option" data-value="pasto">
           <span>Pasto</span>
         </li>
-        <li id="dari-1" role="option" data-value="dari">
+        <li role="option" data-value="dari">
           <span>Dari</span>
         </li>
-        <li id="english-1" role="option" data-value="en">
+        <li role="option" data-value="en">
           <span>English</span>
         </li>
-        <li id="albanian-1" role="option" data-value="albanian">
+        <li role="option" data-value="albanian">
           <span>Albanian</span>
         </li>
-        <li id="arabic-1" role="option" data-value="arabic">
+        <li role="option" data-value="arabic">
           <span>Arabic</span>
         </li>
-        <li id="portuguese-1" role="option" data-value="portuguese">
+        <li role="option" data-value="portuguese">
           <span>Portuguese</span>
         </li>
       </ul>
