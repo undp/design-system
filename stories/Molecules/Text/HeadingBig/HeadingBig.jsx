d6799cae039863c828dd1416608a0987dc2f4714
neelesh-tippe <neelesh.tippe@srijan.net>
2021-12-30 12:38:00 +0530

    UND-760 Animation-issue

--- stories/Molecules/Text/HeadingBig/HeadingBig.jsx
+++ stories/Molecules/Text/HeadingBig/HeadingBig.jsx
@@ -4,18 +4,31 @@ import './headingbig.scss';
 import { Heading } from '../../../Atom/Typography/Heading/Heading';
 import '../../../assets/js/smoothbgchange';
 
-export const HeadingBig = ({ headerText, descriptionText }) => {
+export const transition_options = {
+  true: 'heading-big',
+  false: 'heading-container',
+};
+
+export const HeadingBig = ({ headerText, descriptionText, ...args }) => {
   useEffect(() => {
     viewport('.heading-big');
+    viewport('.heading-big h2');
+    viewport('.heading-big h4');
+    viewport('.heading-container h2');
+    viewport('.heading-container h4');
   }, []);
   return (
-    <div className="grid-x heading-big  align-center" data-color="blue">
+    <div data-color="blue" className={['grid-x align-center', `${transition_options[`${args.Emphasize}`]}`].join(' ')}>
       <div className="cell  medium-4">
-        <Heading type="2" label={headerText} />
+        <Heading className="left-right" type="2" label={headerText} />
       </div>
       <div className="cell  medium-6">
-        <Heading type="4" label={descriptionText} />
+        <Heading className="left-right" type="4" label={descriptionText} />
       </div>
     </div>
   );
+};
+
+HeadingBig.defaultProps = {
+  Emphasize: 'true'
 };
