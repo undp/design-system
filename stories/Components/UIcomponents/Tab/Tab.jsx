67d666ca7d52871b5e3b683ee0ac9297169349ae
harish kumar <harish.kumar@srijan.net>
2021-12-31 12:04:37 +0530

    tabs deep linking not working issue

--- stories/Components/UIcomponents/Tab/Tab.jsx
+++ stories/Components/UIcomponents/Tab/Tab.jsx
@@ -4,24 +4,24 @@ import './tab.scss';
 import viewport from '../../../assets/js/viewport';
 import { BodyColumnTwo } from '../../../Molecules/Text/BodyColumn/BodyColumn';
 
-export const Tab = ({ tabdata }) => {
+export const Tab = ({ tabdata, tabdata2 }) => {
   useEffect(() => {
     tabs();
     viewport('.tabs, .tabs-content');
   }, []);
   return (
     <>
-      <ul className="tabs left-right" data-deep-link="true" data-tabs id="tab" role="tablist">
+      <ul className="tabs left-right" data-deep-link="true" data-tabs id="tablist_1" role="tablist" >
         {tabdata.map((item, index) => (
-          <li key={index} className="tabs-title">
+          <li key={index} className={ index == 0 ? 'tabs-title is-active' : 'tabs-title'}>
             <a href={`#${item.text_id}`} aria-selected="true">{item.text}</a>
           </li>
         ))}
       </ul>
 
-      <div className="tabs-content left-right" data-tabs-content="tab">
+      <div className="tabs-content left-right" data-tabs-content="tablist_1">
         {tabdata.map((item, index) => (
-          <div key={index} id={item.text_id} className="tabs-panel"><BodyColumnTwo descriptionText={item.data} /></div>
+          <div key={index} id={item.text_id} className={ index == 0 ? 'tabs-panel is-active' : 'tabs-panel'}><BodyColumnTwo descriptionText={item.data} /></div>
         ))}
       </div>
     </>
