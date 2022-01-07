11ff66d4775f175b0e64399541a8fbaffadc6796
pallavikadam-srijan <pallavi.kadam@srijan.net>
2022-01-07 10:51:33 +0530

    Removed hover effect

--- stories/assets/js/cardgradient.js
+++
@@ -1,8 +0,0 @@
-export function cardGradient() {
-  $('.wide-card__description .button-arrow').mouseenter(() => {
-    $('.wide-card__image a').addClass('card-gradient');
-  });
-  $('.wide-card__description .button-arrow').mouseleave(() => {
-    $('.wide-card__image a').removeClass('card-gradient');
-  });
-}
