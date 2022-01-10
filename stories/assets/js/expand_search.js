9c7f56e97fc02cdf37121a17f5a96d6bb82fc227
ashish dwivedi <ashish.dwivedi@srijan.net>
2022-01-10 14:50:45 +0530

    und-881

--- stories/assets/js/expand_search.js
+++ stories/assets/js/expand_search.js
@@ -1,18 +1,6 @@
 export const expandSearch = () => {
   $(document).on("click", ".icon-search-black", function () {
-    $(this).parent(".expand-search").addClass("open");
+    $(this).parent(".expand-search").toggleClass("open");
     $(this).siblings("input").focus();
-  });
-
-  $(document).mouseup(function (event) {
-    const expandInput = $(".expand-search input");
-    if (
-      !expandInput.is(event.target) &&
-      expandInput.has(event.target).length === 0
-    ) {
-      if (expandInput.val().length < 1) {
-        expandInput.parent().removeClass("open");
-      }
-    }
   });
 };
