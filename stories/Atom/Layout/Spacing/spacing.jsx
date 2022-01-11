3425c4483b4c481b5fdc0ef82b64ceb98b34b85d
pallavikadam-srijan <pallavi.kadam@srijan.net>
2022-01-11 13:22:04 +0530

    Update spacing component

--- stories/Atom/Layout/Spacing/spacing.jsx
+++ stories/Atom/Layout/Spacing/spacing.jsx
@@ -7,82 +7,69 @@ export const Spacing = () => (
       <div className="spacing__title Primary">Spacing</div>
       <div className="spacing__define">
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-01" />
           <div className="spacing__label">spacing-01</div>
-          <div className="spacing__size">2px</div>
-          <div className="spacing__box1" />
+          <div className="spacing__size">0.125rem</div>
+          <div className="spacing-1" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-02" />
           <div className="spacing__label">spacing-02</div>
-          <div className="spacing__size">4px</div>
-          <div className="spacing__box2" />
+          <div className="spacing__size">0.25rem</div>
+          <div className="spacing-2" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-03" />
           <div className="spacing__label">spacing-03</div>
-          <div className="spacing__size">8px</div>
-          <div className="spacing__box3" />
+          <div className="spacing__size">0.5rem</div>
+          <div className="spacing-3" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-04" />
           <div className="spacing__label">spacing-04</div>
-          <div className="spacing__size">12px</div>
-          <div className="spacing__box4" />
+          <div className="spacing__size">0.75rem</div>
+          <div className="spacing-4" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-05" />
           <div className="spacing__label">spacing-05</div>
-          <div className="spacing__size">16px</div>
-          <div className="spacing__box5" />
+          <div className="spacing__size">1rem</div>
+          <div className="spacing-5" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-06" />
           <div className="spacing__label">spacing-06</div>
-          <div className="spacing__size">24px</div>
-          <div className="spacing__box6" />
+          <div className="spacing__size">1.5rem</div>
+          <div className="spacing-6" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-07" />
           <div className="spacing__label">spacing-07</div>
-          <div className="spacing__size">32px</div>
-          <div className="spacing__box7" />
+          <div className="spacing__size">2rem</div>
+          <div className="spacing-7" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-08" />
           <div className="spacing__label">spacing-08</div>
-          <div className="spacing__size">40px</div>
-          <div className="spacing__box8" />
+          <div className="spacing__size">2.5rem</div>
+          <div className="spacing-8" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-09" />
           <div className="spacing__label">spacing-09</div>
-          <div className="spacing__size">48px</div>
-          <div className="spacing__box9" />
+          <div className="spacing__size">3rem</div>
+          <div className="spacing-9" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-10" />
           <div className="spacing__label">spacing-10</div>
-          <div className="spacing__size">64px</div>
-          <div className="spacing__box10" />
+          <div className="spacing__size">4rem</div>
+          <div className="spacing-10" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-11" />
           <div className="spacing__label">spacing-11</div>
-          <div className="spacing__size">80px</div>
-          <div className="spacing__box11" />
+          <div className="spacing__size">5rem</div>
+          <div className="spacing-11" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-12" />
           <div className="spacing__label">spacing-12</div>
-          <div className="spacing__size">96px</div>
-          <div className="spacing__box12" />
+          <div className="spacing__size">6rem</div>
+          <div className="spacing-12" />
         </div>
         <div className="spacing__name">
-          <div className="spacing__card p-spacing-13" />
           <div className="spacing__label">spacing-13</div>
-          <div className="spacing__size">160px</div>
-          <div className="spacing__box13" />
+          <div className="spacing__size">10rem</div>
+          <div className="spacing-13" />
         </div>
       </div>
     </div>
