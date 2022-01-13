675d4da8266f5ff196c45da0ba0acf03e0406b68
harish kumar <harish.kumar@srijan.net>
2022-01-13 17:02:58 +0530

    UND-898-Heading-issues

--- stories/Atom/Typography/Links/link.jsx
+++ stories/Atom/Typography/Links/link.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import './link.scss';
 
-export const Link = ({ label }) => (
-  <a href="#" title={label}>
+export const variant_options= {
+	default: "",
+	animated: "animated-link",
+};
+
+const cls = (...classes) => (classes.filter(Boolean).length > 0) ? classes.filter(Boolean).join(' ') : null;
+
+export const Link = ({ label, ...args }) => {
+  let variant_link = variant_options[`${args.variant}`];
+  return (
+   <a href="#" title={label} className={cls(`${variant_link}`)}>
     { label }
   </a>
-);
+  );
+};
