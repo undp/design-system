a945092b8563565cf3e7f03ee05683287e488017
neelesh-tippe <neelesh.tippe@srijan.net>
2022-01-14 00:12:24 +0530

    Single content card feedback

--- stories/Components/UIcomponents/Buttons/Cta_link/Cta_link.jsx
+++ stories/Components/UIcomponents/Buttons/Cta_link/Cta_link.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import './cta_link.scss';
 
-export const Ctalink = ({ label, Type, ...args }) => {
+const cls = (...classes) => ((classes.filter(Boolean).length > 0) ? classes.filter(Boolean).join(' ') : null);
+
+export const Ctalink = ({ label, Type, button_option, ...args }) => {
   let type = 'arrow';
   if (Type == 'Space') {
     type = 'space';
   }
 
   return (
-    <a className={['cta__link', `cta--${type}`].join(' ')} href="#">
-      {label}
-    </a>
+    <>
+      {button_option === 'link'
+      ? (
+        <a className={cls('cta__link', `cta--${type}`)} href="#" role="link">
+          {label}
+        </a>
+      )
+      : (
+        <></>
+      )}
+      {button_option === 'span'
+      ? (
+        <span className={cls('cta__link', `cta--${type}`)}>
+          {label}
+        </span>
+      )
+      : (
+        <></>
+      )}
+   </>
   );
+};
+
+Ctalink.defaultProps = {
+  button_option: 'link',
 };
