2ce65f8857dc04f94478e6bc94f5b747de548011
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2022-01-14 18:25:29 +0530

    [UNDP-917][DEVELOP]: Stats Card Class Issues Fixed

--- stories/Components/UIcomponents/Cards/StatsCards/StatsCards.jsx
+++ stories/Components/UIcomponents/Cards/StatsCards/StatsCards.jsx
@@ -27,8 +27,8 @@ export const StatsCards = ({
   }
 
   return (
-    <div className={cls('statscard', `${size}`, `${Hovercolors}`)}>
-      <div className="statscard__box">
+    <div className={cls('stats-card', `${size}`, `${Hovercolors}`)}>
+      <div className="stats-card-box">
         {Size == 'Extra_large' || Size == 'Large' ? (<Heading type="3" label={title} />) : (<></>)}
         <Heading type="2" label={number} />
         <Heading type="4" label={percent} />
