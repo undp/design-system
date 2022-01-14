aafd45392519d001321c80b126853b127849d0c8
Shamimkhan <shamim.khan@srijan.net>
2022-01-14 12:38:28 +0530

    Fixed Author card issue

--- stories/Atom/Images/Authorimage/Authorimages.jsx
+++ stories/Atom/Images/Authorimage/Authorimages.jsx
@@ -13,11 +13,13 @@ export const hover_color_options = {
   azure: 'azure',
 };
 
+const cls = (...classes) => ((classes.filter(Boolean).length > 0) ? classes.filter(Boolean).join(' ') : null);
+
 export const Authorimg = ({ image, alt, ...args }) => {
   let size_variant = size_options[`${args.variant}`];
   let Hovercolors = hover_color_options[`${args.hovercolor}`];
   return (
-    <div className={['author__img', `${size_variant}`, `${Hovercolors}`].join(' ').trim()}>
+    <div className={cls('author__img', `${size_variant}`, `${Hovercolors}`)}>
       <img src={image} alt={alt} title={alt} />
     </div>
   );
