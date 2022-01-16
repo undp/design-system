ba83882fb7524e24d2c554b52c08cfde5b99edfd
harish kumar <harish.kumar@srijan.net>
2022-01-16 20:19:36 +0530

    lightbox-feedback

--- stories/Components/UIcomponents/Gallery/LightboxGallery/LightboxGallery.jsx
+++ stories/Components/UIcomponents/Gallery/LightboxGallery/LightboxGallery.jsx
@@ -7,46 +7,46 @@ import './lightbox.scss';
 import './lightboxgallery.scss';
 
 export const LightboxGallery = ({
-  titleText, descText, buttonText, data,
+  titleText, descText, buttonText, data
 }) => {
   useEffect(() => {
     lightbox_Gallery();
   }, []);
   return (
-    <div className="grid-x grid-margin-x lightbox-gallery-row">
-      <div className={['medium-4 large-3 cell lightbox-gallery-row__left-col'].join(' ')}>
-        <div className="lightbox-gallery-row__content">
+    <div className="grid-x grid-margin-x lightbox-gallery">
+      <div className={['medium-4 large-3 cell lightbox-gallery__left'].join(' ')}>
+        <div className="lightbox-gallery__content">
           <Heading type="5" label={titleText} />
           <P label={descText} />
           <Ctalink label={buttonText} />
         </div>
       </div>
-      <div className="medium-8 large-9 cell lightbox-gallery-row__right-col overflow-hidden">
-        <div className="lightbox-gallery-images">
-          <ul>
-            {data.map((item) => (
-              <>
-                <li>
-                  <a href={item.galleryimg1} className="glightbox" data-glightbox="description:.custom-desc1; descPosition:left;">
-                    <figure>
-                      <div className="image"><img src={item.galleryimg1} alt="image" /></div>
-                      <div className="custom-desc1 image-section__description">
-                        <figcaption className="image__caption">
-                          <div className="image__caption_para">
-                            {item.paragraph}
-                          </div>
-                          <div className="credits">
-                            {item.label}
-                          </div>
-                        </figcaption>
-                      </div>
-                    </figure>
-                  </a>
-                </li>
-              </>
-            ))}
-          </ul>
-        </div>
+      <div className="medium-8 large-9 cell lightbox-gallery__right">
+            <ul className="lightbox-gallery-images">
+              {data.map((item) => {
+                 return (
+                    <>
+                    <li>
+                      <a href={item.galleryimg1} class="glightbox" data-glightbox="description:.image__description; descPosition:left;">
+                        <figure>
+                          <div className="image"><img src={item.galleryimg1} alt="image" /></div>
+                            <div class="image__description">
+                              <figcaption class="image__caption">
+                                <div class="image__caption_para">
+                                {item.paragraph}
+                                </div>
+                                <div class="credits">
+                                  {item.label}
+                                </div>
+                              </figcaption>
+                            </div>
+                          </figure>
+                        </a>
+                      </li>
+                    </>
+                  );
+              })}
+            </ul>
       </div>
     </div>
   );
