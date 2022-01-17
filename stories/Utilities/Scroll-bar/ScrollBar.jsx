f6f987b901042239378971178a42692b3cf0ab5d
harish kumar <harish.kumar@srijan.net>
2022-01-17 09:39:31 +0530

    UND-921-Scrollbar-feedback

--- stories/Utilities/Scroll-bar/ScrollBar.jsx
+++ stories/Utilities/Scroll-bar/ScrollBar.jsx
@@ -3,11 +3,22 @@ import './scrollbar.scss';
 
 export const ScrollBar = () => (
   <>
-    <div className="scrollbar-horizontal-wrapper scrollbar-horizontal" tabIndex="0">
-      <div className="scrollbar-data">.</div>
+    <div className="scrollbar-horizontal-wrapper scrollbar-horizontal scrollbar-vertical" tabIndex="0">
+      <div className="scrollbar-data">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+      </div>
     </div>
-    <div className="scrollbar-vertical-wrapper scrollbar-vertical" tabIndex="0">
+    {/* <div className="scrollbar-vertical-wrapper scrollbar-vertical" tabIndex="0">
       <div className="scrollbar-data__vertical" />
-    </div>
+    </div> */}
   </>
 );
