7518a4f7dbaaae581eec285a22115185f0d29881
sumit3021kumar <manudogra3021@gmail.com>
2022-01-21 17:24:45 +0530

    UNDP-1057: remove unwanted label tag if data now present

--- stories/Components/Forms/Checkbox/Checkbox.jsx
+++ stories/Components/Forms/Checkbox/Checkbox.jsx
@@ -4,6 +4,6 @@ import './checkbox.scss';
 export const Checkbox = ({ label, value, id }) => (
   <div className="form-check">
     <input type="checkbox" id={id} name={id} value={value}/>
-    <label htmlFor={id}>{label}</label>
+    {label && (<label htmlFor={id}>{label}</label>)}
   </div>
 );
