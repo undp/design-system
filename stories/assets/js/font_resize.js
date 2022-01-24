c238883b9863d494e5355c1d53cec114aacdf0af
Shishir Phadke <shishir.phadke@srijan.net>
2022-01-24 18:53:12 +0530

    [UND-0000][DEVELOP]: Font Resize Issue.

--- stories/assets/js/font_resize.js
+++ stories/assets/js/font_resize.js
@@ -1,16 +1,23 @@
-
+import * as utility from './resize';
 export function FontResize(element) {
-  // run the function
-  resize();
   function resize() {
     $(element).each(function() {
       let text_size = $(this).text().length;
+      let text_width = $(this).css('white-space', 'pre').get(0).scrollWidth;
+      let text_parent_size = $(this).parent().width();
       // set 2 bcards looks good with 2 characters
-      if (text_size > 2) {
+      if (text_size > 2 && (text_width > text_parent_size)) {
         $(this).css('font-size', parseInt($(this).width())/text_size);
       } else {
         $(this).removeAttr('style');
       }
+      $(this).css('white-space', '');
     });
   }
+
+  // Custom windowResize;
+  utility.windowResize($(window), (e) => {
+    // run the function
+    resize();
+  });
 }
