7e9f19f918269c4b533db5567cb41b58ead94181
pallavikadam-srijan <pallavi.kadam@srijan.net>
2022-02-18 17:04:50 +0530

    update lightbox close button on mobile

--- stories/assets/js/lightbox-gallery.js
+++ stories/assets/js/lightbox-gallery.js
@@ -2,16 +2,29 @@
  * lightbox-gallery.js library
  */
 export function lightboxGallery() {
-  var lightboxDescription = GLightbox({
+  let lightbox = GLightbox({
     selector: '.glightbox',
     draggable: true,
     moreLength: 0,
   });
 
+  const $windowWidth = $(window).width();
+  const breakpoint = window.UNDP.breakpoints.MEDIUM || 768;
+  if ($windowWidth <= breakpoint) {
+    // Close lightbox on clicking outside on Mobile Breakpoint.
+    $(document).on('click', (e) => {
+      // Check if click triggered on parent selectors.
+      const $triggerClasses = $('.ginner-container, .gslide-inner-content, .gslide, .gslider, .gcontainer, .glightbox-container');
+      const $triggerActive = $($triggerClasses).filter((_, el) => $(el).is(e.target)).length;
+      if ($(e.target).closest('.glightbox-container').length && $triggerActive) {
+        // lightbox close() callback.
+        lightbox.close();
+      }
+    });
+  }
   // caption alignment set according image block
   const captionalignment = () => {
     $('.image__description').each(function () {
-
       // caption class add
       var $gDesHeight = $(this).height();
       var $gImgHeight = $(this).siblings('.image').height();
@@ -23,15 +36,15 @@ export function lightboxGallery() {
     });
 
     // frostedbackground class add
-    $('.lightbox-gallery-images li').click(function(){
+    $('.lightbox-gallery-images li').click(() => {
       $('.goverlay, .gloader').remove();
       $('.gcontainer').addClass('frosted-background');
-    })
-  }
+    });
+  };
 
-  //function load
-  window.onload = function() {
-  if(document.readyState == 'complete') {
+  // function load
+  window.onload = function () {
+    if (document.readyState == 'complete') {
       captionalignment();
     }
   };
