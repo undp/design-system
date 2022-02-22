b5110eda4bf09976a4544496c1484d3ef855f8c7
neelesh-tippe <neelesh.tippe@srijan.net>
2022-02-22 22:26:40 +0530

    Transition issue background

--- stories/assets/js/smooth-bg-change.js
+++ stories/assets/js/smooth-bg-change.js
@@ -2,60 +2,37 @@ export function changeBackground(body, container) {
   // Register GSAP required plugins and effects.
   gsap.registerPlugin(ScrollTrigger);
 
-  let $startTrigger = $(container).find('h2');
+  const $section = $(container);
+  const $startTrigger = $section.find("h2");
   let $endPoint;
-
 
   const $windowWidth = $(window).width();
   const breakpoint = window.UNDP.breakpoints.TABLET || 834;
   if ($windowWidth > breakpoint) {
     $endPoint = ($(container).innerHeight() - 500 )+'px';
   } else {
-    $endPoint = ($(container).innerHeight() + 250 )+'px';
+    $endPoint = ($(container).innerHeight() - 200 )+'px';
   }
 
-  if(!$(container).length) return false;
+  const colorToBlue = gsap.fromTo(body, { backgroundColor: "#ffffff" },{ backgroundColor: "#232E3E" })
 
-  let blueToWhiteScroll = null,
-    blueToWhite = null
+  ScrollTrigger.create({
+    trigger: $startTrigger,
+    start: "top-=500px top+=200px",
+    end: "+=500px",
+    scrub: true,
+    animation: colorToBlue,
+    onLeaveBack: () =>
+     gsap.to(body, { backgroundColor: "white", overwrite: "auto" })
+  });
 
-  function createScrollsWhiteToBlue(){
-    gsap.utils.toArray('.heading-big').forEach((section, i) => {
-      if(section.getAttribute('data-color') !== null) {
-        var colorAttr = section.getAttribute('data-color')
-        
-        gsap.to(body, {
-          backgroundColor: colorAttr === "blue" ? gsap.getProperty("html", "--blue") : gsap.getProperty("html", "--light"),
-          immediateRender: false,
-          scrollTrigger: {
-            trigger: section,
-            scrub: true,
-            start:'top+=40% bottom',
-            end: '+=400px'
-          }
-        });
-      }
-    });
-  }
-  
-  function createScrollsBlueToWhite() {
-    let startBlueToWhite = "bottom top+=200px"
+  const colorToWhite= gsap.fromTo(body, { backgroundColor: "#232E3E" },{ backgroundColor: "#ffffff", immediateRender: false })
 
-    blueToWhite = gsap.fromTo('body',
-      { backgroundColor: "#232E3E" },
-      { backgroundColor: "#ffffff",
-          immediateRender: false })
-     
-      blueToWhiteScroll = ScrollTrigger.create({
-      trigger: $startTrigger,
-      start: startBlueToWhite,
-      end: $endPoint,
-      scrub: true,
-      animation: blueToWhite,
-      onEnter: () => $(body).removeClass('change-background'),
-      onLeaveBack: () => $(body).addClass('change-background')
-    });
-  }
-  createScrollsWhiteToBlue();
-  createScrollsBlueToWhite();
+  ScrollTrigger.create({
+    trigger: $startTrigger,
+    start: "bottom top-=50px",
+    end: $endPoint,
+    scrub: true,
+    animation: colorToWhite,
+  });
 }
