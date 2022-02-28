e024860d3a0a494e53521e3951a29117847bc4f5
KD <kuldev.singh@srijan.net>
2022-02-28 13:45:09 +0530

    updating viewport js

--- stories/assets/js/viewport.js
+++ stories/assets/js/viewport.js
@@ -1,18 +1,23 @@
 /* Function to chech if element is in viewport or not */
-$.fn.isInViewport = function () {
-  const $elementTop = $(this).offset().top;
-  const $elementBottom = $elementTop + $(this).outerHeight();
 
-  const $viewportTop = $(window).scrollTop();
-  const $viewportBottom = $viewportTop + $(window).height();
+(function($) {
 
-  return $elementBottom > $viewportTop && $elementTop < $viewportBottom;
-};
-
-$(window).on('resize scroll load', () => {
-  $('[data-viewport=true]').each(function () {
-    if ($(this).isInViewport()) {
-      $(this).addClass('inviewport');
-    }
+  $.fn.isInViewport = function () {
+    const $elementTop = $(this).offset().top;
+    const $elementBottom = $elementTop + $(this).outerHeight();
+  
+    const $viewportTop = $(window).scrollTop();
+    const $viewportBottom = $viewportTop + $(window).height();
+  
+    return $elementBottom > $viewportTop && $elementTop < $viewportBottom;
+  };
+  
+  $(window).on('resize scroll load', () => {
+    $('[data-viewport=true]').each(function () {
+      if ($(this).isInViewport()) {
+        $(this).addClass('inviewport');
+      }
+    });
   });
-});
+
+})(jQuery);
