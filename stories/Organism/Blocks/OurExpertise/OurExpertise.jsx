53563e4ef50af61e907db86263b308e573dffee0
Jura Khrapunov <jura.khrapunov@undp.org>
2022-11-03 15:16:01 -0400

    #904 - updated styling and javascript logic for Our Expertise

--- stories/Organism/Blocks/OurExpertise/OurExpertise.jsx
+++ stories/Organism/Blocks/OurExpertise/OurExpertise.jsx
@@ -14,7 +14,7 @@ if (window.location.href.indexOf('direction=rtl') > -1) {
 
 export function OurExpertise({ content }) {
   useEffect(() => {
-    ourExpertise();
+    ourExpertise('.our-expertise');
   }, []);
   return (
     <div className="grid-x our-expertise no-gap">
@@ -28,10 +28,9 @@ export function OurExpertise({ content }) {
       <div className="cell large-offset-1 large-6 small-12">
         <div className="swiper" dir={rtl}>
           <div className="swiper-wrapper">
-
             {content.map((category, c) => (
               category.slides.map((slide, s) => (
-                <div className="swiper-slide" data-category={c} data-slide={s} data-cat-length={category.slides.length}>
+                <div className="swiper-slide" data-category={c} data-panel={s} data-cat-length={category.slides.length} key={`key-${c}-${s}`}>
                   <div className="image">
                     {slide.image && <img src={`/images/${slide.image}`} alt="" />}
                   </div>
@@ -47,14 +46,10 @@ export function OurExpertise({ content }) {
           <div className="swiper-navigation" />
           <div className="swiper-tabs">
             {content.map((category, c) => (
-              <div className={c == 0 ? 'is-active' : ''} data-category={c}>{category.title}</div>
+              <div className={c == 0 ? 'is-active' : ''} data-category={c} key={c}>{category.title}</div>
             ))}
           </div>
-          <div className="swiper-counter">
-            1 of
-            {' '}
-            {content[0].slides.length}
-          </div>
+          <div className="swiper-counter">{`1 / ${content[0].slides.length}`}</div>
         </div>
       </div>
     </div>
--- stories/assets/js/our-expertise.js
+++ stories/assets/js/our-expertise.js
@@ -3,8 +3,8 @@
  */
 import { swiper } from './swiper';
 
-export function ourExpertise() {
-  swiper('.our-expertise .swiper', '.swiper-navigation', {
+export function ourExpertise(ele) {
+  swiper(`${ele} .swiper`, '.swiper-navigation', {
     scrollbar: false,
     spaceBetween: 0,
     navigation: {
@@ -17,12 +17,12 @@ export function ourExpertise() {
         const $divs = jQuery(slider.el).find('.swiper-tabs div');
         const $slide = jQuery(slider.slides[slider.realIndex]);
         $divs.removeClass('is-active').eq($slide.data('category')).addClass('is-active');
-        jQuery(slider.el).find('.swiper-counter').text(`${$slide.data('slide') + 1} of ${$slide.data('catLength')}`);
+        jQuery(slider.el).find('.swiper-counter').text(`${$slide.data('panel') + 1} / ${$slide.data('catLength')}`);
       },
     },
   });
-  jQuery('.our-expertise .swiper-tabs div').on('click', (event) => {
-    const slider = jQuery('.our-expertise .swiper').prop('swiper');
+  jQuery(`${ele} .swiper-tabs div`).on('click', (event) => {
+    const slider = jQuery(`${ele} .swiper`).prop('swiper');
     const cat = jQuery(event.target).data('category');
     slider.slides.some((slide, index) => {
       if (jQuery(slide).data('category') == cat) {
