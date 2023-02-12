3f0b1352977ba5d364bd98383f972c4efee9fcce
Philip Boden <philip.boden@gmail.com>
2023-02-12 03:18:09 -0800

    Added Callout (BlockQuoteComponent) to accent control

--- stories/Molecules/Text/BlockquoteComponent/BlockquoteComponent.jsx
+++ stories/Molecules/Text/BlockquoteComponent/BlockquoteComponent.jsx
@@ -2,18 +2,36 @@ import React from 'react';
 import { Blockquote } from '../../../Atom/BaseTypography/Blockquote/Blockquote';
 import './blockquotecomp.scss';
 
-export const BlockquoteComponent = ({ blockquoteText, citeText, Colors }) => (
-  <>
-    {Colors == 'default' ? (
-      <Blockquote text={blockquoteText} citeText={citeText}>
-        {' '}
-      </Blockquote>
-    ) : (
-      <div className={['blockquote', `${Colors}`].join(' ')}>
-        <Blockquote text={blockquoteText} citeText={citeText}>
-          {' '}
-        </Blockquote>
-      </div>
-    )}
-  </>
-);
+export const color_options = {
+  default: 'default',
+  yellow: 'yellow',
+  green: 'green',
+  red: 'red',
+  blue: 'blue',
+  gray: 'gray',
+  accent: '',
+};
+
+export function BlockquoteComponent({
+  blockquoteText,
+  citeText,
+  Colors,
+  ...args
+}) {
+  const { accent } = args;
+  const hovercolor = color_options[`${Colors}`];
+  const Color = (Boolean(accent)) && (!hovercolor) ? accent : hovercolor;
+  const Class = (Color != 'default') ? 'blockquote' : '';
+
+  if (Colors == 'default') {
+    return (
+      <Blockquote text={blockquoteText} citeText={citeText} />
+    );
+  }
+
+  return (
+    <div className={[Class, `${Color}`].join(' ')}>
+      <Blockquote text={blockquoteText} citeText={citeText} />
+    </div>
+  );
+}
