7976639014b66f71d1543d6f359748f5bc32386e
Philip Boden <philip.boden@gmail.com>
2023-02-11 20:50:48 -0800

    Multiple updates to page hero
    #905 - Add option of page hero-full to allow for no background image
    
    #997 - Allow for optional CTA link
    
    #1008 - Added setup for optional overline and subtitle fields

--- stories/Components/UIcomponents/Hero/PageHero/PageHeroOption.jsx
+++ stories/Components/UIcomponents/Hero/PageHero/PageHeroOption.jsx
@@ -3,36 +3,51 @@ import './page-hero.scss';
 import { Video } from '../../../../Atom/Video/Video';
 import { Heading } from '../../../../Atom/Typography/Heading/Heading';
 import { Breadcrumbcomponent } from '../../../Navigationcomponents/Breadcrumbs/Breadcrumbs';
+import { CtaButton } from '../../Buttons/CtaButton/CtaButton';
 
-export const PageHeroOption = ({
-  data, title, content, active, variant, imgsrc, imgsrc2, imgalt, videosrc,
-}) => (
-  <div className="pagehero-tall">
-    <div className="grid-x">
-      <div className="cell medium-4 medium-offset-1">
-        <div className="pagehero-content">
-          <Breadcrumbcomponent data={data} />
-          {content && <Heading type="4" label={content} dataViewport="true"/>}
-          <Heading type="2" label={title} dataViewport="true"/>
+export function PageHeroOption({
+  data,
+  title,
+  subtitle,
+  content,
+  active,
+  variant,
+  imgsrc,
+  imgsrc2,
+  imgalt,
+  videosrc,
+  cta,
+  ...args
+}) {
+  const heroImageClasses = variant === 'no_background' ? 'hero-placeholder' : 'homepage-hero-image';
+  const noImageWrapperClasses = variant === 'no_background' ? 'no-background' : '';
+  const CtaEnabledClass = args.cta_enabled ? 'has-cta' : '';
+  return (
+    <div className={['pagehero-tall', CtaEnabledClass, noImageWrapperClasses].join(' ')}>
+      <div className="grid-x">
+        <div className="cell medium-4 medium-offset-1">
+          <div className="pagehero-content">
+            <Breadcrumbcomponent data={data} />
+            {args.outline_enabled && content && <Heading type="4" label={content} className="color-black" dataViewport="true" />}
+            <Heading type="2" label={title} className="color-black" dataViewport="true" />
+            {args.subtitle_enabled && subtitle && <p className={['subtitle', 'color-black'].join(' ')}>{subtitle}</p>}
+            {args.cta_enabled && cta.label && <CtaButton label={cta.label} For_Primary={cta.for_primary} />}
+          </div>
         </div>
-      </div>
-      <div className="cell medium-7">
-        <div className="homepage-hero-image">
-          {variant === 'video' ? (
-            <Video src={videosrc} width="100%" height="100%" />
-          ) : (
-            <picture>
-              <source media="(min-width: 767px)" srcSet={imgsrc} />
-              <img src={imgsrc2} alt={imgalt} />
-            </picture>
-          )}
+        <div className="cell medium-7">
+          <div className={heroImageClasses}>
+            {variant === 'video' ? (
+              <Video src={videosrc} width="100%" height="100%" />
+            ) : (
+              <picture>
+                <source media="(min-width: 767px)" srcSet={imgsrc} />
+                <img src={imgsrc2} alt={imgalt} />
+              </picture>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
-
-PageHeroOption.defaultProps = {
-  variant: 'image',
-};
+  );
+}
 
