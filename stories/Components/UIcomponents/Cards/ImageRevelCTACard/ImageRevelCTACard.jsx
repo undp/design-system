f8bc7d180b597f8abc1eafcba76cbb3c0b4d572a
Philip Boden <philip.boden@gmail.com>
2023-02-12 02:07:52 -0800

    Image Reveal Card added to accent control

--- stories/Components/UIcomponents/Cards/ImageRevelCTACard/ImageRevelCTACard.jsx
+++ stories/Components/UIcomponents/Cards/ImageRevelCTACard/ImageRevelCTACard.jsx
@@ -4,29 +4,39 @@ import '../../../../assets/scss/_grid.scss';
 import { Heading } from '../../../../Atom/Typography/Heading/Heading';
 import { Ctalink } from '../../Buttons/CtaLink/CtaLink';
 
-
 const cls = (...classes) => ((classes.filter(Boolean).length > 0) ? classes.filter(Boolean).join(' ') : null);
-export const hovercolors_options = {
-  yellow: '',
+
+export const hovercolor_options = {
+  yellow: 'yellow',
   red: 'red',
   blue: 'blue',
   green: 'green',
+  accent: '',
 };
 
-export const ImageRevelCard = ({
-  contentname, image, Size, data, delaytime,
-}) => {
+export function ImageRevelCard({
+  contentname,
+  image,
+  Size,
+  data,
+  delaytime,
+  ...args
+}) {
   let size = 'medium-4';
   if (Size == 'Small') {
     size = 'small';
   }
+
+  let { accent } = args;
+  let hovercolor_variant = hovercolor_options[`${args.Hovercolors}`];
+  let Hovercolor = (Boolean(accent)) && (!hovercolor_variant) ? accent : hovercolor_variant;
 
   return (
     <div className="grid-x grid-margin-x">
       {data.map((item, index) => (
         <div key={index} data-viewport="true" className={['cell', `${size}`, 'image-reveal-card', `delay-${2 + index++}`].join(' ')}>
           <a href={item.link}>
-            <div className='image-reveal-card__content'>
+            <div className={`image-reveal-card__content ${Hovercolor}`}>
               <div className="image">
                 {item.imageback && (<img src={item.imageback} alt={item.imageback} />)}
               </div>
@@ -38,4 +48,4 @@ export const ImageRevelCard = ({
       ))}
     </div>
   );
-};
+}
