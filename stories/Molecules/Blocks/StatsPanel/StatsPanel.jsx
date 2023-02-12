76574fd2b1c6c302435d6bf07a3375073e4237ed
Philip Boden <philip.boden@gmail.com>
2023-02-12 03:34:53 -0800

    Added Stats and StatsPanel to accent control

--- stories/Molecules/Blocks/StatsPanel/StatsPanel.jsx
+++ stories/Molecules/Blocks/StatsPanel/StatsPanel.jsx
@@ -2,27 +2,34 @@ import React from 'react';
 import './stats-panel.scss';
 import { StatsCards } from '../../../Components/UIcomponents/Cards/StatsCards/StatsCards';
 
-export const Hovercolors_options = {
-  yellow: '',
+export const color_options = {
+  yellow: 'yellow',
   red: 'red',
   green: 'green',
   blue: 'blue',
+  accent: '',
 };
 
 const cls = (...classes) => ((classes.filter(Boolean).length > 0) ? classes.filter(Boolean).join(' ') : null);
 
-export const StatsPanel = ({
-  number, percent, content, imageback, ...args
-}) => {
-  let screen_variant = Hovercolors_options[`${args.Hovercolors}`];
+export function StatsPanel({
+  number,
+  percent,
+  content,
+  imageback,
+  ...args
+}) {
+  const { accent } = args;
+  const hovercolor = color_options[`${args.Hovercolors}`];
+  const Color = (Boolean(accent)) && (!hovercolor) ? accent : hovercolor;
   return (
-    <div className={cls('stats-panel', `${screen_variant}`)}>
+    <div className={cls('stats-panel', `${Color}`)}>
       <img src={imageback} alt={imageback} />
       <StatsCards number={number} percent={percent} content={content} />
     </div>
   );
-};
+}
 
 StatsPanel.defaultProps = {
-  Hovercolors: 'yellow'
+  Hovercolors: 'accent',
 };
