2c3f8f239ebc0a3b0b24ac12e7430da85dac8846
Philip Boden <philip.boden@gmail.com>
2023-02-12 02:33:42 -0800

    Added publication card and publication thumbnail to accent control

--- stories/Atom/Cards/PublicationThumbnail/PublicationThumbnail.jsx
+++ stories/Atom/Cards/PublicationThumbnail/PublicationThumbnail.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './publication-thumbnail.scss';
 import user from '../../../assets/images/publication-thumbnail.png';
 
-export const Publicationthumb = ({ Hovercolors }) => {
+export function Publicationthumb({ Hovercolors }) {
   const image = {
     src: user,
     alt: 'Image',
   };
 
   return (
-    <div className={['publication-thumbnail__image'].join(' ')}>
+    <div className={['publication-thumbnail__image', Hovercolors].join(' ')}>
       <img src={image.src} alt={image.alt} />
     </div>
   );
-};
+}
--- stories/Molecules/Blocks/PublicationCard/PublicationCard.jsx
+++ stories/Molecules/Blocks/PublicationCard/PublicationCard.jsx
@@ -5,47 +5,53 @@ import '../../../Atom/Icons/icons.scss';
 import { Publicationthumb } from '../../../Atom/Cards/PublicationThumbnail/PublicationThumbnail';
 import { Cardthumb } from '../../../Atom/Cards/CardThumbnail/CardThumbnail';
 
-export const PublicationCard = ({
-  image, image2, download, readmore, ...args
-}) => (
-  <div className="publication-card">
-    {image && <Publicationthumb />}
-    {image2 && <Cardthumb />}
-    <div className="description">
-      {args.variant === 'Download'
-        && (
-        <div className="download">
-          {download}
-          <span className="download-animated"><i/></span>
-        </div>
-        ) }
-      {args.variant === 'Readmore'
-        && (
-        <div className="readmore">
-          {readmore}
-          <span className="external-link-animated"><i/></span>
-        </div>
-        )}
-      {args.variant === 'Both'
-        && (
-        <div className="grid-x">
-          <div className="cell small-6">
+export function PublicationCard({
+  image,
+  image2,
+  download,
+  readmore,
+  ...args
+}) {
+  return (
+    <div className={['publication-card'].join(' ')}>
+      {image && <Publicationthumb Hovercolors={args.accent} />}
+      {image2 && <Cardthumb />}
+      <div className="description">
+        {args.variant === 'Download'
+          && (
             <div className="download">
               {download}
-              <span className="download-animated"><i/></span>
+              <span className="download-animated"><i /></span>
             </div>
-          </div>
-          <div className="cell small-6">
+          )}
+        {args.variant === 'Readmore'
+          && (
             <div className="readmore">
               {readmore}
-              <span className="external-link-animated"><i/></span>
+              <span className="external-link-animated"><i /></span>
             </div>
-          </div>
-        </div>
-        )}
+          )}
+        {args.variant === 'Both'
+          && (
+            <div className="grid-x">
+              <div className="cell small-6">
+                <div className="download">
+                  {download}
+                  <span className="download-animated"><i /></span>
+                </div>
+              </div>
+              <div className="cell small-6">
+                <div className="readmore">
+                  {readmore}
+                  <span className="external-link-animated"><i /></span>
+                </div>
+              </div>
+            </div>
+          )}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 PublicationCard.defaultProps = {
   variant: 'Both',
