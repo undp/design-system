90bdb67773a4a894397b025f4f7f19db754a7ae0
Philip Boden <philip.boden@gmail.com>
2023-03-08 17:26:31 -0800

    #1157 -- Fix the default setting to blockquotes to use the yellow background in templates

--- stories/Templates/ArticlePage/ArticlePage.jsx
+++ stories/Templates/ArticlePage/ArticlePage.jsx
@@ -14,10 +14,9 @@ import { Footer } from '../../Organism/Footer/Footer';
 import GlobalHeader from '../../Components/Navigationcomponents/Mainnavigation/GlobalHeader/GlobalHeader';
 import { ProgressBarNavigation } from '../../Atom/Navigation/ProgressBarNavigation/ProgressBarNavigation';
 import { Image } from '../../Atom/Images/Image/Image';
-import '../../assets/js/lazyload.js';
+import '../../assets/js/lazyload';
 
-
-const ArticlePage = ({
+function ArticlePage({
   data,
   header,
   country,
@@ -43,125 +42,127 @@ const ArticlePage = ({
   logo,
   logowhite,
   ...args
-}) => (
-  <div className='article-page'>
-    <GlobalHeader
-      backcaption={backcaption}
-      locale={locale}
-      leftNavigationData={leftNavigationData}
-      navigationData={navigationData}
-      rightNavigationData={rightNavigationData}
-      languageswitcherData={languageswitcherData}
-      locationData={locationData}
-      langSelect={langSelect}
-    />
-    <ProgressBarNavigation Colors={args.accent} />
-    <div className="grid-container fluid mt-h">
-      <div className="grid-x">
-        <div className="cell large-9 medium-offset-1 medium-10">
-          <PostHeader
-            data={data}
-            header={header}
-            country={country}
-            description={description}
-            active="default"
-          />
+}) {
+  console.log(args.accent);
+  return (
+    <div className="article-page">
+      <GlobalHeader
+        backcaption={backcaption}
+        locale={locale}
+        leftNavigationData={leftNavigationData}
+        navigationData={navigationData}
+        rightNavigationData={rightNavigationData}
+        languageswitcherData={languageswitcherData}
+        locationData={locationData}
+        langSelect={langSelect}
+      />
+      <ProgressBarNavigation Colors={args.accent} />
+      <div className="grid-container fluid mt-h">
+        <div className="grid-x">
+          <div className="cell large-9 medium-offset-1 medium-10">
+            <PostHeader
+              data={data}
+              header={header}
+              country={country}
+              description={description}
+              active="default"
+            />
+          </div>
+        </div>
+        <div className="grid-x">
+          <div className="cell large-12 large-article-image">
+            <Image />
+          </div>
+        </div>
+        <div className="grid-x">
+          <div className="cell medium-7 medium-offset-2 image-caption">
+            <Imagecaption
+              label={imageCaptionData.label}
+              name={imageCaptionData.name}
+              paragraph={imageCaptionData.paragraph}
+            />
+          </div>
+        </div>
+        <div className="grid-x">
+          <div className="cell large-7 medium-10 medium-offset-2 author-section">
+            <Heading type="5" label={headingData.detail5} />
+            <Author image={user} data={authorCardData} Number="Three" {...args} />
+          </div>
+        </div>
+        <div className="grid-x">
+          <div className="cell medium-7 medium-offset-2">
+            <P label={para.para1} />
+            <P label={para.para2} />
+          </div>
+        </div>
+        <div className="grid-x blockq">
+          <div className="cell medium-9">
+            <BlockquoteComponent
+              blockquoteText={blockquoteData.blockquote}
+              citeText={blockquoteData.cite}
+              Colors={!args.accent ? 'yellow' : args.accent}
+            />
+          </div>
+        </div>
+        <div className="grid-x">
+          <div className="cell medium-7 medium-offset-2">
+            <P label={para.para1} />
+            <P label={para.para2} />
+          </div>
+          <div className="cell medium-7 medium-offset-2 middle-image">
+            <Image className="lazy" />
+          </div>
+          <div className="cell medium-7 medium-offset-2 middle-caption">
+            <Imagecaption
+              label={imageCaptionData.label}
+              name={imageCaptionData.name}
+              paragraph={imageCaptionData.paragraph}
+            />
+          </div>
+          <div className="cell medium-7 medium-offset-2">
+            <P label={para.para1} />
+            <P label={para.para2} />
+          </div>
+        </div>
+        <div className="grid-x section-head">
+          <div className="cell medium-8 medium-offset-1">
+            <Heading type="2" label={headingLabel} />
+          </div>
+        </div>
+        <div className="grid-x">
+          <div className="cell medium-7 medium-offset-2 list-section">
+            <List data={listData} type="ul" />
+          </div>
         </div>
       </div>
-      <div className="grid-x">
-        <div className="cell large-12 large-article-image">
-          <Image />
-        </div>
-      </div>
-      <div className="grid-x">
-        <div className="cell medium-7 medium-offset-2 image-caption">
-          <Imagecaption
-            label={imageCaptionData.label}
-            name={imageCaptionData.name}
-            paragraph={imageCaptionData.paragraph}
-          />
-        </div>
-      </div>
-      <div className="grid-x">
-        <div className="cell large-7 medium-10 medium-offset-2 author-section">
-          <Heading type="5" label={headingData.detail5} />
-          <Author image={user} data={authorCardData} Number="Three" {...args} />
-        </div>
-      </div>
-      <div className="grid-x">
-        <div className="cell medium-7 medium-offset-2">
-          <P label={para.para1} />
-          <P label={para.para2} />
-        </div>
-      </div>
-      <div className="grid-x blockq">
-        <div className="cell medium-9">
-          <BlockquoteComponent
-            blockquoteText={blockquoteData.blockquote}
-            citeText={blockquoteData.cite}
-            Colors={args.accent}
-          />
-        </div>
-      </div>
-      <div className="grid-x">
-        <div className="cell medium-7 medium-offset-2">
-          <P label={para.para1} />
-          <P label={para.para2} />
-        </div>
-        <div className="cell medium-7 medium-offset-2 middle-image">
-          <Image className="lazy" />
-        </div>
-        <div className="cell medium-7 medium-offset-2 middle-caption">
-          <Imagecaption
-            label={imageCaptionData.label}
-            name={imageCaptionData.name}
-            paragraph={imageCaptionData.paragraph}
-          />
-        </div>
-        <div className="cell medium-7 medium-offset-2">
-          <P label={para.para1} />
-          <P label={para.para2} />
-        </div>
-      </div>
-      <div className="grid-x section-head">
-        <div className="cell medium-8 medium-offset-1">
-          <Heading type="2" label={headingLabel} />
-        </div>
-      </div>
-      <div className="grid-x">
-        <div className="cell medium-7 medium-offset-2 list-section">
-          <List data={listData} type="ul" />
-        </div>
-      </div>
+      <FeaturedCard
+        data={featuredCardContent.featureddata}
+        headertext={featuredCardContent.headertext}
+        {...args}
+      />
+      <Footer
+        src={logo}
+        srctwo={logowhite}
+        headerText={footerData.headerText}
+        headerText2={footerData.headerText2}
+        style="default"
+        alt="UNDP Logo"
+        element="input"
+        type="email"
+        required="required"
+        mode="form-email"
+        errorText={footerData.errorLabel}
+        label={footerData.labelname}
+        button={footerData.buttonname}
+        placeholder={footerData.placeholdername}
+        menutitle={footerData.pageTitle}
+        data={footerData.footerdata}
+        copyright={footerData.copyright}
+        menudata={footerData.menudata}
+        {...{ color: 'default' }}
+      />
     </div>
-    <FeaturedCard
-      data={featuredCardContent.featureddata}
-      headertext={featuredCardContent.headertext}
-      {...args}
-    />
-    <Footer
-      src={logo}
-      srctwo={logowhite}
-      headerText={footerData.headerText}
-      headerText2={footerData.headerText2}
-      style="default"
-      alt="UNDP Logo"
-      element="input"
-      type="email"
-      required="required"
-      mode="form-email"
-      errorText={footerData.errorLabel}
-      label={footerData.labelname}
-      button={footerData.buttonname}
-      placeholder={footerData.placeholdername}
-      menutitle={footerData.pageTitle}
-      data={footerData.footerdata}
-      copyright={footerData.copyright}
-      menudata={footerData.menudata}
-      {...{ color: 'default' }}
-    />
-  </div>
-);
+  );
+}
 
 export default ArticlePage;
-
