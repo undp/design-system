746a40bd3aaa488ed2392d21347407c45ef617c0
Caleb Luster <cwl117@outlook.com>
2024-09-18 13:26:21 -0500

    attempting fix on #1367

--- stories/Components/Navigationcomponents/Breadcrumbs/Breadcrumbs.stories.js
+++ stories/Components/Navigationcomponents/Breadcrumbs/Breadcrumbs.stories.js
@@ -21,6 +21,9 @@ export default {
           ],
         },
         args: {
+          backgrounds: {
+            default: "Light gray",
+          },
           Color: "Black",
         },
       docs: {
