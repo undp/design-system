5441d5f13d1baf6bf60d838cec9346405f26b706
vern-gora <danvern99@gmail.com>
2024-10-24 13:49:56 +0300

    Update build-figma-tokens.mjs

--- stories/assets/js/build-figma-tokens.mjs
+++ stories/assets/js/build-figma-tokens.mjs
@@ -1,21 +1,21 @@
-import StyleDictionary from 'style-dictionary';
+import StyleDictionary from 'style-dictionary'
 
-const config = {
-  source: ["../../../figma-tokens/input/**/*.json"],
-  platforms: {
-    scss: {
-      buildPath: "../../../stories/assets/scss/figma-scss/",
-      transformGroup: "scss",
-      files: [
+const myStyleDictionary = new StyleDictionary()
+
+const config = await myStyleDictionary.extend({
+  "source": ["../../../figma-tokens/input/**/*.json"],
+  "platforms": {
+    "scss": {
+      "buildPath": "../../../stories/assets/scss/figma-scss/",
+      "transformGroup": "scss",
+      "files": [
         {
-          format: "scss/variables",
-          destination: "_figma-variables.scss",
+          "format": "scss/variables",
+          "destination": "_figma-variables.scss",
         },
       ],
     },
   },
-};
+});
 
-const myStyleDictionary = StyleDictionary.extend(config);
-
-myStyleDictionary.buildAllPlatforms();
+config.buildAllPlatforms();
