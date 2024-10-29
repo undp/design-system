77ccd85bac6d52a2a01827e1a88c4049842be3d9
vern-gora <danvern99@gmail.com>
2024-10-29 02:10:38 +0200

    add config

--- figma-tokens/build-figma-tokens.mjs
+++ figma-tokens/build-figma-tokens.mjs
@@ -1,19 +1,4 @@
 import StyleDictionary from 'style-dictionary';
 
-const config = StyleDictionary.extend({
-  source: ['../../../figma-tokens/input/**/*.json'],
-  platforms: {
-    scss: {
-      buildPath: '../../../stories/assets/scss/figma-scss/',
-      transformGroup: 'scss',
-      files: [
-        {
-          format: 'scss/variables',
-          destination: '_figma-variables.scss',
-        },
-      ],
-    },
-  },
-});
-
-config.buildAllPlatforms();
+const sd = new StyleDictionary('config.json');
+await sd.buildAllPlatforms();
