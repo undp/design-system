f24774ddc2c4dd81fc808f4724fbb996034e90b0
elijah <elijahkim45@gmail.com>
2025-01-11 09:21:28 -0500

    #1375 - page hero tall enhancement

--- stories/Atom/Typography/Heading/Heading.jsx
+++ stories/Atom/Typography/Heading/Heading.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import '../../../assets/scss/_typography.scss';
+import React, { useEffect } from "react";
+import "../../../assets/scss/_typography.scss";
+import { fitHeading } from "../../../assets/js/fitText";
 
 export function Heading({
   type,
@@ -7,14 +8,34 @@ export function Heading({
   className,
   tabIndex,
   dataViewport,
+  isFitHeading,
 }) {
+  useEffect(() => {
+    if (isFitHeading) {
+      // use setTimeout to apply Fitty after the DOM has stabilized.
+      // If you don’t use setTimeout, the heading may overlap the top of
+      // the right image when refreshing the page or loading the homepage
+      // for the first time.
+      const timeoutId = setTimeout(() => {
+        const headingFunction = fitHeading(".fit-text");
+        return headingFunction;
+      }, 100);
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [isFitHeading]);
   const HeadingTag = `h${type}`;
-  const tab_index = tabIndex ?? '0';
+  const tab_index = tabIndex ?? "0";
   const data_viewport = dataViewport ?? false;
-  const heading_classes = className ?? '';
+  const heading_classes = className ?? "";
 
   return (
-    <HeadingTag className={heading_classes} tabIndex={tab_index} data-viewport={data_viewport}>
+    <HeadingTag
+      className={`${heading_classes} fit-text`}
+      tabIndex={tab_index}
+      data-viewport={data_viewport}
+    >
       {label}
     </HeadingTag>
   );
--- stories/assets/js/fitText.js
+++ stories/assets/js/fitText.js
@@ -1,16 +1,44 @@
-import fitty from 'fitty';
+import fitty from "fitty";
 
 export const fitText = (selector) => {
-  let items = (typeof selector === 'string') ? document.querySelectorAll(selector) : [selector];
+  let items =
+    typeof selector === "string"
+      ? document.querySelectorAll(selector)
+      : [selector];
   items.forEach((ele) => {
     // remove inline font size and retireve it from stylesheet
     if (ele.style.removeProperty) {
-      ele.style.removeProperty('font-size');
+      ele.style.removeProperty("font-size");
     } else {
-      ele.style.removeAttribute('font-size');
+      ele.style.removeAttribute("font-size");
     }
-    let fontSize = window.getComputedStyle(ele, null).getPropertyValue('font-size');
+    let fontSize = window
+      .getComputedStyle(ele, null)
+      .getPropertyValue("font-size");
     let maxSize = Math.round(parseFloat(fontSize));
     fitty(ele, { maxSize });
+  });
+};
+
+export const fitHeading = (selector) => {
+  const items =
+    typeof selector === "string"
+      ? document.querySelectorAll(selector)
+      : [selector];
+
+  const getMinSize = () => (window.innerWidth > 480 ? 24 : 16);
+
+  items.forEach((ele) => {
+    if (ele.style.removeProperty) {
+      ele.style.removeProperty("font-size");
+    } else {
+      ele.style.removeAttribute("font-size");
+    }
+
+    ele.style.whiteSpace = "normal";
+    fitty(ele, {
+      multiLine: true,
+      minSize: getMinSize(),
+    });
   });
 };
