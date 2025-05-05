97881440c4582491f8f80142328e05159d611520
Jura Khrapunov <jura.khrapunov@undp.org>
2025-05-05 16:17:36 -0400

    #1541 - updated auto initiation routines

--- stories/assets/js/scrolling-progress-bar.js
+++ stories/assets/js/scrolling-progress-bar.js
@@ -1,8 +1,11 @@
-if (jQuery('.progress-container .progress-bar').length > 0) {
-  jQuery(window).on('scroll', () => {
-    const $windowHeight = (jQuery(window).height() == jQuery(document).height()) ? window.innerHeight : jQuery(window).height();
-    const $scrollPercent = (jQuery(window).scrollTop() / (jQuery(document).height() - $windowHeight)) * 100;
-    const $roundScroll = Math.round($scrollPercent);
-    jQuery('.progress-container .progress-bar').css('width', `${$roundScroll}%`).toggleClass('progress-top', $roundScroll > 1);
-  });
-}
+jQuery(window).on('load', () => {
+  const $progressBar = jQuery('.progress-container .progress-bar');
+  if ($progressBar.length > 0) {
+    jQuery(window).on('scroll', () => {
+      const $windowHeight = (jQuery(window).height() == jQuery(document).height()) ? window.innerHeight : jQuery(window).height();
+      const $scrollPercent = (jQuery(window).scrollTop() / (jQuery(document).height() - $windowHeight)) * 100;
+      const $roundScroll = Math.round($scrollPercent);
+      $progressBar.css('width', `${$roundScroll}%`).toggleClass('progress-top', $roundScroll > 1);
+    });
+  }
+});
--- stories/assets/js/viewport.js
+++ stories/assets/js/viewport.js
@@ -18,19 +18,28 @@
 //   });
 // });
 
-const isElementInViewport = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('inviewport');
-      isElementInViewport.unobserve(entry.target);
-      if (isElementInViewport.takeRecords().length === 0) {
-        isElementInViewport.disconnect();
-      }
-    }
-  });
-});
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('[data-viewport=true]').forEach((element) => {
-    isElementInViewport.observe(element);
-  });
+
+
+jQuery(window).on('load', () => {
+
+  const $objs = jQuery('[data-viewport=true]');
+  if ($objs.length > 0) {
+
+    const isElementInViewport = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          jQuery(entry.target).addClass('inviewport');
+          isElementInViewport.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.1 // Trigger when at least 10% of the element is visible
+    });
+
+    $objs.each((i, element) => {
+      isElementInViewport.observe(element);
+    });
+
+  }
+
 });
