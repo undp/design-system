e7e69759c59745d77ccc74ed396692d757d1e4b1
Jura Khrapunov <jura.khrapunov@undp.org>
2025-05-05 16:41:11 -0400

    #1541 - updated compiled assets

--- stories/assets/js/viewport.js
+++ stories/assets/js/viewport.js
@@ -1,25 +1,3 @@
-/* Function to chech if element is in viewport or not */
-
-// jQuery.fn.isInViewport = function () {
-//   const $elementTop = jQuery(this).offset().top;
-//   const $elementBottom = $elementTop + jQuery(this).outerHeight();
-
-//   const $viewportTop = jQuery(window).scrollTop();
-//   const $viewportBottom = $viewportTop + jQuery(window).height();
-
-//   return $elementBottom > $viewportTop && $elementTop < $viewportBottom;
-// };
-
-// jQuery(window).on('resize scroll load', () => {
-//   jQuery('[data-viewport=true]').each(function () {
-//     if (jQuery(this).isInViewport()) {
-//       jQuery(this).addClass('inviewport');
-//     }
-//   });
-// });
-
-
-
 jQuery(window).on('load', () => {
 
   const $objs = jQuery('[data-viewport=true]');
