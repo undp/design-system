52e8efb99f7b63b121641239e3b41eaf3747b3cb
Jura Khrapunov <jura.khrapunov@undp.org>
2025-10-02 18:16:38 -0400

    Added checkboxes to download modal widget, closes #1592

--- stories/Molecules/DownloadRow/DownloadRow.jsx
+++ stories/Molecules/DownloadRow/DownloadRow.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import './download-row.scss';
 import { Checkbox } from '../../Components/Forms/Checkbox/Checkbox';
-import { Heading } from '../../Atom/Typography/Heading/Heading';
 
 export function DownloadRow({
   title, subtitle, value, dataValue, ...args
