50839ac840161df11c92e7bca84ab120e619ac17
Jura Khrapunov <jura.khrapunov@undp.org>
2025-11-06 15:29:07 -0500

    Force full chromatic rebuild

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,8 +5,9 @@ import { createRequire } from "node:module";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const require = createRequire(import.meta.url);
-// Original file content:
 const path = require('path');
+
+// force full rebuild
 
 const config = {
   staticDirs: ['../stories/assets'],
