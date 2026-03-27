7e792cb35d599941626a7bab1878951899b26477
Jura Khrapunov <jura.khrapunov@undp.org>
2026-03-27 17:11:33 -0400

    #1704 - updated Featured cards styling

--- stories/Components/UIcomponents/Cards/FeaturedCard/FeaturedContentCard.jsx
+++ stories/Components/UIcomponents/Cards/FeaturedCard/FeaturedContentCard.jsx
@@ -17,20 +17,6 @@ export const image_options = {
   color: 'without-image',
 };
 
-export const hovercolor_options = {
-  yellow: 'yellow',
-  red: 'red',
-  green: 'green',
-  blue: 'blue',
-};
-
-export const accent_color_options = {
-  yellow: 'yellow',
-  red: 'red',
-  blue: 'blue',
-  green: 'green',
-};
-
 const cls = (...classes) => ((classes.filter(Boolean).length > 0) ? classes.filter(Boolean).join(' ') : null);
 
 export function FeaturedContentCard({
@@ -39,14 +25,10 @@ export function FeaturedContentCard({
   data,
   image,
   headertext,
-  hovercolor,
   ...args
 }) {
   let size_variant = size_options[`${args.size}`];
   let image_variant = image_options[`${image}`];
-
-  let color = (hovercolor) ? accent_color_options[`${hovercolor}`] : accent_color_options[`${args.accent}`];
-  let accent_color = color ? `accent-${color}` : '';
   const fcCardCls = isChromatic() ? 'cell small-3 medium-3 large-3 medium-offset-1 small-offset-1 feature__card-title inviewport' : 'cell small-3 medium-3 large-3 medium-offset-1 small-offset-1 feature__card-title';
 
   return (
@@ -59,18 +41,16 @@ export function FeaturedContentCard({
       {data.map((item, index) => (
         <div
           key={index}
-          className={cls('cell', `${item.scale ? size_options[`${item.scale}`] : size_variant}`, 'feature__card', `${item.type ? `${image_options[`${item.type}`]}` : `${image_variant}`}`, `${item.hovercolor ? `${hovercolor_options[`${item.hovercolor}`]}` : `${accent_color}`}`)}
+          className={cls('cell', `${item.scale ? size_options[`${item.scale}`] : size_variant}`, 'feature__card', `${item.type ? `${image_options[`${item.type}`]}` : `${image_variant}`}`)}
         >
           <a href={item.url ? item.url : '#'}>
             <div className="feature__card-slide">
               {item.imgback && (item.type ? item.type === 'image' : image_variant === 'image') && <div className="feature__card-image"><img src={item.imgback} alt={item.imgback} /></div>}
             </div>
             <article className="feature__card-content">
-              <Heading type="6" label={item.contenttile} />
+              <div className='feature__card-tag'>{item.contenttile}</div>
               <div className="feature__card-description">
-                {(item.scale ? item.scale === 'large' : args.size === 'large') && item.contentname && <Heading type="4" label={item.contentname} />}
-                {(item.scale ? item.scale === 'medium' : args.size === 'medium') && item.contentname && <Heading type="5" label={item.contentname} />}
-                {(item.scale ? item.scale === 'small' : args.size === 'small') && item.contentname && <Heading type="5" label={item.contentname} />}
+                <div className="feature__card-title">{item.contentname}</div>
                 {item.size}
                 {item.descriptionText && <P label={item.descriptionText} />}
               </div>
